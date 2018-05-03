f8dd64f8308cec598943b87d430b10d2997bc199
Travis Amaral <travis@konghq.com>
2018-05-03 08:52:36 -0700

    Feat/custom styles (#14)
    * feat(*) Start adding custom logo
    
    * feat(theme) Add support for React/JSX & add logo
    
    * feat(*) Some fancy styling edits
    
    * feat(*) Add animated github banner
    
    * chore(babel): remove babel es2015 in place of env
    
    * fix(config): Change es2015 preset to env
    
    Adds various eslinting styling fixes

--- styleguide.config.js
+++ styleguide.config.js
@@ -1,27 +1,10 @@
+const path = require('path')
 const rhythm = (value = 1, unit = 'rem', basis = 1.5) => (
   `${basis * value}${unit}`
 )
 
 module.exports = {
   components: 'packages/**/*.vue',
-  webpackConfig: {
-    module: {
-      rules: [
-        {
-          test: /\.vue$/,
-          loader: 'vue-loader',
-        },
-        {
-          test: /\.css$/,
-          use: ['style-loader', 'css-loader', 'sass-loader'],
-        },
-        {
-          test: /\.scss$/,
-          use: ['style-loader', 'css-loader', 'sass-loader'],
-        },
-      ],
-    },
-  },
   editorConfig: {
     theme: 'dracula'
   },
@@ -36,7 +19,7 @@ module.exports = {
       base: '#505659',
       link: '#0070bf',
       linkHover: '#0052a1',
-      ribbonBackground: '#0052a1',
+      ribbonBackground: '#042943',
       sidebarBackground: '#042943',
       errorBackground: '#d01040'
     }
@@ -47,7 +30,7 @@ module.exports = {
         marginBottom: rhythm(0.5)
       },
       heading3: {
-        borderBottom: `thin solid #eee`,
+        borderBottom: `thin solid #fff`,
         paddingBottom: rhythm(0.25),
         marginBottom: rhythm(1),
         textTransform: 'uppercase',
@@ -56,10 +39,9 @@ module.exports = {
         }
       }
     },
-    Logo: {
+    StyleGuide: {
       logo: {
-        color: 'white',
-        fontSize: 20
+        border: 0
       }
     },
     ComponentsList: {
@@ -72,7 +54,7 @@ module.exports = {
         '& a': {
           'color': 'rgba(255, 255, 255, 0.9) !important',
           'fontWeight': 500,
-          'font-size': '1.5em !important',
+          'font-size': '1.25em !important',
           '&:hover': {
             cursor: 'pointer',
             textDecoration: 'underline',
@@ -92,11 +74,6 @@ module.exports = {
       }
     }
   },
-  ribbon: {
-    url: "https://github.com/kong/kongponents",
-    text: "Fork me on GitHub"
-  },
-  showUsage: true,
   template: {
     head: {
       links: [
@@ -105,7 +82,46 @@ module.exports = {
           href: 'https://fonts.googleapis.com/css?family=Roboto'
         }
       ]
+    },
+    body: {
+      raw: '<a href="https://github.com/kong/kongponents" class="github-corner"><svg width="80" height="80" viewBox="0 0 250 250" style="fill:#042943; color:#FFF; position: fixed; top: 0; border: 0; right: 0;"><path d="M0,0 L115,115 L130,115 L142,142 L250,250 L250,0 Z"></path><path d="M128.3,109.0 C113.8,99.7 119.0,89.6 119.0,89.6 C122.0,82.7 120.5,78.6 120.5,78.6 C119.2,72.0 123.4,76.3 123.4,76.3 C127.3,80.9 125.5,87.3 125.5,87.3 C122.9,97.6 130.6,101.9 134.4,103.2" fill="currentColor" style="transform-origin: 130px 106px;" class="octo-arm"></path><path d="M115.0,115.0 C114.9,115.1 118.7,116.5 119.8,115.4 L133.7,101.6 C136.9,99.2 139.9,98.4 142.2,98.6 C133.8,88.0 127.5,74.4 143.8,58.0 C148.5,53.4 154.0,51.2 159.7,51.0 C160.3,49.4 163.2,43.6 171.4,40.1 C171.4,40.1 176.1,42.5 178.8,56.2 C183.1,58.6 187.2,61.8 190.9,65.4 C194.5,69.0 197.7,73.2 200.1,77.6 C213.8,80.2 216.3,84.9 216.3,84.9 C212.7,93.1 206.9,96.0 205.4,96.6 C205.1,102.4 203.0,107.8 198.3,112.5 C181.9,128.9 168.3,122.5 157.7,114.1 C157.9,116.9 156.7,120.9 152.7,124.9 L141.0,136.5 C139.8,137.7 141.6,141.9 141.8,141.8 Z" fill="currentColor" class="octo-body"></path></svg></a><style>.github-corner:hover .octo-arm{animation:octocat-wave 560ms ease-in-out}@keyframes octocat-wave{0%,100%{transform:rotate(0)}20%,60%{transform:rotate(-25deg)}40%,80%{transform:rotate(10deg)}}@media (max-width:500px){.github-corner:hover .octo-arm{animation:none}.github-corner .octo-arm{animation:octocat-wave 560ms ease-in-out}}</style>'
+    }
+  },
+  showUsage: true,
+  styleguideComponents: {
+    Logo: path.join(__dirname, 'styleguide/components/Logo')
+  },
+  webpackConfig: {
+    module: {
+      rules: [
+        {
+          test: /\.vue$/,
+          loader: 'vue-loader'
+        },
+        {
+          test: /\.jsx?$/,
+          exclude: /node_modules/,
+          loader: 'babel-loader',
+          query: {
+            cacheDirectory: true,
+            presets: ['react', 'env']
+          }
+        },
+        {
+          test: /\.css$/,
+          use: ['style-loader', 'css-loader', 'sass-loader']
+        },
+        {
+          test: /\.scss$/,
+          use: ['style-loader', 'css-loader', 'sass-loader']
+        },
+        {
+          test: /\.(jpe?g|png|gif|svg)$/i,
+          loaders: [
+            'file-loader'
+          ]
+        }
+      ]
     }
   }
-  // vuex: 'store/index',
 }
---
+++ styleguide/components/Logo.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import logo from './logo.png'
+
+export function LogoRenderer ({children}) {
+  return <img src={logo} width="150" alt={children} />
+}
+
+LogoRenderer.propTypes = {
+  children: PropTypes.node
+}
+
+export default LogoRenderer
