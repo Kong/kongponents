78d325a6ff94f660a1ac668a9d18c0b6bbafbd14
cat fistori <cat@konghq.com>
2018-05-11 11:38:46 -0700

    feat(KInput): Adds input component (#17)
    * feat(KInput): Adds input component
    
    * feat(kinput): Add click and focus methods
    
    * fix(listeners): Adds listener support to KInput
    
    * fix(styleguidist) Adds stage-3 dependency to babel for spread operator support
    
    * style(input) Removed extra space in input class prop
    
    * docs(input): add inputAttributes example

---
+++ packages/KInput/KInput.vue
@@ -0,0 +1,66 @@
+<template>
+  <input
+    v-bind="inputAttributes"
+    :value="value"
+    :class="[inputAttributes['class']]"
+    v-on="listeners">
+</template>
+
+<script>
+export default {
+  name: 'KInput',
+  props: {
+    /**
+     * Pass whether or not the input should be disabled
+     */
+    inputAttributes: {
+      type: Object,
+      default: function () {
+        return {
+          class: ''
+        }
+      }
+    }
+  },
+  computed: {
+    listeners () {
+      return {
+        ...this.$listeners
+      }
+    }
+  }
+}
+</script>
+
+<style scoped>
+input {
+  display: block;
+  width: 100%;
+  -webkit-box-sizing: border-box; /* Safari/Chrome, other WebKit */
+  -moz-box-sizing: border-box; /* Firefox, other Gecko */
+  box-sizing: border-box; /* Opera/IE 8+ */
+  padding: 8px 12px;
+  font-size: 15px;
+  color: hsla(0, 0%, 0%, 0.7);
+  border-color: hsla(0, 0%, 0%, 0.12);
+  background-color: #ffffff;
+  border-radius: 3px;
+  border-width: 1px;
+}
+
+input:focus {
+  outline: none !important;
+  border: 1px solid hsla(205, 100%, 75%, 1);
+}
+
+input:disabled {
+  cursor: not-allowed;
+  background-color: #f7f7f7;
+  opacity: 0.75;
+}
+
+input.input-error {
+  outline: none !important;
+  border: 1px solid hsla(1, 100%, 75%, 1);
+}
+</style>
