32e9539a38224a5827b8cbc56b1c2d13b068eefa
Travis Amaral <travis@konghq.com>
2018-06-07 12:11:42 -0700

    feat(KIcon): Add Icon component

---
+++ packages/KIcon/KIcon.vue
@@ -0,0 +1,65 @@
+<template>
+  <svg
+    :height="size"
+    :width="size"
+    class="kong-icon"
+    viewBox="0 0 24 24"
+    role="img"
+  >
+    <title>{{ icon }}</title>
+    <path
+      :d="path"
+      :fill="fill" />
+  </svg>
+</template>
+
+<script>
+import icons from './icons'
+const iconNames = Object.keys(icons)
+
+export default {
+  name: 'KIcon',
+  props: {
+    /**
+     * Checks for valid icon name
+     * 'collapseExpand'  | 'gateway' | 'portal' | 'security' | 'workspaces'| 'workspacesCollapsed' | 'vitals'
+     */
+    icon: {
+      type: String,
+      default: '',
+      validator: function (value) {
+        return iconNames.indexOf(value) !== -1
+      }
+    },
+    /**
+     * Defines the height and width in pixels
+     */
+    size: {
+      type: Number,
+      default: 24
+    }
+  },
+
+  data () {
+    return {
+      icons,
+      path: '',
+      fill: ''
+    }
+  },
+
+  created () {
+    let icon = icons[this.icon]
+    let parser = new DOMParser()
+    let doc = parser.parseFromString(icon, 'image/svg+xml')
+    let path = doc.getElementsByTagName('path')[0]
+
+    this.fill = path.getAttribute('fill')
+    this.path = path.getAttribute('d')
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
