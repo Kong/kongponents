c4092abcd344269959e5dc1a3d13a45e84bc62dd
Travis Amaral <travis@konghq.com>
2018-07-30 13:43:17 -0700

    feat(KEmptyState): Creates base empty component (#30)
    * feat(KEmptyState): Creates base empty component
    
    * feat(KEmptyState) Style based off Jitachi specs, Update props

---
+++ packages/KEmptyState/KEmptyState.vue
@@ -0,0 +1,70 @@
+<template>
+  <div class="empty-state-wrapper">
+    <div class="empty-state-title">
+      <h5><slot name="title">Empty state title</slot></h5>
+    </div>
+    <div class="empty-state-content">
+      <p><slot name="message">Empty state message</slot></p>
+      <p>
+        <KButton
+          v-if="!ctaIsHidden"
+          :is-rounded="true"
+          :handle-click="handleClick"
+          appearance="outline-primary">
+          {{ ctaText }}
+        </KButton>
+      </p>
+    </div>
+  </div>
+</template>
+
+<script>
+import KButton from '@kongponents/kbutton'
+export default {
+  name: 'KEmptyState',
+  components: { KButton },
+  props: {
+    ctaIsHidden: {
+      type: Boolean,
+      default: false
+    },
+    ctaText: {
+      type: String,
+      default: 'CTA Text'
+    },
+    handleClick: {
+      type: Function,
+      default: () => () => null
+    }
+  }
+}
+</script>
+
+<style scoped>
+  .empty-state-wrapper {
+  padding: 4rem 0;
+  text-align: center;
+  color: rgba(0,0,0,.70);
+  border: 1px solid rgba(151,151,151,.1);
+  border-radius: 3px;
+  background-color: rgba(0,0,0,.03);
+}
+.empty-state-wrapper h5 {
+  margin: 0 0 .75rem;
+  font-size: 18px;
+  font-weight: 500;
+  line-height: 21px;
+}
+.empty-state-wrapper p {
+  margin: 0 auto 1.5rem;
+  font-size: 1rem;
+  line-height: 1.375rem;
+  font-weight: 400;
+  max-width: 50%;
+}
+.empty-state-wrapper p:last-child {
+  margin: 0;
+  margin-left: auto;
+  margin-right: auto;
+}
+</style>
