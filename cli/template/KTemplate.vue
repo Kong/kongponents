3a145652360521425a36212194aece20840b03d4
Darren Jennings <dmjenn02@gmail.com>
2018-09-06 16:08:00 -0700

    feat(cli) add cli for creating kongponents from templates
    * feat(cli) add cli for creating kongponents from templates
    * feat(cli) fallback when tree command fails
    * docs(readme) add docs for cli
    * feat(cli) change name argument to be first default arg
    * tests(jest) ensure jest ignore template files
    * make sure generate component tests succeed
    
    From #32

---
+++ cli/template/KTemplate.vue
@@ -0,0 +1,26 @@
+<template>
+  <div
+    v-on="listeners">
+    <span>{{ description }}</span>
+    <slot/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: '{%kongponent_name%}',
+  props: {
+    /**
+     * Example documentation comment for a prop
+     */
+    description: {
+      type: String,
+      required: false,
+      default: 'A default description'
+    }
+  }
+}
+</script>
+
+<style scoped>
+</style>
