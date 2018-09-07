fd04a3960b35c63ea552dcc6d1c2a2b1248ce1fe
Travis Amaral <travis@konghq.com>
2018-09-06 19:09:44 -0700

    test(KIcon): Render each, test for size

--- packages/KIcon/KIcon.spec.js
+++ packages/KIcon/KIcon.spec.js
@@ -1,24 +1,28 @@
 import { mount } from '@vue/test-utils'
-import KCard from '@/KIcon/KIcon'
+import KIcon from '@/KIcon/KIcon'
+import icons from './icons'
+const iconNames = Object.keys(icons)
 
-describe('KIcon', () => {
-  it('renders vitals icon', () => {
-    const wrapper = mount(KCard, {
+let rendersIcon = (icon) => {
+  it(`renders ${icon} icon`, () => {
+    const wrapper = mount(KIcon, {
       propsData: {
-        'icon': 'vitals',
-        'size': '32'
+        'icon': `${icon}`
       }
     })
 
-    const title = wrapper.find('title').text()
-
-    expect(wrapper.attributes().width).toEqual('32')
-    expect(title).toEqual(expect.stringContaining(title))
+    expect(wrapper.find('title').text()).toEqual(expect.stringContaining(`${icon}`))
     expect(wrapper.html()).toMatchSnapshot()
+  })
+}
+
+describe('KIcon', () => {
+  iconNames.map(icon => {
+    rendersIcon(icon)
   })
 
   it('renders icon with red fill', () => {
-    const wrapper = mount(KCard, {
+    const wrapper = mount(KIcon, {
       propsData: {
         'icon': 'portal',
         'color': 'red'
@@ -28,5 +32,17 @@ describe('KIcon', () => {
     const path = wrapper.find('path').attributes()
 
     expect(path.fill).toEqual('red')
+  })
+
+  it('renders 32x32 sized icon', () => {
+    const wrapper = mount(KIcon, {
+      propsData: {
+        'icon': 'portal',
+        'size': '32'
+      }
+    })
+
+    expect(wrapper.attributes().width).toEqual('32')
+    expect(wrapper.attributes().height).toEqual('32')
   })
 })
