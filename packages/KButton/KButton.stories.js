58d0306803a7b40b9f55c04676f09a5354bf5775
Darren Jennings <dmjenn02@gmail.com>
2018-09-27 17:14:45 -0700

    feat(*) add kongponents improvements and kbutton disabled states (#39)
    * feat(*) add kongponents improvements and kbutton disabled states
    
    * bump package minor versions
    * add storybook addon for actions
    * add more stories for kbutton
    * add kbutton listeners and disabled states
    * add built storybook static assets
    * add script to "build all"
    
    * style(storybook) add roboto to storybook
    
    * chore(storybook) build latest static assets

--- packages/KButton/KButton.stories.js
+++ packages/KButton/KButton.stories.js
@@ -1,5 +1,6 @@
 import Vue from 'vue'
 import { storiesOf } from '@storybook/vue'
+import { action } from '@storybook/addon-actions'
 import VueInfoAddon from 'storybook-addon-vue-info'
 
 import KButton from './KButton.vue'
@@ -14,7 +15,51 @@ storiesOf('KButton', module)
     components: { KButton },
     template: `<KButton :isRounded='true'>button</KButton>`
   }))
-  .add('Primary Styles', () => ({
+  .add('Styles', () => ({
     components: { KButton },
-    template: `<KButton appearance='primary'>button</KButton>`
+    template: `<div>
+      <KButton appearance='primary'>primary</KButton>
+      <KButton appearance='outline-primary'>outline-primary</KButton>
+      <KButton appearance='btn-link'>btn-link</KButton>
+      <KButton appearance='danger'>danger</KButton>
+      <KButton appearance='outline-danger'>outline-danger</KButton>
+      <KButton appearance='btn-link-danger'>btn-link-danger</KButton>
+    </div>`
+  }))
+  .add('Styles - Disabled', () => ({
+    components: { KButton },
+    template: `<div>
+      <KButton disabled appearance='primary'>primary</KButton>
+      <KButton disabled appearance='outline-primary'>outline-primary</KButton>
+      <KButton disabled appearance='btn-link'>btn-link</KButton>
+      <KButton disabled appearance='danger'>danger</KButton>
+      <KButton disabled appearance='outline-danger'>outline-danger</KButton>
+      <KButton disabled appearance='btn-link-danger'>btn-link-danger</KButton>
+    </div>`
+  }))
+  .add('Button Actions', () => ({
+    components: { KButton },
+    template: `
+    <KButton 
+      @click="onClick"
+      @focus="onFocus"
+      @blur="onBlur"
+      @dblclick="onDblClick"
+      appearance='primary'>
+      button
+    </KButton>`,
+    methods: {
+      onClick (e) {
+        action('onClick')(e)
+      },
+      onFocus (e) {
+        action('onFocus')(e)
+      },
+      onBlur (e) {
+        action('onBlur')(e)
+      },
+      onDblClick (e) {
+        action('onDblClick')(e)
+      }
+    }
   }))
