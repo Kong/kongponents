9289a16c703a47d32f43f8bc03c864417daae49d
Josh Eveleth <joshua.eveleth@gmail.com>
2019-01-01 10:34:19 -0800

    chore(config) use U.S. spelling

--- testconfig/jest.overrides.js
+++ testconfig/jest.overrides.js
@@ -1,6 +1,6 @@
 let error = console.error
 
 console.error = function (message) {
-  error.apply(console, arguments) // keep default behaviour
+  error.apply(console, arguments) // keep default behavior
   throw (message instanceof Error ? message : new Error(message))
 }
