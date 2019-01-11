08be61ba83b3355f3097c87d60fcadb1ec42557d
jeveleth <joshua.eveleth@gmail.com>
2019-01-11 10:56:05 -0800

    tests(*) remove console error override

--- testconfig/jest.overrides.js
+++
@@ -1,6 +0,0 @@
-let error = console.error
-
-console.error = function (message) {
-  error.apply(console, arguments) // keep default behavior
-  throw (message instanceof Error ? message : new Error(message))
-}
