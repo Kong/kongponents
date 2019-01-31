b95c26a230ee44351f47bb36135fd86aae0f6aca
h3xar0n <aron@eideldev.com>
2019-01-31 15:29:34 -0800

    test(empty) test that kemptystate is really empty

--- packages/KEmptyState/KEmptyState.spec.js
+++ packages/KEmptyState/KEmptyState.spec.js
@@ -15,4 +15,15 @@ describe('KEmptyState', () => {
     expect(wrapper.find('.empty-state-title').html()).toEqual(expect.stringContaining(emptyTitle))
     expect(wrapper.find('.empty-state-content').html()).toEqual(expect.stringContaining(emptyMessage))
   })
+
+  it('remains empty when no slots are passed', () => {
+    const emptyTitle = ''
+    const emptyMessage = ''
+    const wrapper = mount(KEmptyState, {
+      slots: {}
+    })
+
+    expect(wrapper.find('.empty-state-title').html()).toEqual(expect.stringContaining(emptyTitle))
+    expect(wrapper.find('.empty-state-content').html()).toEqual(expect.stringContaining(emptyMessage))
+  })
 })
