d2fbdf24056112635b8450d5dbe91221817de721
Aron Eidelman <aron@konghq.com>
2019-02-01 10:38:09 -0800

    feat(kemptystate) use latest kbutton INTF-1022 (#57)
    * feat(deps) bump kbutton to beta.6
    * feat(kbutton) use new click listener
    * fix(defaults) remove default/placeholder text
    * test(kemptystate) provide a test for message slot
    * style(kemptystate) reformat linting
    * chore(lockfiles) bump kemptystate version
    * test(empty) test that kemptystate is really empty
    * chore(lock) patch lock file

--- packages/KEmptyState/KEmptyState.vue
+++ packages/KEmptyState/KEmptyState.vue
@@ -1,17 +1,17 @@
 <template>
   <div class="empty-state-wrapper">
     <div class="empty-state-title">
-      <h5><slot name="title">Empty state title</slot></h5>
+      <h5><slot name="title"/></h5>
     </div>
     <div class="empty-state-content">
-      <p><slot name="message">Empty state message</slot></p>
+      <p><slot name="message"/></p>
       <p>
         <slot name="cta">
           <KButton
             v-if="!ctaIsHidden"
             :is-rounded="true"
-            :handle-click="handleClick"
-            appearance="outline-primary">
+            appearance="outline-primary"
+            @click.native="handleClick">
             {{ ctaText }}
           </KButton>
         </slot>
@@ -32,7 +32,7 @@ export default {
     },
     ctaText: {
       type: String,
-      default: 'CTA Text'
+      default: ''
     },
     handleClick: {
       type: Function,
