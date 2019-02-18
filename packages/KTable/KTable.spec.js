5d1d149832910a9445fd1c3f5adcd8cbab053c4a
Travis Amaral <travis@konghq.com>
2019-02-18 12:54:04 -0800

    test(KTable) add test for table

---
+++ packages/KTable/KTable.spec.js
@@ -0,0 +1,73 @@
+import { mount } from '@vue/test-utils'
+import KTable from '@/KTable/KTable'
+
+const options = {
+  headers: [
+    { label: 'Name', key: 'name', sortable: true },
+    { label: 'Type', key: 'type', sortable: true },
+    { label: 'Credentials', key: 'credentials' },
+    { key: 'actions', hideLabel: true }
+  ],
+  data: [
+    {
+      name: 'iOS App',
+      type: 'Key',
+      credentials: 'xyz'
+    },
+    {
+      name: 'Website Desktop',
+      type: 'Basic',
+      credentials: '123xyz'
+    }
+  ]
+}
+
+describe('KTable', () => {
+  it('renders link in action slot', () => {
+    const wrapper = mount(KTable, {
+      propsData: {
+        options
+      },
+      scopedSlots: {
+        actions: '<a href="#" slot-scope="actions">Link</a>'
+      }
+    })
+
+    const actions = wrapper.findAll('.table td:last-of-type > *')
+
+    expect(actions.at(0).is('a')).toBe(true)
+    expect(actions.at(1).is('a')).toBe(true)
+  })
+
+  it('has hover class when passed', () => {
+    const wrapper = mount(KTable, {
+      propsData: {
+        options,
+        hasHover: true
+      }
+    })
+
+    expect(wrapper.classes()).toContain('hover')
+  })
+
+  it('has striped class when passed', () => {
+    const wrapper = mount(KTable, {
+      propsData: {
+        options,
+        isStriped: true
+      }
+    })
+
+    expect(wrapper.classes()).toContain('striped')
+  })
+
+  it('matches snapshot', () => {
+    const wrapper = mount(KTable, {
+      propsData: {
+        options
+      }
+    })
+
+    expect(wrapper.html()).toMatchSnapshot()
+  })
+})
