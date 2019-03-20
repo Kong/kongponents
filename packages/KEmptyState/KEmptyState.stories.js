8be327aa25d21937af28a85b666c538738540e8f
Darren Jennings <dmjenn02@gmail.com>
2019-03-20 12:55:38 -0700

    docs(storybook/kemptystate) switch back to click

--- packages/KEmptyState/KEmptyState.stories.js
+++ packages/KEmptyState/KEmptyState.stories.js
@@ -5,7 +5,7 @@ storiesOf('EmptyState', module)
   .add('Call to action', () => ({
     components: { KEmptyState },
     template: `<div>
-    <KEmptyState cta-text="Do A Thing" @cta="callToAction">
+    <KEmptyState cta-text="Do A Thing" :handle-click="callToAction">
       <template slot="title">No Enabled Entities</template>
       <template slot="message">To do the thing, click below.</template>
       </KEmptyState>
