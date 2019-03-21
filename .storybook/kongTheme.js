3db8c2e163be930e963b9cfdbf072d608b03bb38
Darren Jennings <dmjenn02@gmail.com>
2019-03-21 15:45:43 -0700

    docs(theme) add storybook theme kong/logo (#65)
    * chore(storybook) build latest static assets
    
    Fixes INTF-1282

---
+++ .storybook/kongTheme.js
@@ -0,0 +1,8 @@
+import { create } from '@storybook/theming';
+
+export default create({
+  base: 'light',
+  brandTitle: 'Kong Storybook',
+  brandUrl: 'https://github.com/kong/kongponents',
+  brandImage: 'https://konghq.com/wp-content/themes/konghq/assets/img/gradient-logo.svg',
+});
