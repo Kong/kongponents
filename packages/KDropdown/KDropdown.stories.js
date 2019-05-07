e90bd9d2c4de1bf1fb041b6f67a70e8902b70c32
Travis Amaral <travis@konghq.com>
2019-05-07 10:31:05 -0700

    feat(KDropdown) Update to match styleguide

--- packages/KDropdown/KDropdown.stories.js
+++ packages/KDropdown/KDropdown.stories.js
@@ -1,58 +1,98 @@
 import Vue from 'vue'
 import { storiesOf } from '@storybook/vue'
 import { action } from '@storybook/addon-actions'
+import StoryRouter from 'storybook-vue-router'
 
 import { default as KDropdown, alignments } from './KDropdown.vue'
+import KIcon from '../KIcon/KIcon.vue'
 
 storiesOf('KDropdown', module)
   .add('Default', () => ({
     components: { KDropdown },
-    template: `
-    <div style="margin-left: 50px">
+    template: `<div>
+    <KDropdown
+      :items="['item 1', 'item 2', 'item 3']"
+      toggle-text="Open Dropdown" />
       <KDropdown
-        :items="['item 1', 'item 2', 'item 3']"
-        toggle-text="Open Dropdown" />
-    </div>
-  `}), {info: {}})
+      :items="['item 1', 'item 2', 'item 3']"
+      toggle-text="Open Dropdown"
+      button-type="text" />
+    </div>`
+  }), {info: {}})
   .add('Alignments', () => ({
     components: { KDropdown },
-    template: `
-    <div style="display:flex">
+    template: `<div>
     ${Object.keys(alignments).map(position => {
-      return `<div style="margin: 1rem">
-        <KDropdown
-          :items="['item 1', 'item 2', 'item 3']"
-          position="${position}"
-          toggle-text="Open ${position}" />
-      </div>`
-    }).join('')}
-    </div>
-  `}), {info: {}})
-  .add('Custom Text', () => ({
-    components: { KDropdown },
-    template: `
-    <div style="margin-left: 50px">
+      return `
       <KDropdown
         :items="['item 1', 'item 2', 'item 3']"
-        :toggle-icon="false"
-        position="left">
-        <template slot="toggle-text">Look mah, no arrow!</template>
+        position="${position}"
+        toggle-text="${position}" />
+      `
+    }).join('')}
+    </div>
+    `
+  }), { info: {} })
+  .add('Icons', () => ({
+    components: { KDropdown, KIcon },
+    template: `<div>
+      <KDropdown
+        :items="['item 1', 'item 2', 'item 3']" >
+        <KIcon slot="icon" icon="gear" />
       </KDropdown>
     </div>
-  `}), { info: {} })
+    `
+  }), { info: {} })
+  .add('Computed', () => ({
+    components: { KDropdown },
+    template: `<div>
+      <KDropdown :items="items" :toggle-text="selectedText"/>
+    </div>
+    `,
+    computed: {
+      selectedText () {
+        return this.items.filter(i => i.selected).length + ' Selected'
+      }
+    },
+    data: () => {
+      return {
+        items: [
+          { name: 'item 1', selected: false },
+          { name: 'item 2', selected: true }
+        ]
+      }
+    }
+  }))
+  .add('Slotted', () => ({
+    components: { KDropdown },
+    template: `<div>
+     <KDropdown :items="items" toggle-text="Select Items">
+        <template slot-scope="{ item }">
+          <a :href="item.path">{{ item.name }}</a>
+        </template>
+      </KDropdown>
+    </div>
+    `,
+    data: () => {
+      return {
+        items: [
+          { name: 'item 1', selected: false, id: 1, path: '/services' },
+          { name: 'item 2', selected: true, id: 2, path: '/routes' }
+        ]
+      }
+    }
+  }))
   .add('Actions', () => ({
     components: { KDropdown },
     template: `
-    <div style="margin-left: 50px">
-      <KDropdown
-        :items="['item 1', 'item 2', 'item 3']"
-        toggle-text="Cool Actions"
-        position="left">
-        <template slot="item-1">
-          <span @click="onClick">Click me!</span>
-        </template>
-      </KDropdown>
-    </div>`,
+    <KDropdown
+      :items="['item 1', 'item 2', 'item 3']"
+      toggle-text="Cool Actions">
+      <template slot="item-1">
+        <span @click="onClick">Click me!</span>
+      </template>
+    </KDropdown>
+    `,
     methods: {
       onClick (e) {
         action('onClick')(e)
--- packages/KDropdown/KDropdown.vue
+++ packages/KDropdown/KDropdown.vue
@@ -1,33 +1,42 @@
 <template>
   <div
-    :class="{ 'no-toggle-icon': !toggleIcon }"
+    :class="{ 'no-toggle-icon': !showToggleIcon, 'is-open': isOpen, 'has-icon': hasIcon }"
     class="k-dropdown">
-    <span
-      id="dropdown"
+    <!-- Overlay to click away -->
+    <div
+      v-if="isOpen"
+      class="overlay"
+      @click="isOpen = !isOpen"/>
+    <KButton
+      :appearance="buttonType === 'btn' ? 'secondary' : 'btn-link'"
+      aria-haspopup="true"
       class="k-dropdown-toggle"
-      aria-haspopup="listbox">
-      <slot name="toggle-text">{{ toggleText }}</slot>
-    </span>
+      @click="() => isOpen = !isOpen">
+      <slot name="icon" />
+      <span>{{ toggleText }}</span>
+    </KButton>
     <ul
-      :class="[`k-dropdown-menu-${position}`, ulClasses]"
+      :class="`k-dropdown-menu-${position}`"
       class="k-dropdown-menu"
       role="listbox"
       aria-labelledby="dropdown">
       <li
-        v-for="(item, idx) in items"
-        :key="idx"
-        :id="item.replace(' ', '-')"
-        :class="liClasses"
-        role="option"
+        v-for="item in items"
+        :key="item && item.name || item"
+        :id="item && item.name || item"
+        :class="{ 'is-selected': item.selected }"
         class="k-dropdown-item"
-        v-on="$listeners">
-        <slot :name="item.replace(' ', '-')">{{ item }}</slot>
+        role="option"
+        @click="clickHandler(item)">
+        <slot :item="item">{{ item && item.name || item }}</slot>
       </li>
     </ul>
   </div>
 </template>
 
 <script>
+import KButton from '@kongponents/kbutton'
+
 export const alignments = {
   left: 'left',
   center: 'center',
@@ -36,7 +45,12 @@ export const alignments = {
 
 export default {
   name: 'KDropdown',
+  components: { KButton },
+
   props: {
+    /**
+     * Align dropdown menu to left, center or right
+     */
     position: {
       type: String,
       default: 'left',
@@ -44,23 +58,39 @@ export default {
         return Object.values(alignments).indexOf(value) !== -1
       }
     },
+    /**
+     * Button text
+     */
     toggleText: {
       type: String,
       default: ''
     },
-    toggleIcon: {
+    /**
+     * Style as button or text link
+     */
+    buttonType: {
+      type: String,
+      default: 'btn',
+      validator: function (value) {
+        return [
+          'btn',
+          'text'
+        ].indexOf(value) !== -1
+      }
+    },
+    /**
+     * Show dropdown arrow
+     */
+    showToggleIcon: {
       type: Boolean,
       default: true
     },
+    /**
+     * Array of items to render in menu. Can pass array of Strings or Objects
+     * String: ['item', 'item', 'item]
+     * Object: [ { name: 'item', selected: false }, { name: 'item'} ]
+     */
     items: {
-      type: Array,
-      default: () => []
-    },
-    ulClasses: {
-      type: Array,
-      default: () => []
-    },
-    liClasses: {
       type: Array,
       default: () => []
     }
@@ -68,55 +98,87 @@ export default {
 
   data () {
     return {
-      activeItem: this.showActive ? this.items[0] : ''
+      isOpen: false
     }
   },
 
+  computed: {
+    hasIcon () {
+      return !!this.$slots.icon
+    }
+  },
+
+  mounted () {
+    this.hasIcon && this.$slots.icon[0].elm.setAttribute('viewBox', '0 0 16 16')
+  },
+
   methods: {
-    setActiveItem (item) {
-      this.activeItem = item
+    clickHandler (item) {
+      // Only run if item has selected key
+      if (Object.keys(item).includes('selected')) {
+        item.selected = !item.selected
+        this.$emit('selected', item)
+      }
     }
   }
 }
 </script>
 
-<style>
+<style scoped>
+.overlay {
+  position: fixed;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  left: 0;
+}
+
 .k-dropdown {
   position: relative;
   display: inline-block;
 }
-.k-dropdown:hover .k-dropdown-menu {
-  display: block;
-}
-.k-dropdown .k-dropdown-toggle {
-  position: relative;
-  cursor: pointer;
+
+  /* Toggle button arrow */
+.k-dropdown.has-icon .k-dropdown-toggle > span:not(:empty) {
+  margin-left: var(--spacing-xs);
 }
 .k-dropdown:not(.no-toggle-icon) .k-dropdown-toggle:after {
   display: inline-block;
   width: 0;
   height: 0;
-  margin-left: 0.3em;
+  margin-left: var(--spacing-xs);
   vertical-align: middle;
   content: "";
-  border-top: 0.3em solid;
-  border-right: 0.3em solid transparent;
-  border-left: 0.3em solid transparent;
+  border-top: 0.325em solid;
+  border-right: 0.325em solid transparent;
+  border-left: 0.325em solid transparent;
+}
+.k-dropdown.is-open:not(.no-toggle-icon) .k-dropdown-toggle:after {
+  transform: rotate(180deg);
+}
+
+/* Dropdown menu */
+.k-dropdown.is-open .k-dropdown-menu {
+  display: block;
 }
 .k-dropdown .k-dropdown-menu {
   display: none;
   position: absolute;
   right: 0;
   left: auto;
-  min-width: 10rem;
+  min-width: 100px;
+  max-width: 300px;
+  width: auto;
   margin: 0;
-  padding: 0.5rem 0;
+  padding: var(--spacing-xs) 0;
   list-style: none;
   border-radius: 3px;
   background: #fff;
-  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.15);
+  border: 1px solid var(--grey-88);
+  box-shadow: 0 2px 12px rgba(0, 0, 0, 0.15);
   z-index: 9999;
 }
+/* Dropdown menu alignment */
 .k-dropdown .k-dropdown-menu-left {
   left: 0;
   right: auto;
@@ -131,15 +193,64 @@ export default {
   transform: translateX(-50%);
   -webkit-transform: translateX(-50%);
 }
+
+/* Menu items */
 .k-dropdown .k-dropdown-menu .k-dropdown-item {
   display: block;
-  padding: 0.5rem 1.5rem;
+  padding: 0 var(--spacing-md);
   white-space: nowrap;
-  color: inherit;
+  color: var(--tblack-70);
+  line-height: 2;
+  white-space: nowrap;
+  overflow: hidden;
+  text-overflow: ellipsis;
+  transition: all 200ms ease-in;
+}
+.k-dropdown .k-dropdown-menu .k-dropdown-item.is-selected {
+  background-image: url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' width='16' height='13'%3E%3Cpath fill='%2366BFFF' fill-rule='evenodd' d='M4.5 10L13.7968194.7031806c.3883559-.3883559 1.021292-.3850692 1.4092576.0028964l.087846.087846c.3899555.3899555.3864986 1.0256554.0040118 1.4081422L5.2020652 12.2979348c-.3877399.3877399-1.0256941.3784363-1.4051902-.0010598l-3.09375-3.09375C.3147998 8.8147998.3181114 8.1818886.706077 7.793923l.087846-.087846c.3899555-.3899555 1.0271296-.3850244 1.4170351.0048811L4.5 10z'/%3E%3C/svg%3E");
+  background-position: var(--spacing-sm) 50%;
+  background-repeat: no-repeat;
+  padding-left: var(--spacing-xl);
+  color: var(--blue-link);
+}
+.k-dropdown .k-dropdown-menu .k-dropdown-item:hover,
+.k-dropdown .k-dropdown-menu .k-dropdown-item.active {
+  background-color: var(--blue-lightest);
+  cursor: pointer;
+  transition: all 200ms ease-out;
 }
 .k-dropdown .k-dropdown-menu .k-dropdown-item a,
 .k-dropdown .k-dropdown-menu .k-dropdown-item > div > a {
   color: inherit;
   display: block;
 }
+</style>
+
+<style>
+/* Toggle button */
+.k-dropdown .k-dropdown-toggle.btn-link,
+.k-dropdown .k-dropdown-toggle.btn-link:hover {
+  color: inherit;
+}
+.k-dropdown.is-open .k-dropdown-toggle {
+  background-color: var(--secondaryActive);
+}
+.k-dropdown.is-open .k-dropdown-toggle.btn-link {
+  background-color: transparent;
+}
+.k-dropdown.has-icon .k-dropdown-toggle {
+  display: flex;
+  align-items: center;
+}
+.k-dropdown .k-dropdown-toggle:focus,
+.k-dropdown .k-dropdown-toggle:active {
+  outline: 0;
+}
+
+/* Toggle button icon */
+.k-dropdown .k-dropdown-toggle > svg {
+  width: 16px;
+  height: 16px;
+}
+
 </style>
