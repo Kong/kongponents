06e7f3f41e33af7d931ea98d32602fbd469c372f
Travis Amaral <travis@konghq.com>
2019-05-07 10:31:21 -0700

    tests(KDropdown) Add tests

--- packages/KDropdown/KDropdown.spec.js
+++ packages/KDropdown/KDropdown.spec.js
@@ -1,5 +1,6 @@
-import { mount } from '@vue/test-utils'
+import { mount, shallowMount } from '@vue/test-utils'
 import { default as KDropdown, alignments } from './KDropdown.vue'
+import KIcon from '../KIcon/KIcon.vue'
 
 const Alignments = Object.keys(alignments)
 
@@ -12,13 +13,41 @@ let rendersAlignments = (position) => {
     })
 
     expect(wrapper.find('.k-dropdown-menu').html()).toEqual(expect.stringContaining(`k-dropdown-menu-${position}`))
-    expect(wrapper.html()).toMatchSnapshot()
   })
 }
 
 describe('KDropdown', () => {
   Alignments.map(position => {
     rendersAlignments(position)
+  })
+
+  it('renders icon with correct viewbox', () => {
+    const icon = {
+      render (h) {
+        return h(KIcon, { props: { icon: 'gear' } })
+      }
+    }
+    const wrapper = mount(KDropdown, {
+      slots: {
+        'icon': icon
+      }
+    })
+
+    expect(wrapper.find('.k-dropdown-toggle svg').attributes().viewBox).toBe('0 0 16 16')
+    expect(wrapper.find('.k-dropdown').classes('has-icon')).toBe(true)
+  })
+
+  it('correctly adds active class', () => {
+    const wrapper = mount(KDropdown, {
+      propsData: {
+        items: [
+          { name: 'Item 1', selected: true },
+          { name: 'Item 2', selected: false }
+        ]
+      }
+    })
+
+    expect(wrapper.find('.k-dropdown-menu > li:first-child').classes('is-selected')).toBe(true)
   })
 
   it('matches snapshot', () => {
