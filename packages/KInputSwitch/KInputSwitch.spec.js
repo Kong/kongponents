cd949204c2ff76c23a93cdcfba3e7aae5d26585e
Travis Amaral <travis@konghq.com>
2019-07-24 19:29:08 -0700

    tests(Switch) Add tests for switch component

---
+++ packages/KInputSwitch/KInputSwitch.spec.js
@@ -0,0 +1,30 @@
+import { mount } from '@vue/test-utils'
+import KInputSwitch from '@/KInputSwitch/KInputSwitch'
+
+describe('KInputSwitch', () => {
+  it('shows as checked when prop passed', () => {
+    const wrapper = mount(KInputSwitch, {
+      propsData: {
+        isChecked: true
+      }
+    })
+
+    expect(wrapper.find('input').element.checked).toBe(true)
+  })
+
+  it('emits checked value on click', () => {
+    const wrapper = mount(KInputSwitch)
+    const toggle = wrapper.find('input')
+
+    toggle.trigger('click')
+
+    expect(wrapper.emitted().change).toBeTruthy()
+    expect(wrapper.emitted().change).toHaveLength(1)
+  })
+
+  it('matches snapshot', () => {
+    const wrapper = mount(KInputSwitch)
+
+    expect(wrapper.html()).toMatchSnapshot()
+  })
+})
