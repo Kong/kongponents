d31e2f28733206ff4549c5ad262f17c479a31a56
Darren Jennings <dmjenn02@gmail.com>
2019-07-31 09:01:09 -0400

    docs(vuepress) fix github link

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -6,7 +6,6 @@ module.exports = {
   themeConfig: {
     repo: 'kong/kongponents',
     logo: 'https://2tjosk2rxzc21medji3nfn1g-wpengine.netdna-ssl.com/wp-content/uploads/2018/08/kong-logomark-color-64px.png',
-    docsDir: 'vuepress',
     editLinks: true,
     sidebarDepth: 0,
     search: false,
