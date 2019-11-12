4d91db41799575ca99240c370c066a35b09b6963
Travis Amaral <travis@konghq.com>
2019-11-12 14:36:47 -0800

    tests(KRadio) Add tests

--- packages/KRadio/KRadio.spec.js
+++ packages/KRadio/KRadio.spec.js
@@ -1,9 +1,34 @@
 import { mount } from '@vue/test-utils'
-import KInputRadio from '@/KRadio/KRadio'
+import KRadio from '@/KRadio/KRadio'
 
-describe('KInputRadio', () => {
+describe('KRadio', () => {
+  it('shows as checked when value true', () => {
+    const wrapper = mount(KRadio, {
+      propsData: {
+        model: true,
+        value: true
+      }
+    })
+
+    expect(wrapper.find('input').element.checked).toBe(true)
+  })
+
+  it('emits checked value on click', () => {
+    const wrapper = mount(KRadio, {
+      propsData: {
+        model: false
+      }
+    })
+    const toggle = wrapper.find('input')
+
+    toggle.trigger('click')
+
+    expect(wrapper.emitted().change).toBeTruthy()
+    expect(wrapper.emitted().change).toHaveLength(1)
+  })
+
   it('matches snapshot', () => {
-    const wrapper = mount(KInputRadio)
+    const wrapper = mount(KRadio)
 
     expect(wrapper.html()).toMatchSnapshot()
   })
