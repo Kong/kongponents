5edb3c8deb83d765cf0fa8b2a228c2d29d710238
Travis Amaral <travis@konghq.com>
2019-11-14 16:05:08 -0800

    tests(ktabs): add tests & snapshots

--- packages/KTabs/KTabs.spec.js
+++ packages/KTabs/KTabs.spec.js
@@ -1,9 +1,56 @@
 import { mount } from '@vue/test-utils'
 import KTabs from '@/KTabs/KTabs'
 
+const TABS = [
+  { hash: '#pictures', title: 'Pictures' },
+  { hash: '#movies', title: 'Movies' },
+  { hash: '#books', title: 'Books' }
+]
+
 describe('KTabs', () => {
+  it('first tab is set if hash not found', () => {
+    window.location.hash = 'wow'
+    const wrapper = mount(KTabs, {
+      propsData: {
+        tabs: TABS
+      }
+    })
+
+    expect(wrapper.find('.tab-item').classes()).toContain('active')
+  })
+
+  it('correct tab is set if hash is found', () => {
+    window.location.hash = 'books'
+    const wrapper = mount(KTabs, {
+      propsData: {
+        tabs: TABS
+      }
+    })
+
+    const booksTab = wrapper.findAll('.tab-item')
+
+    expect(booksTab.at(2).classes()).toContain('active')
+  })
+
+  it('emits change event on click', () => {
+    const wrapper = mount(KTabs, {
+      propsData: {
+        tabs: TABS
+      }
+    })
+
+    wrapper.findAll('.tab-item').at(1).trigger('click')
+
+    expect(wrapper.emitted().changed).toHaveLength(1)
+    expect(wrapper.emitted().changed).toEqual([['#movies']])
+  })
+
   it('matches snapshot', () => {
-    const wrapper = mount(KTabs)
+    const wrapper = mount(KTabs, {
+      propsData: {
+        tabs: TABS
+      }
+    })
 
     expect(wrapper.html()).toMatchSnapshot()
   })
