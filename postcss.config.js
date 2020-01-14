6712ae9941bfb01f8a5db81a26c95ac459caa6ac
jeffyongtaotang <jeff.tong@konghq.com>
2020-01-14 14:01:06 -0800

    chore(*) fix config

--- postcss.config.js
+++ postcss.config.js
@@ -4,7 +4,7 @@ const config = require('./postcss-custom-properties.config')
 module.exports = () => ({
   plugins: {
     'postcss-custom-properties': {
-      ...config,
+      ...config.plugins['postcss-custom-properties'],
       importFrom: path.resolve(__dirname, 'packages/styles/styles.css')
     }
   }
