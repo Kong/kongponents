0d677b8241addda33f33291f91a8e66b49355973
Travis Amaral <travis@konghq.com>
2020-05-19 11:16:31 -0700

    tests(KMultiselect) Add filter, update snapshots

--- packages/KMultiselect/KMultiselect.spec.js
+++ packages/KMultiselect/KMultiselect.spec.js
@@ -17,7 +17,7 @@ describe('KMultiselect', () => {
     expect(wrapper.find('.k-multiselect-menu > li:first-child').classes('is-selected')).toBe(true)
   })
 
-  it('emits proper updates on apply', async () => {
+  it('emits proper updates on apply', () => {
     const wrapper = mount(KMultiselect, {
       propsData: DEFAULT_PROPS
     })
@@ -26,10 +26,23 @@ describe('KMultiselect', () => {
     wrapper.find('.k-multiselect-menu > li:last-child').trigger('click')
     wrapper.find('.k-multiselect-apply').trigger('click')
 
-    await wrapper.vm.$nextTick()
-
     expect(wrapper.emitted().changes).toBeTruthy()
     expect(wrapper.emitted().changes[0][0][1]).toEqual({ label: 'Item 2', selected: true })
+  })
+
+  it('filters list items', () => {
+    const wrapper = mount(KMultiselect, {
+      propsData: {
+        ...DEFAULT_PROPS,
+        hasFilter: true
+      }
+    })
+
+    wrapper.find('.k-multiselect-trigger').trigger('click')
+    wrapper.find('.k-multiselect-filter input').setValue('2')
+
+    expect(wrapper.findAll('.k-multiselect-menu li')).toHaveLength(1)
+    expect(wrapper.findAll('.k-multiselect-menu li').at(0).text()).toEqual('Item 2')
   })
 
   it('matches snapshot', () => {
