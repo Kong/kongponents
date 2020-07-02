50d0cf2b3d9c357213b0df43fa31e876fdfd95a5
Jack Kavanagh <jackkav@gmail.com>
2020-07-02 16:58:14 +0200

    feat(icon) brain immunity

--- packages/KIcon/icons/index.js
+++ packages/KIcon/icons/index.js
@@ -1,37 +1,40 @@
+import back from './icn-back.svg'
+import brain from './icn-brain.svg'
+import close from './icn-close.svg'
 import collapseExpand from './icn-collapse-expand.svg'
+import connections from './icn-connections.svg'
+import dangerCircle from './icn-danger-circle.svg'
+import dashboard from './icn-dashboard.svg'
+import externalLink from './icn-external-link.svg'
+import featureRequest from './icn-feature-request.svg'
 import gateway from './icn-gateway.svg'
 import gear from './icn-gear.svg'
+import gearFilled from './icn-gear-filled.svg'
+import immunity from './icn-immunity.svg'
 import info from './icn-info.svg'
+import lock from './icn-lock.svg'
+import notificationBell from './icn-notification-bell.svg'
+import people from './icn-people.svg'
 import portal from './icn-dev-portal.svg'
-import security from './icn-security.svg'
-import workspaces from './icn-workspaces.svg'
-import workspacesCollapsed from './icn-collapse-workspaces.svg'
-import vitals from './icn-vitals.svg'
-import back from './icn-back.svg'
+import portalV2 from './icn-dev-portal-v2.svg'
+import portalV3 from './icn-portal-v3.svg'
+import profile from './icn-profile.svg'
 import search from './icn-search.svg'
-import trash from './icn-trash.svg'
-import dashboard from './icn-dashboard.svg'
-import support from './icn-support.svg'
-import featureRequest from './icn-feature-request.svg'
-import externalLink from './icn-external-link.svg'
+import security from './icn-security.svg'
+import services from './icn-services.svg'
 import spinner from './icn-spinner.svg'
-import dangerCircle from './icn-danger-circle.svg'
+import stackedCards from './icn-stacked-cards.svg'
+import support from './icn-support.svg'
+import trash from './icn-trash.svg'
+import vitals from './icn-vitals.svg'
 import warning from './icn-warning.svg'
 import warning2 from './icn-warning2.svg'
-import lock from './icn-lock.svg'
-import close from './icn-close.svg'
-import portalV2 from './icn-dev-portal-v2.svg'
-import people from './icn-people.svg'
-import stackedCards from './icn-stacked-cards.svg'
-import profile from './icn-profile.svg'
-import connections from './icn-connections.svg'
-import portalV3 from './icn-portal-v3.svg'
-import gearFilled from './icn-gear-filled.svg'
-import services from './icn-services.svg'
-import notificationBell from './icn-notification-bell.svg'
+import workspaces from './icn-workspaces.svg'
+import workspacesCollapsed from './icn-collapse-workspaces.svg'
 
 export default {
   back,
+  brain,
   close,
   collapseExpand,
   connections,
@@ -41,6 +44,7 @@ export default {
   gateway,
   gear,
   gearFilled,
+  immunity,
   info,
   lock,
   notificationBell,
