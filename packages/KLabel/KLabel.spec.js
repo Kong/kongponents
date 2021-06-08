d6b86d109712a87da80434270c5cf1fe7ec19e1b
Michael Heap <m@michaelheap.com>
2021-06-08 15:39:42 +0100

    feat(klabel): add help property with icon + tooltip (#306)
    Some inputs require additional context to explain what the user should provide. This feature uses KIcon to add a help icon and KoolTip to show a tooltip if the help property is provided. Also adds support for an info prop that displays the info icon.
    
    KoolTip uses the prop label to provide the text. I have chosen help in this component so that there is no confusion between the KLabel slot and the KoolTip prop.

---
+++ packages/KLabel/KLabel.spec.js
@@ -0,0 +1,40 @@
+import { mount } from '@vue/test-utils'
+import KLabel from '@/KLabel/KLabel'
+
+describe('KLabel', () => {
+  it('renders a plain label by default', () => {
+    const wrapper = mount(KLabel, {
+      slots: {
+        default: 'Full Name'
+      }
+    })
+
+    expect(wrapper.find('label').text()).toBe('Full Name')
+  })
+
+  it('renders a tooltip when `help` is provided', () => {
+    const wrapper = mount(KLabel, {
+      propsData: {
+        help: 'This is a tooltip'
+      },
+      slots: {
+        default: 'Full Name'
+      }
+    })
+
+    expect(wrapper.html()).toMatchSnapshot()
+  })
+
+  it('renders a tooltip when `info` is provided', () => {
+    const wrapper = mount(KLabel, {
+      propsData: {
+        info: 'This is a tooltip'
+      },
+      slots: {
+        default: 'Full Name'
+      }
+    })
+
+    expect(wrapper.html()).toMatchSnapshot()
+  })
+})
