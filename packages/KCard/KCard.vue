772c97caa1eead58b9cc08aaad30f92b34b457a2
Dustin Ryerson <dpryerson@gmail.com>
2021-06-28 13:41:06 -0700

    style(KCard): lint

--- packages/KCard/KCard.vue
+++ packages/KCard/KCard.vue
@@ -17,7 +17,9 @@
         <slot name="actions"/>
       </div>
     </div>
-    <div v-if="helpText || $scopedSlots.helpText" class="k-card-help-text">
+    <div
+      v-if="helpText || $scopedSlots.helpText"
+      class="k-card-help-text">
       <!-- @slot Use this slot to pass help text under the title -->
       <slot name="helpText">
         <span>{{ helpText }}</span>
