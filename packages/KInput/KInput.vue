8fc447ae58f5b870ab807bd4ce6bdd4ea6fa42d3
kai.arrowood <kai.arrowood@konghq.com>
2021-07-16 15:13:17 -0400

    feat(KInput) fun with labels

--- packages/KInput/KInput.vue
+++ packages/KInput/KInput.vue
@@ -1,9 +1,37 @@
 <template>
-  <input
-    :value="value"
-    class="form-control k-input"
-    @input="e => $emit('input', e.target.value)"
-    v-on="listeners">
+  <div class="k-input-wrapper">
+    <input
+      v-if="!label"
+      :value="value"
+      v-bind="attrs"
+      class="form-control k-input"
+      @input="e => $emit('input', e.target.value)"
+      v-on="listeners">
+
+    <div
+      v-else
+      class="col-md-4 mt-5">
+      <div class="text-on-input">
+        <label :class="{ focused: isFocused }">
+          {{ label }}
+        </label>
+        <input
+          :value="value"
+          v-bind="attrs"
+          class="form-control k-input"
+          @input="e => $emit('input', e.target.value)"
+          @focus="() => isFocused = true"
+          @blur="() => isFocused = false"
+          v-on="listeners">
+      </div>
+    </div>
+
+    <p
+      v-if="help"
+      class="help">
+      {{ help }}
+    </p>
+  </div>
 </template>
 
 <script>
@@ -13,9 +41,25 @@ export default {
     value: {
       type: String,
       default: ''
+    },
+    label: {
+      type: String,
+      default: ''
+    },
+    help: {
+      type: String,
+      default: ''
+    }
+  },
+  data () {
+    return {
+      isFocused: false
     }
   },
   computed: {
+    attrs () {
+      return this.$attrs
+    },
     listeners () {
       const listeners = { ...this.$listeners }
 
@@ -31,4 +75,49 @@ export default {
 <style scoped lang="scss">
 @import '~@kongponents/styles/_variables.scss';
 @import '~@kongponents/styles/forms/_inputs.scss';
+
+.text-on-input {
+  position: relative;
+
+  .focused {
+    color: var(--KInputLabelColor, var(--KInputBorder, var(--blue-500)));
+  }
+
+  label {
+    position: absolute;
+    top: -8px;
+    left: 13px;
+    padding: 2px;
+    z-index: 1;
+
+    font-size: 11px;
+    font-weight: 500;
+    color: var(--KInputBorder, var(--gray-300));
+    background-color: var(--KInputBackground, var(--white));
+    display: inline-block;
+    margin-bottom: .5rem;
+  }
+
+  &:after {
+    content: " ";
+    background-color: var(--white);
+    width: 100%;
+    height: 13px;
+    position: absolute;
+    left: 0;
+    bottom: 0;
+    z-index: -1;
+  }
+}
+
+.form-control {
+  box-shadow: none !important;
+}
+
+.help {
+  display: block;
+  margin: var(--spacing-xs, spacing(xs)) 0 0;
+  font-size: var(--type-sm, type(sm));
+  color: var(--black-45, color(black-45));
+}
 </style>
