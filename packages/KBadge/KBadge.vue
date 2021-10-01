700f1a218b050305afec24f78f3e9c6a4a1ddf10
Adrian Goławski <adrian.golawski@gmail.com>
2021-10-01 15:49:59 +0200

    chore(kbadge): redesigning KBadge (#364)
    Add new "info" appearance for badge
    Add round and rectangular shape for badge
    Add variables for line height
    Update test snapshots

--- packages/KBadge/KBadge.vue
+++ packages/KBadge/KBadge.vue
@@ -1,6 +1,6 @@
 <template>
   <div
-    :class="[appearance !== 'custom' && `kbadge-${appearance}`]"
+    :class="[ `kbadge-${appearance}`, `kbadge-${shape}`]"
     :style="color && backgroundColor && {backgroundColor, color}"
     class="k-badge truncate"
   >
@@ -13,7 +13,14 @@ export const appearances = {
   default: 'default',
   success: 'success',
   danger: 'danger',
-  warning: 'warning'
+  info: 'info',
+  warning: 'warning',
+  custom: 'custom'
+}
+
+export const shapes = {
+  rounded: 'rounded',
+  rectangular: 'rectangular'
 }
 
 export default {
@@ -26,9 +33,18 @@ export default {
       type: String,
       required: false,
       validator: function (value) {
-        return Object.keys({...appearances, 'custom': ''}).indexOf(value) !== -1
+        return Object.keys({...appearances}).indexOf(value) !== -1
       },
       default: 'default'
+    },
+
+    shape: {
+      type: String,
+      required: false,
+      validator: function (value) {
+        return Object.keys({...shapes}).indexOf(value) !== -1
+      },
+      default: 'rounded'
     },
 
     color: {
@@ -53,35 +69,48 @@ export default {
   display: inline-block;
   font-weight: 500;
   vertical-align: top;
-  font-size: var(--KBadgeFontSize, var(--type-sm, type(sm)));
+  font-size: var(--KBadgeFontSize, var(--type-xxs, type(xxs)));
+  line-height: var(--KBadgeLineHeight, var(--type-md, type(md)));
   height: auto;
   text-align: center;
   max-width: var(--KBadgeMaxWidth, 200px);
   width: var(--KBadgeWidth, auto);
-  padding: var(--KBadgePaddingY, 2px) var(--KBadgePaddingX, var(--spacing-sm));
+  padding: var(--KBadgePaddingY, 2px) var(--KBadgePaddingX, 6px);
   font-family: var(--font-family-sans, font(sans));
-  border-radius: var(--KBadgeBorderRadius, 25px);
 
   // Appearances
   &.kbadge-default {
-    color: var(--KBadgeDefaultColor, var(--steel-600, color(steel-600)));
-    border-color: var(--KBadgeDefaultBorder, var(--steel-200, color(steel-200)));
-    background-color: var(--KBadgeDefaultBackground, var(--steel-200, color(steel-200)));
+    color: var(--KBadgeDefaultColor, var(--blue-500, color(blue-500)));
+    border-color: var(--KBadgeDefaultBorder, var(--blue-500, color(blue-500)));
+    background-color: var(--KBadgeDefaultBackground, var(--grey-100, color(grey-100)));
   }
   &.kbadge-success {
     color: var(--KBadgeSuccessColor, var(--green-500, color(green-500)));
-    border-color: var(--KBadgeSuccessBorder, var(--green-200, color(green-200)));
-    background-color: var(--KBadgeSuccessBackground, var(--green-200, color(green-200)));
+    border-color: var(--KBadgeSuccessBorder, var(--green-500, color(green-500)));
+    background-color: var(--KBadgeSuccessBackground, var(--green-100, color(green-100)));
   }
   &.kbadge-danger {
-    color: var(--KBadgeDangerColor, var(--red-600, color(red-600)));
-    border-color: var(--KBadgeDangerBorder, var(--red-200, color(red-200)));
-    background-color: var(--KBadgeDangerBackground, var(--red-200, color(red-200)));
+    color: var(--KBadgeDangerColor, var(--red-700, color(red-700)));
+    border-color: var(--KBadgeDangerBorder, var(--red-700, color(red-700)));
+    background-color: var(--KBadgeDangerBackground, var(--red-100, color(red-100)));
+  }
+  &.kbadge-info {
+    color: var(--KBadgeInfoColor, var(--petrol-200, color(petrol-200)));
+    border-color: var(--KBadgeInfoBorder, var(--petrol-200, color(petrol-200)));
+    background-color: var(--KBadgeInfoBackground, var(--petrol-100, color(petrol-100)));
   }
   &.kbadge-warning {
     color: var(--KBadgeWarningColor, var(--yellow-500, color(yellow-500)));
-    border-color: var(--KBadgeWarningBorder, var(--yellow-200, color(yellow-200)));
-    background-color: var(--KBadgeWarningBackground, var(--yellow-200, color(yellow-200)));
+    border-color: var(--KBadgeWarningBorder, var(--yellow-500, color(yellow-500)));
+    background-color: var(--KBadgeWarningBackground, var(--yellow-100, color(yellow-100)));
+  }
+
+  &.kbadge-rectangular {
+    border-radius: var(--KBadgeBorderRadius, 4px);
+  }
+
+  &.kbadge-rounded {
+    border-radius: var(--KBadgeBorderRadius, 25px);
   }
 }
 </style>
