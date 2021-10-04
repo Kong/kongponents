1b08aeb01cc7e5056a1f1e9afd4669b3a8082f1e
Kai Arrowood <kai.arrowood@konghq.com>
2021-10-04 17:35:36 -0400

    fix(kcard): pr suggestion
    Co-authored-by: Dustin Ryerson <dpryerson@gmail.com>

--- packages/KCard/KCard.vue
+++ packages/KCard/KCard.vue
@@ -39,7 +39,7 @@
       </div>
       <div
         v-if="$scopedSlots.notifications"
-        class="k-card-notifications pl-3">
+        class="k-card-notifications ml-3">
         <slot name="notifications" />
       </div>
     </div>
