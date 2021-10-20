7d7218dcfdca52fca45cb0203d82b074a065e5fa
Kai Arrowood <kai.arrowood@konghq.com>
2021-10-20 15:45:31 -0400

    feat(tailwind): add tailwind (#376)
    Add tailwind support.

--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,11 @@
 const path = require('path')
+const autoprefixer = require('autoprefixer')
+const tailwindcss = require('tailwindcss')
 
 module.exports = () => ({
   plugins: {
+    tailwindcss,
+    autoprefixer,
     'postcss-custom-properties': {
       preserve: true,
       importFrom: path.resolve(__dirname, 'packages/styles/styles.css')
---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  purge: [
+    './packages/**/*.vue'
+  ],
+  theme: {
+    extend: {
+      screens: {
+        '2xl': '1390px'
+      },
+      maxWidth: {
+        'screen-2xl': '1390px'
+      }
+    }
+  },
+  variants: {},
+  plugins: []
+}
