f1b05132955f7c2256080ffa143e5014e5db0445
kai.arrowood <kai.arrowood@konghq.com>
2021-10-20 10:29:16 -0900

    chore(kicon): add title attribute

--- packages/KIcon/KIcon.vue
+++ packages/KIcon/KIcon.vue
@@ -8,7 +8,7 @@
     class="kong-icon"
     role="img"
   >
-    <title v-if="!hideTitle">{{ title || icon }}</title>
+    <title v-if="!hideTitle">{{ titleText }}</title>
     <slot name="svgElements"/>
     <g>
       <template v-for="(elem, idx) in fills">
@@ -100,6 +100,20 @@ export default {
   },
 
   computed: {
+    titleText () {
+      if (this.title) { // use title prop if they provided
+        return this.title
+      }
+
+      const titleElems = this.doc.getElementsByTagName('title')
+      if (titleElems.length) { // use title in SVG if it exists
+        return titleElems[0].innerHTML
+      }
+
+      const icnName = this.icon.split(/(?=[A-Z])/).join(' ') // split on capital letters in icon name and add space
+
+      return this.convertToTitleCase(icnName)
+    },
     iconSVG () {
       return icons[this.icon]
     },
@@ -150,6 +164,12 @@ export default {
   beforeMount () {
     // Do not render KIcon until client is available
     this.isSSR = true
+  },
+
+  methods: {
+    convertToTitleCase (str) {
+      return str.split('-').map(i => i.charAt(0).toUpperCase() + i.substring(1)).join(' ')
+    }
   }
 }
 </script>
