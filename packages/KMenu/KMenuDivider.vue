ff9ee9374e7aaab55dbe571e2c8383f2979b1261
Aanchal Mishra <87779967+aanchalm01@users.noreply.github.com>
2021-11-05 08:56:43 -0400

    feat(kmenu): add new kongponent (#370)
    Create new `KMenu` kongponent.
    
    Changes made:
    Add new component `KMenu`, and child component `KMenuItem`
    Supports use of `items` prop or `body` slot which can instantiate `KMenuItem`
    Add docs
    Add tests

---
+++ packages/KMenu/KMenuDivider.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="k-menu-item-divider">
+    <hr >
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'KMenuDivider'
+}
+</script>
+
+<style lang="scss" scoped>
+.k-menu-item-divider{
+  padding: 0 19px 0 19px;
+}
+</style>
