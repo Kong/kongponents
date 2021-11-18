2a29ab5d870fe53b2fcc60d3d7fd81430ef209a5
kai.arrowood <kai.arrowood@konghq.com>
2021-11-18 17:49:14 -0500

    chore(kcardcatalog): typo

--- packages/KCardCatalog/index.js
+++ packages/KCardCatalog/index.js
@@ -1,15 +1,15 @@
 import KCardCatalog from './KCardCatalog'
-import KCardCatalogItem from './KCardCatalogItem'
+import KCatalogItem from './KCatalogItem'
 
 const Plugin = {
   install (Vue) {
     Vue.component('KCardCatalog', KCardCatalog)
-    Vue.component('KCardCatalogItem', KCardCatalogItem)
+    Vue.component('KCatalogItem', KCatalogItem)
   }
 }
 
 export default Plugin
-export { KCardCatalog, KCardCatalogItem }
+export { KCardCatalog, KCatalogItem }
 
 if (typeof window !== 'undefined' && window.Vue) {
   window.Vue.use(Plugin)
