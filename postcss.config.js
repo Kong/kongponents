128995790945b39d192dd1a054b4bd231cd1dcc2
kai.arrowood <kai.arrowood@konghq.com>
2021-11-19 08:52:22 -0500

    chore(*): styles bundling

--- postcss.config.js
+++ postcss.config.js
@@ -1,14 +1,13 @@
 const path = require('path')
-const autoprefixer = require('autoprefixer')
-const tailwindcss = require('tailwindcss')
 
-module.exports = () => ({
-  plugins: {
-    tailwindcss,
-    autoprefixer,
-    'postcss-custom-properties': {
+module.exports = {
+  plugins: [
+    require('postcss-import'),
+    require('tailwindcss'),
+    require('autoprefixer'),
+    require('postcss-custom-properties')({
       preserve: true,
       importFrom: path.resolve(__dirname, 'packages/styles/styles.css')
-    }
-  }
-})
+    })
+  ]
+}
