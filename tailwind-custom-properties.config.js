b97971edc114574e8fdf7a552fc8c4c57328b95b
Adam DeHaven <adam.dehaven@konghq.com>
2021-11-22 12:22:16 -0500

    fix(*): fix deps and build process
    Update build process to copy necessary config files in a separate step,
    leave them in place until build:cli finishes, then remove, as other steps
    in the build process will fail if postcss.config.js files are not
    left in place.
    
    Update @vue/composition-api to be a peerDependency.
    
    Add docs info on KTable and KCardCatalog for how to use with
    Composition API.

---
+++ tailwind-custom-properties.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  purge: [
+    './**/*.vue'
+  ],
+  theme: {
+    extend: {
+      screens: {
+        '2xl': '1390px'
+      },
+      maxWidth: {
+        'screen-2xl': '1390px'
+      }
+    }
+  },
+  variants: {},
+  plugins: []
+}
