3e2c73344df17a353e2821490f86ea91970748fd
Adam DeHaven <adam.dehaven@konghq.com>
2021-11-30 20:54:11 -0500

    fix(kicon): add raw-loader as dev dep
    Update KIcon package.json to include raw-loader in
    order to inline SVGs.
    
    Add svg rule to the vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -6,5 +6,22 @@ module.exports = {
       .use('url-loader')
       .loader('url-loader')
       .tap(options => Object.assign(options, { limit: 14000 }))
+
+    const svgRule = config.module.rule('svg')
+
+    svgRule.uses.clear()
+    svgRule
+      .oneOf('external')
+      .resourceQuery(/external/)
+      .use('url')
+      .loader('url-loader')
+      .options({
+        limit: 10000,
+        name: 'img/[name].[hash:7].[ext]'
+      }).end().end()
+      .oneOf('normal')
+      .use('raw')
+      .loader('raw-loader')
+      .end().end()
   }
 }
