a35380998f6d0184bcfd9482c256eaf89f891d48
Adam DeHaven <adam.dehaven@konghq.com>
2022-02-08 16:01:36 -0500

    chore(router): add vue-router

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
 import { createApp } from 'vue'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import App from './App.vue'
 
 // Import a single component
@@ -18,5 +19,28 @@ const app = createApp(App)
 
 // Install plugin (all components)
 app.use(Kongponents)
+
+// 1. Define route components.
+// These can be imported from other files
+const Home = { template: '<div>Home</div>' }
+const About = { template: '<div>About</div>' }
+
+// 2. Define some routes
+// Each route should map to a component.
+// We'll talk about nested routes later.
+const routes = [
+  { path: '/', component: Home },
+  { path: '/about', component: About },
+]
+
+// 3. Create the router instance and pass the `routes` option
+// You can pass in additional options here, but let's
+// keep it simple for now.
+const router = createRouter({
+  // 4. Provide the history implementation to use. We are using the hash history for simplicity here.
+  history: createWebHashHistory(),
+  routes, // short for `routes: routes`
+})
+app.use(router)
 
 app.mount('#app')
