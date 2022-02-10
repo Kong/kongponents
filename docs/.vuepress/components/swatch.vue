cfeb24c1e52ecd9f135bef3ec724e384f41fc638
Adam DeHaven <adam.dehaven@konghq.com>
2022-02-09 19:45:50 -0500

    chore(styles): update style variable

--- docs/.vuepress/components/swatch.vue
+++ docs/.vuepress/components/swatch.vue
@@ -59,12 +59,14 @@ export default defineComponent({
 .swatch-container {
   display: flex;
   padding: 1.5rem 0;
+
   .swatch {
     width: 40px;
     height: 40px;
     border: 1px solid var(--grey-200);
     border-radius: 4px;
   }
+
   .description {
     flex: 1;
     margin-left: 1rem;
@@ -75,7 +77,7 @@ export default defineComponent({
         margin-bottom: .25rem;
       }
       &:last-of-type {
-        color: var(--black-70);
+        color: var(--c-text, var(--black-70));
         font-size: var(--type-sm);
       }
     }
