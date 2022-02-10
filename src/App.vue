c6e282ba3d9f591c64d625751aab8339ea8df999
Adam DeHaven <2229946+adamdehaven@users.noreply.github.com>
2022-02-10 15:37:49 -0500

    chore(tailwind): remove tailwind css (#485)

--- src/App.vue
+++ src/App.vue
@@ -25,10 +25,6 @@
   <KButton @click="openNotification('Adam message')">
     Show toaster
   </KButton>
-
-  <div class="text-white bg-indigo-500 mx-auto my-4 px-4 py-4 rounded-xl max-w-sm">
-    <p>This text block lets you know TailwindCSS is being properly imported.</p>
-  </div>
 </template>
 
 <script lang="ts">
