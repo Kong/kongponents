84a0e16e008eb4f6d12a5fa8d5a60145d4c9379e
Adam DeHaven <2229946+adamdehaven@users.noreply.github.com>
2022-03-29 12:42:21 -0400

    feat(kselect): upgrade kselect for vue 3 (#535)

---
+++ src/components/KSelect/KSelect.vue
@@ -0,0 +1,511 @@
+<template>
+  <div
+    :style="widthStyle"
+    class="k-select"
+  >
+    <KLabel
+      v-if="label"
+      :for="selectId"
+      v-bind="labelAttributes"
+      data-testid="k-select-label"
+    >
+      {{ label }}
+    </KLabel>
+    <div
+      :id="selectId"
+      data-testid="k-select-selected-item"
+    >
+      <div
+        v-if="selectedItem && appearance === 'dropdown'"
+        class="k-select-item-selection px-3 py-1"
+      >
+        <div
+          class="selected-item-label"
+        >
+          {{ selectedItem.label }}
+        </div>
+        <button
+          class="clear-selection-icon cursor-pointer non-visual-button"
+          @click="clearSelection"
+          @keyup.enter="clearSelection"
+        >
+          <KIcon
+            color="var(--blue-200)"
+            icon="clear"
+          />
+        </button>
+      </div>
+      <KToggle v-slot="{ toggle, isToggled }">
+        <KPop
+          v-bind="boundKPopAttributes"
+          :on-popover-click="() => {
+            toggle()
+            return isToggled
+          }"
+          :position-fixed="positionFixed"
+          :test-mode="testMode"
+          :target="`[id='${selectInputId}']`"
+          @opened="() => {
+            filterStr = ''
+            toggle()
+          }"
+          @closed="() => {
+            if (selectedItem && appearance === 'select') {
+              filterStr = selectedItem.label
+            }
+            if (isToggled) {
+              toggle()
+            }
+          }"
+        >
+          <div
+            v-if="appearance === 'button'"
+            :id="selectInputId"
+            class="k-select-button"
+            data-testid="k-select-input"
+            style="position: relative;"
+            role="listbox"
+          >
+            <KButton
+              :id="selectTextId"
+              :style="widthStyle"
+              :is-open="isToggled"
+              :is-rounded="false"
+              v-bind="$attrs"
+              appearance="btn-link"
+              @keyup="triggerFocus(isToggled)"
+            >
+              {{ selectButtonText }}
+            </KButton>
+          </div>
+          <div
+            v-else
+            :id="selectInputId"
+            :class="{ 'k-select-input': appearance === 'select'}"
+            data-testid="k-select-input"
+            style="position: relative;"
+            role="listbox"
+          >
+            <KIcon
+              v-if="appearance === 'select'"
+              icon="chevronDown"
+              color="var(--grey-500)"
+              size="15"
+            />
+            <KInput
+              :id="selectTextId"
+              v-bind="$attrs"
+              v-model="filterStr"
+              :is-open="isToggled"
+              :placeholder="placeholderText"
+              class="k-select-input"
+              autocomplete="off"
+              autocapitalize="off"
+              @keyup="triggerFocus(isToggled)"
+            />
+          </div>
+          <template #content>
+            <ul class="k-select-list ma-0 pa-0">
+              <slot
+                :items="items"
+                :is-open="isToggled"
+                name="items"
+              >
+                <div v-if="filteredItems.length">
+                  <KSelectItem
+                    v-for="item in filteredItems"
+                    :key="item.key"
+                    :item="item"
+                    @selected="handleItemSelect"
+                  />
+                </div>
+                <KSelectItem
+                  v-else
+                  key="k-select-empty-state"
+                  :item="{ label: 'No results', value: 'no_results' }"
+                  class="k-select-empty-item"
+                />
+              </slot>
+            </ul>
+          </template>
+        </KPop>
+      </KToggle>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, ref, Ref, computed, onBeforeMount, PropType } from 'vue'
+import { v1 as uuidv1 } from 'uuid'
+import KButton from '@/components/KButton/KButton.vue'
+import KIcon from '@/components/KIcon/KIcon.vue'
+import KInput from '@/components/KInput/KInput.vue'
+import KLabel from '@/components/KLabel/KLabel.vue'
+import KPop from '@/components/KPop/KPop.vue'
+import KToggle from '@/components/KToggle'
+import KSelectItem from '@/components/KSelect/KSelectItem.vue'
+
+const defaultKPopAttributes = {
+  hideCaret: true,
+  popoverClasses: 'k-select-popover mt-0',
+  popoverTimeout: 0,
+  placement: 'bottomStart',
+}
+
+interface SelectItem {
+  label: string
+  value: string
+  key?: string
+  selected?: boolean
+}
+
+export default defineComponent({
+  name: 'KSelect',
+  components: {
+    KButton,
+    KIcon,
+    KInput,
+    KLabel,
+    KPop,
+    KSelectItem,
+    KToggle,
+  },
+  inheritAttrs: false,
+  props: {
+    modelValue: {
+      type: [String, Number],
+      default: '',
+    },
+    kpopAttributes: {
+      type: Object,
+      default: () => ({
+        popoverClasses: '',
+      }),
+    },
+    label: {
+      type: String,
+      default: '',
+    },
+    labelAttributes: {
+      type: Object,
+      default: () => ({}),
+    },
+    /**
+     * The width of the select and popover's min-width
+     */
+    width: {
+      type: String,
+      default: '',
+    },
+    placeholder: {
+      type: String,
+      default: '',
+    },
+    /**
+     * The display style, can be either dropdown, select, or button
+     */
+    appearance: {
+      type: String,
+      default: 'dropdown',
+      validator: (value: string) => ['dropdown', 'select', 'button'].includes(value),
+    },
+    /**
+     * Override the text displayed on the button if `appearance` is `button` after an item
+     * has been selected. By default display the selected item's label
+     */
+    buttonText: {
+      type: String,
+      default: '',
+    },
+    /**
+     * Items are JSON objects with required 'label' and 'value'
+     * {
+     *  label: 'Item 1',
+     *  value: 'item1'
+     * }
+     */
+    items: {
+      type: Array as PropType<SelectItem[]>,
+      required: false,
+      default: () => [],
+      // Items must have a label & value
+      validator: (items: SelectItem[]) => !items.length || items.some(i => !!i.label && !!i.value),
+    },
+    /**
+     * A flag to use fixed positioning of the popover to avoid content being clipped by parental boundaries.
+     */
+    positionFixed: {
+      type: Boolean,
+      default: true,
+    },
+    /**
+     * Test mode - for testing only, strips out generated ids
+     */
+    testMode: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  emits: ['selected', 'input', 'change', 'update:modelValue'],
+  setup(props, { attrs, emit }) {
+    const filterStr = ref('')
+    const selectedItem = ref<SelectItem|null>(null)
+    const selectId = computed((): string => props.testMode ? uuidv1() : 'test-select-id-1234')
+    const selectInputId = computed((): string => props.testMode ? uuidv1() : 'test-select-input-id-1234')
+    const selectTextId = computed((): string => props.testMode ? uuidv1() : 'test-select-text-id-1234')
+    const selectItems: Ref<SelectItem[]> = ref([])
+
+    const boundKPopAttributes = computed(() => {
+      return {
+        ...defaultKPopAttributes,
+        ...props.kpopAttributes,
+        popoverClasses: `${defaultKPopAttributes.popoverClasses} ${props.kpopAttributes.popoverClasses} k-select-pop-${props.appearance}`,
+        width: props.width,
+        disabled: attrs.disabled,
+      }
+    })
+
+    const widthValue = computed(() => {
+      let w
+      if (!props.width) {
+        w = 170
+        if (props.appearance === 'button') {
+          w = 200
+        }
+      } else {
+        w = props.width
+      }
+      return w === 'auto' ? w : w + 'px'
+    })
+
+    const widthStyle = computed(() => {
+      return {
+        width: widthValue.value,
+      }
+    })
+
+    const filteredItems = computed(() => selectItems.value.filter((item: SelectItem) => item.label.toLowerCase().includes(filterStr.value.toLowerCase())))
+
+    const placeholderText = computed((): string => {
+      if (props.placeholder) {
+        return props.placeholder
+      } else if (attrs.placeholder) {
+        return attrs.placeholder as string
+      }
+      if (props.appearance === 'button') {
+        return 'Select an item'
+      }
+      return 'Filter...'
+    })
+
+    const selectButtonText = computed((): string => {
+      if (props.buttonText && selectedItem.value) {
+        return props.buttonText
+      } else if (selectedItem.value) {
+        return selectedItem.value.label
+      }
+      return placeholderText.value
+    })
+
+    const handleItemSelect = (item: SelectItem) => {
+      selectItems.value.forEach(anItem => {
+        if (anItem.key === item.key) {
+          anItem.selected = true
+          anItem.key += '-selected'
+          selectedItem.value = anItem
+        } else if (anItem.selected) {
+          delete anItem.selected
+          anItem.key = anItem?.key?.split('-selected')[0]
+        }
+      })
+      filterStr.value = props.appearance === 'dropdown' ? '' : item.label
+      emit('selected', item)
+      // this 'input' event must be emitted for v-model binding to work properly
+      emit('input', item.value)
+      emit('change', item)
+      emit('update:modelValue', item.value)
+    }
+
+    const clearSelection = (): void => {
+      selectItems.value.forEach(anItem => {
+        delete anItem.selected
+        anItem.key = anItem?.key?.split('-selected')[0]
+      })
+      selectedItem.value = null
+      // this 'input' event must be emitted for v-model binding to work properly
+      emit('input', null)
+      emit('change', null)
+      emit('update:modelValue', null)
+    }
+
+    const triggerFocus = (isToggled: boolean):void => {
+      const inputElem = document.getElementById(selectTextId.value)
+      if (!isToggled && inputElem) { // simulate click to trigger dropdown open
+        inputElem.click()
+      }
+    }
+
+    onBeforeMount(() => {
+      // items need keys
+      selectItems.value = props.items
+      for (let i = 0; i < selectItems.value.length; i++) {
+        selectItems.value[i].key = `${selectItems.value[i].label.replace(' ', '-')}-${i}`
+        if (selectItems.value[i].value === props.modelValue || selectItems.value[i].selected) {
+          selectItems.value[i].selected = true
+          selectedItem.value = selectItems.value[i]
+          selectItems.value[i].key += '-selected'
+
+          if (props.appearance === 'select') {
+            filterStr.value = selectedItem.value.label
+          }
+        }
+      }
+    })
+
+    return {
+      filterStr,
+      selectedItem,
+      selectId,
+      selectInputId,
+      selectTextId,
+      selectItems,
+      boundKPopAttributes,
+      widthValue,
+      widthStyle,
+      filteredItems,
+      placeholderText,
+      selectButtonText,
+      handleItemSelect,
+      clearSelection,
+      triggerFocus,
+    }
+  },
+})
+</script>
+
+<style lang="scss" scoped>
+@import '@/styles/variables';
+
+.k-select {
+  width: fit-content; // necessary for correct placement of popup
+
+  .k-select-item-selection {
+    background-color: var(--blue-100);
+    color: var(--blue-500);
+    font-weight: 400;
+    display: flex;
+    border-radius: 4px;
+    margin-bottom: 6px;
+
+    .selected-item-label {
+      align-self: center;
+      font-size: var(--type-xs);
+      line-height: 16px;
+    }
+
+    .clear-selection-icon {
+      margin-left: auto;
+      margin-top: auto;
+      margin-bottom: auto;
+      padding: 0;
+      height: 24px;
+
+      .kong-icon {
+        margin-left: auto;
+      }
+    }
+  }
+
+  .k-select-input {
+    position: relative;
+    display: inline-block;
+    width: 100%;
+    height: 32px;
+
+    input.k-input {
+      padding: var(--spacing-xs);
+      height: 100%;
+      border-radius: 4px 4px 0 0;
+    }
+
+    .kong-icon {
+      position: absolute;
+      top: 15px;
+      right: 6px;
+      z-index: 9;
+    }
+
+    .kong-icon-chevronDown {
+      top: 10px;
+    }
+  }
+
+  .k-select-button .has-caret :deep(.kong-icon) {
+    margin-left: auto;
+  }
+
+  .k-input {      // need this so input takes the
+    width: 100%;  // k-input-wrapper's width which uses this.width prop
+  }
+
+  .k-select-trigger:after {
+    display: inline-block;
+    width: 0;
+    height: 0;
+    margin-left: var(--spacing-xs, spacing(xs));
+    vertical-align: middle;
+    content: "";
+    border-top: 0.325em solid;
+    border-right: 0.325em solid transparent;
+    border-left: 0.325em solid transparent;
+  }
+
+  .k-select-popover {
+    box-sizing: border-box;
+    width: 100%;
+    border-radius: 0 0 4px 4px;
+
+    &.k-select-pop-button {
+      --KPopPaddingY: var(--spacing-md);
+      --KPopPaddingX: var(--spacing-xxs);
+      border-radius: 4px;
+      border: 1px solid var(--blue-200);
+    }
+
+    &.k-select-pop-dropdown {
+      --KPopPaddingY: var(--spacing-md);
+      --KPopPaddingX: var(--spacing-xxs);
+      border: 1px solid var(--blue-200);
+    }
+
+    &.k-select-pop-select {
+      --KPopPaddingY: var(--spacing-md);
+      --KPopPaddingX: var(--spacing-xxs);
+      border: 1px solid var(--black-10);
+    }
+
+    .k-select-empty-item button,
+    .k-select-empty-item button:focus,
+    .k-select-empty-item button:hover {
+      font-style: italic;
+      color: var(--grey-400);
+    }
+
+    ul {
+      margin: 0;
+      padding: 0;
+    }
+
+    a {
+      flex: 1;
+      color: var(--black-70);
+
+      &:hover,
+      &:active,
+      &:focus {
+        text-decoration: none;
+      }
+    }
+  }
+}
+</style>
---
+++ src/components/KSelect/KSelectItem.vue
@@ -0,0 +1,123 @@
+<template>
+  <div
+    :key="item.key"
+    :data-testid="`k-select-item-${item.value}`"
+    class="k-select-item"
+    @click="handleClick"
+  >
+    <li
+      role="option"
+      class="d-block"
+    >
+      <button
+        :class="{ disabled, selected: item.selected }"
+        :value="item.value"
+      >
+        <span class="k-select-item-label mr-2">
+          <slot>{{ item.label }}</slot>
+        </span>
+        <span class="k-select-selected-icon-container">
+          <KIcon
+            v-if="item.selected"
+            class="selected-item-icon"
+            icon="check"
+            color="var(--blue-200)"
+          />
+        </span>
+      </button>
+    </li>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import KIcon from '@/components/KIcon/KIcon.vue'
+
+export default defineComponent({
+  name: 'SelectItem',
+  components: { KIcon },
+  props: {
+    item: {
+      type: Object,
+      default: null,
+      // Items must have a label and value
+      validator: (item: Record<string, string>): boolean => !!item.label && !!item.value,
+    },
+    disabled: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  emits: ['selected'],
+  setup(props, { emit }) {
+    const handleClick = (): void => {
+      emit('selected', props.item)
+    }
+
+    return {
+      handleClick,
+    }
+  },
+})
+</script>
+
+<style lang="scss" scoped>
+@import '@/styles/variables';
+
+.k-select-item {
+  margin-bottom: 6px;
+
+  button {
+    display: flex;
+    align-items: center;
+    font-size: var(--type-xs);
+    line-height: 26px;
+    color: var(--grey-500);
+    padding-left: var(--spacing-xxs);
+    width: 100%;
+    border: none;
+    font-family: var(--font-family-sans);
+    background-color: var(--white);
+    border-radius: 4px;
+    text-align: left;
+    font-weight: 200;
+
+    .k-select-item-label {
+      width: auto;
+      line-height: 16px;
+    }
+
+    .kong-icon:not(.selected-item-icon) {
+      margin-right: .75rem;
+    }
+
+    .k-select-selected-icon-container {
+      margin-left: auto;
+      margin-top: auto;
+      margin-bottom: auto;
+      height: 24px;
+      width: 24px;
+
+      .kong-icon {
+        position: relative;
+        top: 0;
+      }
+    }
+
+    &:hover {
+      background-color: var(--grey-100);
+      color: var(--grey-600);
+    }
+
+    &.selected {
+      background-color: var(--blue-100);
+      color: var(--blue-500);
+      font-weight: 400;
+    }
+
+    &.danger {
+      color: var(--red-500);
+    }
+  }
+}
+</style>
