16b091567d4b4400b1fb039db034408d5d62ca25
Adam DeHaven <2229946+adamdehaven@users.noreply.github.com>
2022-03-31 10:48:54 -0400

    feat(kbutton): update kbutton with icon prop (#544)

--- src/components/KButton/KButton.spec.ts
+++ src/components/KButton/KButton.spec.ts
@@ -36,6 +36,32 @@ describe('KButton', () => {
     cy.get('.k-button').should('have.class', 'small')
   })
 
+  it('sets medium class when size passed', () => {
+    mount(KButton, {
+      props: {
+        size: 'medium',
+      },
+      slots: {
+        default: () => 'Medium Button',
+      },
+    })
+
+    cy.get('.k-button').should('have.class', 'medium')
+  })
+
+  it('sets large class when size passed', () => {
+    mount(KButton, {
+      props: {
+        size: 'large',
+      },
+      slots: {
+        default: () => 'Large Button',
+      },
+    })
+
+    cy.get('.k-button').should('have.class', 'large')
+  })
+
   it('renders a native link with KButton styles', () => {
     mount(KButton, {
       props: {
@@ -49,5 +75,33 @@ describe('KButton', () => {
 
     cy.get('a').should('have.class', 'k-button').and('have.class', 'secondary')
     cy.get('a').invoke('attr', 'href').should('eq', 'https://google.com')
+  })
+
+  it('renders an icon when using icon prop', () => {
+    mount(KButton, {
+      props: {
+        icon: 'spinner',
+      },
+      slots: {
+        default: () => 'Click me',
+      },
+    })
+
+    cy.get('.k-button .k-button-icon').should('be.visible')
+  })
+
+  it('renders an icon when using icon slot', () => {
+    const iconText = 'Pretend I am an icon'
+    mount(KButton, {
+      props: {
+        icon: 'spinner',
+      },
+      slots: {
+        default: () => 'Click me',
+        icon: () => iconText,
+      },
+    })
+
+    cy.get('.k-button').should('contain.text', iconText)
   })
 })
