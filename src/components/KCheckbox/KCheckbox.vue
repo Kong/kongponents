5275647ac867e93f83dc1ac611432f9f3e3bf2c8
Adam DeHaven <2229946+adamdehaven@users.noreply.github.com>
2022-04-01 17:11:24 -0400

    feat(kcheckbox): update for vue 3 (#550)
    * feat(kcheckbox): update for vue 3
    
    * fix(scrollbar): scrollbar thumb color

---
+++ src/components/KCheckbox/KCheckbox.vue
@@ -0,0 +1,48 @@
+<template>
+  <label class="k-checkbox">
+    <input
+      :checked="modelValue"
+      v-bind="$attrs"
+      type="checkbox"
+      class="k-input"
+      @change="handleChange"
+    >
+    <slot />
+  </label>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'KCheckbox',
+  inheritAttrs: false,
+  props: {
+    /**
+     * Sets whether or not checkbox is checked
+     */
+    modelValue: {
+      type: Boolean,
+      default: false,
+      required: true,
+    },
+  },
+  emits: ['input', 'change', 'update:modelValue'],
+  setup(props, { emit }) {
+    const handleChange = (e: any): void => {
+      emit('change', e.target.checked)
+      emit('input', e.target.checked)
+      emit('update:modelValue', e.target.checked)
+    }
+
+    return {
+      handleChange,
+    }
+  },
+})
+</script>
+
+<style lang="scss" scoped>
+@import '@/styles/variables';
+
+</style>
