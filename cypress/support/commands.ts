146544062b9f94b6a20d08de55c60b36c06be86a
Adam DeHaven <adam.dehaven@konghq.com>
2022-04-02 22:51:44 -0400

    chore(deps): update dependencies

--- cypress/support/commands.ts
+++ cypress/support/commands.ts
@@ -3,7 +3,7 @@ import { addMatchImageSnapshotCommand } from 'cypress-image-snapshot/command'
 addMatchImageSnapshotCommand({
   failureThreshold: 0.03, // threshold for entire image
   failureThresholdType: 'percent', // percent of image or number of pixels
-  customDiffConfig: { threshold: 0.15 }, // threshold for each pixel
+  customDiffConfig: { threshold: 0.1 }, // threshold for each pixel
   capture: 'viewport', // capture viewport in screenshot
 })
 
