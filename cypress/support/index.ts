5a90c876e9b5e179417144705736f8d3ddee355f
Adam DeHaven <adam.dehaven@konghq.com>
2022-04-04 14:09:45 -0400

    test: make test fail

--- cypress/support/index.ts
+++ cypress/support/index.ts
@@ -1,3 +1,5 @@
+import '@/styles/cypress-tests.scss'
+
 // Import Kongponent styles
 import '@/styles/styles.scss'
 
@@ -7,5 +9,10 @@ import './commands'
 afterEach(() => {
   // Match visual snapshot from 'cypress-image-snapshot' after each test
   // https://github.com/jaredpalmer/cypress-image-snapshot
-  cy.matchImageSnapshot()
+  cy.matchImageSnapshot(undefined, {
+    failureThreshold: 0.03, // threshold for entire image
+    failureThresholdType: 'percent', // percent of image or number of pixels
+    customDiffConfig: { threshold: 0.01 }, // threshold for each pixel
+    capture: 'viewport', // capture viewport in screenshot
+  })
 })
