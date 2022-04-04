3b9327cb44a9d5c716af1db473ca13efb0d50a5e
Adam DeHaven <adam.dehaven@konghq.com>
2022-04-04 12:39:33 -0400

    test: make test fail

--- cypress/support/index.ts
+++ cypress/support/index.ts
@@ -8,7 +8,7 @@ afterEach(() => {
   // Match visual snapshot from 'cypress-image-snapshot' after each test
   // https://github.com/jaredpalmer/cypress-image-snapshot
   cy.matchImageSnapshot(undefined, {
-    failureThreshold: 0.03, // threshold for entire image
+    failureThreshold: 0.1, // threshold for entire image
     failureThresholdType: 'percent', // percent of image or number of pixels
     customDiffConfig: { threshold: 0.1 }, // threshold for each pixel
     capture: 'viewport', // capture viewport in screenshot
