ba196e13b301d62303f0c91d479bb19e73d6e075
kai.arrowood <kai.arrowood@konghq.com>
2022-04-15 12:58:59 -0400

    feat(kcatalog): vue3 upgrade

---
+++ src/components/KCatalog/KCatalog.spec.ts
@@ -0,0 +1,11 @@
+// Import types for custom commands
+/// <reference types="../../cypress/support" />
+
+import { mount } from '@cypress/vue'
+import KCatalog from '@/components/KCatalog/KCatalog.vue'
+
+describe('KCatalog', () => {
+  it('TODO: This is just an example test', () => {
+    mount(KCatalog)
+  })
+})
