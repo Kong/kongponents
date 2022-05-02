581c4601e5ee7bc6d3e9a46d073f55e41ddf909e
Dustin Ryerson <dpryerson@gmail.com>
2022-05-02 11:32:45 -0600

    fix(ktable): create listeners even when td is not clicked target (#609)
    Removed a condition that limited @row:click event listening to just <td></td> elements, and not any child elements that may exist within <td></td> elements.

--- packages/KTable/KTable.vue
+++ packages/KTable/KTable.vue
@@ -532,7 +532,7 @@ export default defineComponent({
           ...rowListeners,
           ...cellListeners,
           click (e) {
-            if (e.target.tagName === 'TD' && (rowListeners.click || cellListeners.click)) {
+            if (rowListeners.click || cellListeners.click) {
               if (cellListeners.click) {
                 cellListeners.click(e, entity, 'cell')
               } else {
