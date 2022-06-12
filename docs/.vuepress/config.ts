2a71838592548cead6fc9e3b8e754b4d2072187a
Adam DeHaven <adam.dehaven@konghq.com>
2022-06-12 00:48:04 -0400

    chore(logo): update logo asset path

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -44,7 +44,7 @@ export default defineUserConfig({
   theme: defaultTheme({
     repo: 'kong/kongponents',
 
-    logo: 'https://2tjosk2rxzc21medji3nfn1g-wpengine.netdna-ssl.com/wp-content/uploads/2018/08/kong-logomark-color-64px.png',
+    logo: '/img/kong-logomark.png',
 
     docsDir: 'docs',
 
