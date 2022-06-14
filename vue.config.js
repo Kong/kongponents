d5a36913fc3190c60eeffd8d74ddc2c1d3c38dd5
Adam DeHaven <adam.dehaven@konghq.com>
2022-06-14 19:15:39 -0400

    fix(icons): import all icons with raw loader

--- vue.config.js
+++ vue.config.js
@@ -7,12 +7,15 @@ module.exports = {
     svgRule
       .oneOf('external')
       .resourceQuery(/external/)
-      .use('url')
-      .loader('url-loader')
-      .options({
-        limit: 10000,
-        name: 'img/[name].[hash:7].[ext]'
-      }).end().end()
+      .use('raw')
+      .loader('raw-loader')
+      .end().end()
+      // .use('url')
+      // .loader('url-loader')
+      // .options({
+      //   limit: 10000,
+      //   name: 'img/[name].[hash:7].[ext]'
+      // }).end().end()
       .oneOf('normal')
       .use('raw')
       .loader('raw-loader')
