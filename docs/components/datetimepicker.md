e23e2f54b7af50c82293dbc5d9bfcd16c2705502
mihai-peteu <mihai.peteu@konghq.com>
2022-08-31 11:33:35 -0400

    chore(examples): add a non-timeframe timepicker, emit back chosen values

--- docs/components/datetimepicker.md
+++ docs/components/datetimepicker.md
@@ -4,18 +4,90 @@
 
 **KDateTimePicker** - A date and time selection tool, displayed inside a popover.
 
+## Examples
+
+Calendar Display both a calendar and relative time frames
+
 <KCard>
   <template v-slot:body>
-    <!-- <div class="mt-3">Selected: {{ currentValue }}</div> -->
-    <KDateTimePicker @change="newVal => currentValue = newVal" v-model="currentValue" :value="{ timeframeText: '15 minutes', timeframeLength: 60 * 15 }" />
+    <div class="mt-3">Selected: <pre>{{ currentValue1 }}</pre></div>
+    <KDateTimePicker
+      @changed="newVal => currentValue1 = newVal"
+      defaultMessage="Please select a date or time range"
+      :timePeriods="exampleTimeFrames"
+      :defaultValue="defaultRange"
+      v-model="currentValue1"
+    />
   </template>
 </KCard>
 
 ```html
-<KDateTimePicker @change="newVal => currentValue = newVal"/>
+<KDateTimePicker 
+  @change="newVal => currentValue = newVal" 
+  :timePeriods=[
+    {
+      section: 'Last',
+      values: [
+        { timeframeText: '15 minutes', timeframeLength: 60*15 },
+        ...
+      ]
+    },{
+      section: 'P',
+      values: [
+        { timeframeText: '15 minutes', timeframeLength: 60*15 },
+        ...
+      ]
+    },
+  ]
+/>
 ```
 
+Calendar Display only - the `timePeriods` prop is omitted
+
+<KCard>
+  <template v-slot:body>
+    <div class="mt-3">Selected: <pre>{{ currentValue2 }}</pre></div>
+    <KDateTimePicker
+      @changed="newVal => currentValue2 = newVal"
+      defaultMessage="Please select some dates"
+      :defaultValue="defaultRange"
+      v-model="currentValue2"
+    />
+  </template>
+</KCard>
+
 ## Props
+
+### timePeriods
+
+An array of time frame values to be displayed as buttons in the "Relative" section of the popover, eg:
+
+```json
+[
+  {
+    section: 'Last',
+    values: [
+      { timeframeText: '15 minutes', timeframeLength: 60*15 },
+      { timeframeText: '12 hours', timeframeLength: 60*60*12 },
+      { timeframeText: '24 hours', timeframeLength: 60*60*24 }
+    ]
+  },
+  {
+    section: 'Previous',
+    values: [
+      { timeframeText: 'Week', timeframeLength: 60*60*24*14  }
+    ]
+  }
+]
+```
+
+### value
+
+The timestamp that the component should default to
+
+### defaultMessage
+
+User-facing messaging that encourages the user to click things
 
 </div>
 
@@ -24,7 +96,32 @@ export default {
   data() {
     return {
       hasMounted: false,
-      currentValue: { timeframeText: '15 minutes', timeframeLength: 60 * 15 },
+      currentValue1: '',
+      currentValue2: '',
+      defaultRange: {
+        start: new Date().getTime(),
+        end: new Date().getTime() - (7*24*60*60*1000)
+      },
+      exampleTimeFrames: [
+        {
+          section: 'Last',
+          values: [
+            { timeframeText: '15 minutes', timeframeLength: 60*15 },
+            { timeframeText: '12 hours', timeframeLength: 60*60*12 },
+            { timeframeText: '24 hours', timeframeLength: 60*60*24 },
+            { timeframeText: '7 days', timeframeLength: 60*60*24*7 },
+            { timeframeText: '30 days', timeframeLength: 60*60*24*30 },
+            { timeframeText: '90 days', timeframeLength: 60*60*24*90 }
+          ]
+        },
+        {
+          section: 'Previous',
+          values: [
+            { timeframeText: 'Week', timeframeLength: 60*60*24*14 },
+            { timeframeText: 'Quarter', timeframeLength: 60*60*24*90 }
+          ]
+        }
+      ]
     }
   },
   mounted() {
--- packages/KDateTimePicker/KDateTimePicker.vue
+++ packages/KDateTimePicker/KDateTimePicker.vue
@@ -14,6 +14,7 @@
     </KButton>
     <template #content>
       <KSegmentedControl
+        v-if="hasRelativeTimeframes"
         v-model="mode"
         :options="[
           { label: 'Custom', value: 'custom' },
@@ -32,21 +33,30 @@
         is-expanded
       />
       <div
-        v-else
-        class="relative-frames d-flex justify-content-start"
+        v-else-if="!showCalendar && hasRelativeTimeframes"
+        class="d-flex flex-column"
       >
-        <KButton
-          v-for="(item, index) in allowedTimePeriods"
-          :key="`time-${item.timeframeLength}-${index}`"
-          :is-rounded="false"
-          :class="{'selected-option': item.timeframeText === selectedTimeframe.timeframeText}"
-          :data-testid="'select-timeframe-' + item.timeframeLength"
-          appearance="outline"
-          size="medium"
-          @click="changeTimeframe(item)"
+        <div
+          v-for="(item, index) in timePeriods"
+          :key="`section-${index}`"
+          class="timeframe-section d-flex flex-column"
         >
-          {{ item.timeframeText }}
-        </KButton>
+          <h4>{{ item.section }}</h4>
+          <div class="timeframe-buttons d-flex flex-row justify-content-start">
+            <KButton
+              v-for="(timeFrame, index) in item.values"
+              :key="`time-${timeFrame.timeframeLength}-${index}`"
+              :is-rounded="false"
+              :class="{'selected-option': timeFrame.timeframeText === selectedTimeframe.timeframeText}"
+              :data-testid="'select-timeframe-' + timeFrame.timeframeLength"
+              appearance="outline"
+              size="medium"
+              @click="changeTimeframe(timeFrame)"
+            >
+              {{ timeFrame.timeframeText }}
+            </KButton>
+          </div>
+        </div>
       </div>
     </template>
     <template #footer>
@@ -60,6 +70,7 @@
           Clear
         </KButton>
         <KButton
+          :disabled="submitDisabled"
           :is-rounded="false"
           size="medium"
           appearance="btn-link"
@@ -79,54 +90,6 @@ import KPop from '@kongponents/kpop/KPop.vue'
 import KSegmentedControl from '@kongponents/ksegmentedcontrol/KSegmentedControl.vue'
 import { format, fromUnixTime, getUnixTime, roundToNearestMinutes } from 'date-fns'
 
-const allowedTimePeriods = [
-  {
-    section: 'Last',
-    timeframeText: '15 minutes',
-    timeframeLength: 60 * 15
-  },
-  {
-    section: 'Last',
-    timeframeText: '12 hours',
-    timeframeLength: 60 * 60 * 12
-  },
-  {
-    section: 'Last',
-    timeframeText: '24 hours',
-    timeframeLength: 60 * 60 * 24
-  },
-  {
-    section: 'Last',
-    timeframeText: '7 days',
-    timeframeLength: 60 * 60 * 24 * 7
-  },
-  {
-    section: 'Last',
-    timeframeText: '30 days',
-    timeframeLength: 60 * 60 * 24 * 30
-  },
-  {
-    section: 'Last',
-    timeframeText: '90 days',
-    timeframeLength: 60 * 60 * 24 * 90
-  },
-  {
-    section: 'Previous',
-    timeframeText: 'Week',
-    timeframeLength: 60 * 60 * 24 * 90
-  },
-  {
-    section: 'Previous',
-    timeframeText: 'Month',
-    timeframeLength: 60 * 60 * 24 * 90
-  },
-  {
-    section: 'Previous',
-    timeframeText: 'Quarter',
-    timeframeLength: 60 * 60 * 24 * 90
-  }
-]
-
 export default {
   name: 'KTimePicker',
   components: {
@@ -136,19 +99,33 @@ export default {
     DatePicker: () => import('v-calendar/lib/components/date-picker.umd')
   },
   props: {
-    value: {
+    defaultMessage: {
+      type: String,
+      required: true
+    },
+    defaultValue: {
       type: Object,
       required: true
+    },
+    timePeriods: {
+      type: Array,
+      required: false,
+      default: () => []
     }
   },
 
   data () {
+    console.warn(' >>> defaultValue: ')
+    console.log(this.defaultValue)
+    console.warn(' >>> timePeriods: ')
+    console.log(this.timePeriods)
+
     return {
-      allowedTimePeriods,
       mode: this.mode || 'custom',
-      displayedRange: 'Please select a time range',
+      displayedRange: this.defaultMessage,
       selectedCalendarRange: this.selectedCalendarRange,
-      selectedTimeframe: this.value || this.allowedTimePeriods[0],
+      selectedTimeframe: this.timePeriods[0],
+      range: { start: '', end: '' },
       modelConfig: {
         type: 'number'
       },
@@ -172,29 +149,21 @@ export default {
   computed: {
     showCalendar () {
       return (this.mode === 'custom')
+    },
+    hasRelativeTimeframes () {
+      return (this.timePeriods.length > 0)
+    },
+    submitDisabled () {
+      return (!this.range.start || !this.range.end)
     }
   },
 
   watch: {
     selectedCalendarRange (newVal) {
-      console.warn('>>>> selectedCalendarRange watcher <<<')
-      console.log(newVal.start)
-      console.log(newVal.end)
-
-      this.formatDisplayDate(Math.floor(newVal.start / 1000), Math.floor(newVal.end / 1000))
+      if (newVal) {
+        this.formatDisplayDate(Math.floor(newVal.start / 1000), Math.floor(newVal.end / 1000))
+      }
     }
-    //   handler (relativeTimeFrame) {
-    //     console.warn('>>>> selectedTimeFrame watcher <<<')
-    //     const end = getUnixTime(roundToNearestMinutes(Date.now()))
-    //     const start = end - this.selectedTimeframe.timeframeLength
-
-    //     console.log(start)
-    //     console.log(end)
-    //     console.log(relativeTimeFrame)
-    //     this.formatDisplayDate(start, end)
-    //   },
-    //   immediate: true
-    // }
   },
 
   methods: {
@@ -214,29 +183,20 @@ export default {
       this.formatDisplayDate(start, end)
     },
     clearSelection () {
-      this.selectedCalendarRange = ''
-      this.selectedTimeframe = this.allowedTimePeriods[0]
+      this.selectedCalendarRange = null
+      this.displayedRange = this.defaultMessage
+      this.range.start = ''
+      this.range.end = ''
+      this.selectedTimeframe = this.timePeriods[0]
     },
     formatDisplayDate (start, end) {
+      this.range = { start, end }
       this.displayedRange = `${format(fromUnixTime(start), 'PP hh:mm a')} - ${format(fromUnixTime(end), 'PP hh:mm a')}`
     },
     submitTimeFrame () {
-      console.warn('>>> submitTimeFrame')
-      let start = 0
-      let end = 0
+      console.log(this.range)
 
-      // If calendar currently shown, send start/end values
-      if (this.showCalendar) {
-        start = Math.floor(this.selectedCalendarRange.start / 1000)
-        end = Math.floor(this.selectedCalendarRange.end / 1000)
-      } else {
-        end = getUnixTime(roundToNearestMinutes(Date.now()))
-        start = end - this.selectedTimeframe.timeframeLength
-      }
-
-      console.log({ start, end })
-
-      // this.$emit('changed', { start, end })
+      this.$emit('changed', this.range)
     }
   }
 }
@@ -248,6 +208,7 @@ $margin: .2rem;
 
 .analytics-timepicker {
   .timepicker-input {
+    min-width: 22rem;
     padding: var(--spacing-sm) var(--spacing-xl) var(--spacing-sm) 40px !important;
     background-image: url("data:image/svg+xml,%3Csvg width='16' height='16' viewBox='0 0 16 16' fill='none' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath fill-rule='evenodd' clip-rule='evenodd' d='M14 14H2V2H4V4H6V2H10V4H12V2H14V14ZM14 0H2C0.895 0 0 0.896 0 2V14C0 15.104 0.895 16 2 16H14C15.104 16 16 15.104 16 14V2C16 0.896 15.104 0 14 0ZM7 12H9V10H7V12ZM10 9H12V7H10V9ZM7 9H9V7H7V9ZM4 9H6V7H4V9ZM4 12H6V10H4V12Z' fill='%236F7787'/%3E%3C/svg%3E%0A");
     background-repeat: no-repeat;
@@ -257,34 +218,6 @@ $margin: .2rem;
 
     &:focus {
       box-shadow: none !important;
-    }
-  }
-
-  .k-popover {
-    .k-popover-content {
-      .relative-frames {
-        flex-wrap: wrap;
-
-        .k-button {
-          flex-basis: calc(32% - $margin);
-          font-weight: 400;
-          margin: $margin;
-          padding: .75rem .5rem;
-          justify-content: center;
-          width: 3rem;
-
-          &.selected-option {
-            color: white;
-            background: var(--blue-500)
-          }
-        }
-      }
-    }
-    .k-popover-footer {
-      // margin: .75rem auto 0;
-      button {
-        padding: 0 1rem;
-      }
     }
   }
 }
@@ -298,8 +231,27 @@ $margin: .2rem;
 .analytics-timepicker {
   .k-popover {
     min-width: 22rem;
-
+    padding: 1rem ;
     .k-popover-content {
+      .timeframe-section {
+        .timeframe-buttons {
+          flex-wrap: wrap;
+          .k-button {
+            flex-basis: calc(32% - $margin);
+            font-weight: 400;
+            margin: $margin;
+            padding: .75rem .5rem;
+            justify-content: center;
+            width: 3rem;
+
+            &.selected-option {
+              color: white;
+              background: var(--blue-500)
+            }
+          }
+        }
+      }
+
       // v-calendar overrides
       .vc-container {
         border: 0;
@@ -346,7 +298,12 @@ $margin: .2rem;
         }
       }
     }
-
+    .k-popover-footer {
+      margin: .75rem auto 0;
+      button {
+        padding: .25rem 1rem;
+      }
+    }
   }
 }
 </style>
