57a8ba1a3812202e5e19a3ce7ca507b8fdf3e315
Adam DeHaven <2229946+adamdehaven@users.noreply.github.com>
2022-09-26 16:57:23 -0400

    fix(kpop): always remove click listeners (#810)

--- src/components/KPop/KPop.vue
+++ src/components/KPop/KPop.vue
@@ -327,11 +327,12 @@ export default defineComponent({
   },
   beforeUnmount() {
     const popper = this.$refs.popper
-    if (popper && this.trigger === 'click') {
-      this.reference && this.reference.removeEventListener('click', this.handleClick)
-      popper.removeEventListener('click', this.showPopper)
-      document.documentElement.removeEventListener('click', this.handleClick)
-    } else if (this.reference) {
+
+    document.documentElement.removeEventListener('click', this.handleClick)
+    popper && popper.removeEventListener('click', this.showPopper)
+
+    if (this.reference) {
+      this.reference.removeEventListener('click', this.handleClick)
       this.reference.removeEventListener('mouseenter', this.createInstance)
       this.reference.removeEventListener('mouseleave', this.toggle)
       this.reference.removeEventListener('focus', this.createInstance)
