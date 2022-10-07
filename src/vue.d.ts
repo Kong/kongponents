089bcaf98b7bba86ef5d34f2edb3471a04f44220
Philipp Rudloff <philipp.rudloff@konghq.com>
2022-10-07 16:14:32 +0200

    chore: split off global properties types
    Splits off the global properties type augmentation (see https://vuejs.org/guide/typescript/options-api.html#augmenting-global-properties) because defining it next to the one in vue.d.ts actually breaks its effect of TypeScript no longer complaining about .vue imports.

--- src/vue.d.ts
+++ src/vue.d.ts
@@ -1,16 +1,7 @@
 /// <reference types="vite/client" />
-/* eslint-disable */
-import ToastManager from './components/KToaster/ToastManager'
 
 declare module '*.vue' {
-  import type { DefineComponent } from 'vue'
+  import { DefineComponent } from 'vue'
   const component: DefineComponent<{}, {}, any>
   export default component
-}
-
-
-declare module 'vue' {
-  interface ComponentCustomProperties {
-    $toaster: typeof ToastManager
-  }
 }
