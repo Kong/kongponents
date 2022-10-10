ea0f6b4e7a081be5251a17b7b2369dbd6d05725e
Philipp Rudloff <philipprudloff@fastmail.com>
2022-10-10 16:27:29 +0200

    chore: clean up linter setup (#820)
    * chore: split off global properties types
    
    Splits off the global properties type augmentation (see https://vuejs.org/guide/typescript/options-api.html#augmenting-global-properties) because defining it next to the one in vue.d.ts actually breaks its effect of TypeScript no longer complaining about .vue imports.
    
    * chore(deps): cleans up linter setup
    
    Removes `@vue/eslint-config-standard` and `@vue/eslint-config-typescript`. These packages were used by Vue CLI which this project no longer uses.
    
    Replaces eslint-plugin-node and eslint-plugin-standard with eslint-config-standard and eslint-plugin-n respectively which brings the linter setup in line with how standard is supposed to be configured (see also the deprecation note at https://www.npmjs.com/package/eslint-plugin-standard).
    
    Tunes the ESLint configuration. Specifically, the correct parser for TypeScript is now enabled. Declares the plugins that are in use. Removes unnecessary configuration extensions (e.g. essential is unnecessary when using recommended). Moves TypeScript specific rules (and correspondingly deactivated JavaScript rules) into a file extension-specific override so that they don’t apply to plain JavaScript files.
    
    Co-authored-by: Adam DeHaven <2229946+adamdehaven@users.noreply.github.com>

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,22 +1,19 @@
 module.exports = {
   root: true,
+  parserOptions: {
+    parser: '@typescript-eslint/parser',
+    ecmaVersion: 2020,
+  },
   env: {
     node: true,
   },
+  plugins: ['vue', 'import', '@typescript-eslint'],
   extends: [
-    'plugin:vue/vue3-recommended',
-    '@vue/standard',
-    '@vue/typescript',
-    'plugin:vue/vue3-essential',
     'eslint:recommended',
-    '@vue/typescript/recommended',
+    'plugin:vue/vue3-recommended',
+    'standard',
   ],
-  parserOptions: {
-    ecmaVersion: 2020,
-  },
   rules: {
-    indent: 'off',
-    'space-before-function-paren': 'off',
     quotes: ['error', 'single', { avoidEscape: true }],
     'no-multi-spaces': 'error',
     'no-trailing-spaces': 'error',
@@ -31,13 +28,30 @@ module.exports = {
       ignoreTemplateLiterals: true,
       ignoreRegExpLiterals: true,
     }],
-    '@typescript-eslint/space-before-function-paren': ['error', {
-      anonymous: 'never',
-      named: 'never',
-      asyncArrow: 'always',
-    }],
-    '@typescript-eslint/no-explicit-any': 'off',
-    '@typescript-eslint/indent': ['error', 2],
-    '@typescript-eslint/func-call-spacing': ['error', 'never'],
+    'space-before-function-paren': 'off',
   },
+  overrides: [
+    {
+      files: ['*.ts', '*.vue'],
+      rules: {
+        '@typescript-eslint/no-explicit-any': 'off',
+        // Avoids false errors like “'NodeListOf' is not defined”.
+        'no-undef': 'off',
+        // Turns off some non-TypeScript rules in favor of their specific TypeScript rules to avoid false negatives:
+        indent: 'off',
+        '@typescript-eslint/indent': ['error', 2],
+        'no-unused-vars': 'off',
+        '@typescript-eslint/no-unused-vars': 'error',
+        'space-before-function-paren': 'off',
+        '@typescript-eslint/space-before-function-paren': ['error', {
+          anonymous: 'never',
+          named: 'never',
+          asyncArrow: 'always',
+        }],
+        // Ensures ESLint understands that `defineEmits<{ ... }>()` does _not_ fail this rule.
+        'func-call-spacing': 'off',
+        '@typescript-eslint/func-call-spacing': 'error',
+      },
+    },
+  ],
 }
--- cypress/support/index.d.ts
+++ cypress/support/index.d.ts
@@ -3,17 +3,17 @@
 declare namespace Cypress {
   interface Chainable {
     /**
-       * @description Custom alias command for cy.get() to select DOM element by data-testid attribute.
-       * @param {string} dataTestId
-       * @example cy.dataTestId('kong-auth-login-submit')
-       */
+     * @description Custom alias command for cy.get() to select DOM element by data-testid attribute.
+     * @param {string} dataTestId
+     * @example cy.dataTestId('kong-auth-login-submit')
+     */
     getTestId(dataTestId: string): Chainable<Element>
 
     /**
-       * @description Custom alias command for cy.find() to select DOM element by data-testid attribute.
-       * @param {string} dataTestId
-       * @example cy.findTestId('kong-auth-login-submit')
-       */
+     * @description Custom alias command for cy.find() to select DOM element by data-testid attribute.
+     * @param {string} dataTestId
+     * @example cy.findTestId('kong-auth-login-submit')
+     */
     findTestId(dataTestId: string): Chainable<Element>
   }
 }
