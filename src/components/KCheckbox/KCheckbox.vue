e806c877eeff9c22178b28a2e5a9c91187664124
Manjiri Tapaswi <manjiri.tapaswi@konghq.com>
2022-11-11 15:05:46 -0800

    style: fix styles

--- src/components/KCheckbox/KCheckbox.vue
+++ src/components/KCheckbox/KCheckbox.vue
@@ -96,7 +96,7 @@ export default defineComponent({
   font-size: 14px;
   line-height: 20px;
   /* identical to box height, or 143% */
-  display: flex;
+  display: inline-block;
   align-items: center;
 
   /* Black/85 */
--- src/components/KRadio/KRadio.vue
+++ src/components/KRadio/KRadio.vue
@@ -106,7 +106,7 @@ export default defineComponent({
   font-size: 14px;
   line-height: 20px;
   /* identical to box height, or 143% */
-  display: flex;
+  display: inline-block;
   align-items: center;
 
   /* Black/85 */
