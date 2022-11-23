9bb0701be361fe2afd39830677c629007954a147
kai.arrowood <kai.arrowood@konghq.com>
2022-11-23 11:38:51 -0500

    fix(*): pr feedback

--- src/components/KDropdownMenu/KDropdownMenu.vue
+++ src/components/KDropdownMenu/KDropdownMenu.vue
@@ -139,7 +139,7 @@ export default defineComponent({
     items: {
       type: Array as PropType<Array<DropdownItem>>,
       default: () => [],
-      validator: (items: DropdownItem[]) => !items.length || items.some(i => i.label !== undefined),
+      validator: (items: DropdownItem[]) => !items.length || items.every(i => i.label !== undefined),
     },
     disabled: {
       type: Boolean,
