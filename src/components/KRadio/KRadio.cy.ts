8f7f638b9fcecf03a1462f0b8f80fdafae9bab45
portikM <maksym.portianoi@konghq.com>
2023-01-20 11:33:51 -0500

    test(kradio): add card behavior tests [KHCP-5660]

--- src/components/KRadio/KRadio.cy.ts
+++ src/components/KRadio/KRadio.cy.ts
@@ -37,4 +37,47 @@ describe('KRadio', () => {
       cy.wrap(Cypress.vueWrapper.emitted()).should('have.property', 'update:modelValue')
     })
   })
+
+  it('renders input element hidden when type prop is card', () => {
+    mount(KRadio, {
+      props: {
+        modelValue: false,
+        selectedValue: true,
+        type: 'card',
+      },
+    })
+
+    cy.get('input').should('not.be.visible')
+  })
+
+  it('should be checkable by click within entire label element when type prop is card', () => {
+    mount(KRadio, {
+      props: {
+        modelValue: false,
+        selectedValue: true,
+        type: 'card',
+      },
+    })
+
+    cy.get('label').click().then(() => {
+      cy.get('input').should('be.checked')
+    })
+  })
+
+  it('should not be checkable when disabled and type prop is card', () => {
+    mount(KRadio, {
+      props: {
+        modelValue: false,
+        selectedValue: true,
+        type: 'card',
+      },
+      attrs: {
+        disabled: true,
+      },
+    })
+
+    cy.get('label').click().then(() => {
+      cy.get('input').should('not.be.checked')
+    })
+  })
 })
