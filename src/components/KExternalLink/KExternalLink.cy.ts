b363f3bb708fb7bcf2afa2c651aba83d7d828a1a
portikM <maksym.portianoi@konghq.com>
2023-01-26 18:49:16 -0500

    test(kexternallink): add invalid href test scenario [KHCP-5745]

--- src/components/KExternalLink/KExternalLink.cy.ts
+++ src/components/KExternalLink/KExternalLink.cy.ts
@@ -2,6 +2,21 @@ import { mount } from 'cypress/vue'
 import KExternalLink from '@/components/KExternalLink/KExternalLink.vue'
 
 describe('KExternalLink', () => {
+  it('should not render link element when passed invalid href', () => {
+    const linkHref = 'kongponents'
+    const linkText = 'This is external link'
+    mount(KExternalLink, {
+      props: {
+        href: linkHref,
+      },
+      slots: {
+        default: () => linkText,
+      },
+    })
+
+    cy.get('.k-external-link').should('not.exist')
+  })
+
   it('should render link with href attribute', () => {
     const linkHref = 'https://kongponents.konghq.com/'
     const linkText = 'This is external link'
