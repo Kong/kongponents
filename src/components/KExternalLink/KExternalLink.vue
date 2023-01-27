55e55e660950bc5e59bb8f7e142b476c217aaac2
Maksym Portianoi <portimaksym@gmail.com>
2023-01-27 09:14:21 -0500

    feat(*): add external link component [KHCP-5745] (#1073)
    * feat(*): add external link component [KHCP-5745]
    
    * docs(external-link): add external link docs page [KHCP-5745]
    
    * docs(kexternallink): add component tests [KHCP-5745]
    
    * refactor(kexternallink): use url constructor for validation [KHCP-5745]
    
    * docs(external-link): add hideIcon prop docs section [KHCP-5745]
    
    * feat(kexternallink): add theming options [KHCP-5745]
    
    * docs(external-link): add theming options docs section [KHCP-5745]
    
    * test(kexternallink): add invalid href test scenario [KHCP-5745]
    
    * fix(kexternallink): minor fix [KHCP-5745]
    
    Co-authored-by: Adam DeHaven <2229946+adamdehaven@users.noreply.github.com>
    
    * docs(external-link): tweak theming section [KHCP-5745]
    
    Co-authored-by: Adam DeHaven <2229946+adamdehaven@users.noreply.github.com>

---
+++ src/components/KExternalLink/KExternalLink.vue
@@ -0,0 +1,77 @@
+<template>
+  <a
+    v-if="isHrefValid"
+    class="k-external-link"
+    :href="href"
+    rel="noopener"
+    target="_blank"
+    @mouseleave="isMouseOver = false"
+    @mouseover="isMouseOver = true"
+  >
+    <slot />
+    <KIcon
+      v-if="!hideIcon"
+      :color="iconColor"
+      icon="externalLink"
+      size="12"
+    />
+  </a>
+</template>
+
+<script lang="ts">
+const isValidUrl = (url: string): boolean => {
+  try {
+    // eslint-disable-next-line no-new
+    new URL(url)
+
+    // No error was thrown, so return true
+    return true
+  } catch {
+    // Invalid URL
+    return false
+  }
+}
+</script>
+
+<script setup lang="ts">
+// eslint-disable-next-line import/first
+import { computed, ref } from 'vue'
+
+const props = defineProps({
+  href: {
+    type: String,
+    required: true,
+    validator: (value: string): boolean => !!isValidUrl(value),
+  },
+  hideIcon: {
+    type: Boolean,
+    default: false,
+  },
+})
+
+const isHrefValid = computed((): boolean => !!isValidUrl(props.href))
+
+const isMouseOver = ref(false)
+const iconColor = computed((): string => isMouseOver.value ? 'var(--KExternalLinkColorHover, var(--blue-600))' : 'var(--KExternalLinkColor, var(--blue-500))')
+</script>
+
+<style lang="scss" scoped>
+@import '@/styles/variables';
+@import '@/styles/functions';
+
+.k-external-link {
+  align-items: center;
+  color: var(--KExternalLinkColor, color(blue-500));
+  display: inline-flex;
+  font-weight: 400;
+  text-decoration: none;
+
+  &:hover {
+    color: var(--KExternalLinkColorHover, color(blue-600));
+  }
+
+  .kong-icon {
+    margin-left: var(--spacing-xs);
+  }
+}
+</style>
