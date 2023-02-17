e225d159bd9816368f48a8d6bc97347c849a75b7
portikM <maksym.portianoi@konghq.com>
2023-02-17 17:04:21 -0500

    feat(kselect): support grouping items [KHCP-6147]

--- src/components/KSelect/KSelectItems.vue
+++ src/components/KSelect/KSelectItems.vue
@@ -1,6 +1,6 @@
 <template>
   <KSelectItem
-    v-for="item in items"
+    v-for="item in nonGroupedItems"
     :key="item.key"
     :item="item"
     @selected="handleItemSelect"
@@ -12,16 +12,40 @@
       />
     </template>
   </KSelectItem>
+  <div
+    v-for="(grouping, n) in groupings"
+    :key="`${grouping}-${n}`"
+    class="mb-1"
+  >
+    <span class="d-block k-select-grouping-title">
+      {{ grouping }}
+    </span>
+    <KSelectItem
+      v-for="item in getGroupingItems(grouping)"
+      :key="item.key"
+      :item="item"
+      @selected="handleItemSelect"
+    >
+      <template #content>
+        <slot
+          :item="item"
+          name="content"
+        />
+      </template>
+    </KSelectItem>
+  </div>
 </template>
 
-<script lang="ts">
-import { PropType } from 'vue'
+<script setup lang="ts">
+import { computed, PropType } from 'vue'
 import { SelectItem } from './KSelect.vue'
 import KSelectItem from '@/components/KSelect/KSelectItem.vue'
-</script>
 
-<script setup lang="ts">
-defineProps({
+interface SelectItemWithGrouping extends SelectItem {
+  grouping: string
+}
+
+const props = defineProps({
   items: {
     type: Array as PropType<SelectItem[]>,
     required: false,
@@ -34,4 +58,17 @@ defineProps({
 const emit = defineEmits(['selected'])
 
 const handleItemSelect = (item: SelectItem) => emit('selected', item)
+
+const nonGroupedItems = computed((): SelectItem[] => props.items.filter(item => !item.grouping))
+const groupings = computed((): string[] => [...new Set((props.items.filter(item => item.grouping) as unknown as SelectItemWithGrouping[]).map(item => item.grouping))].sort((a, b) => a.localeCompare(b.toLowerCase())))
+
+const getGroupingItems = (grouping: string) => props.items.filter(item => item.grouping === grouping)
 </script>
+
+<style lang="scss" scoped>
+.k-select-grouping-title {
+  font-weight: 600;
+  margin-bottom: var(--spacing-xxs);
+  padding: var(--spacing-xs);
+}
+</style>
