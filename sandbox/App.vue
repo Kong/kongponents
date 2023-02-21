de71242a007cf66057d60aa0beceae67bccaa763
Adam DeHaven <2229946+adamdehaven@users.noreply.github.com>
2023-02-21 15:14:31 -0500

    chore(sandbox): add component sandbox (#1141)
    * chore(sandbox): add component sandbox
    
    * chore: rename pages
    
    * chore: add sandbox to tsignore

---
+++ sandbox/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <router-view />
+</template>
+
+<style lang="scss">
+body,
+html {
+  height: 100%;
+  margin: 0;
+  padding: 0;
+  width: 100%;
+}
+
+body {
+  padding: 20px;
+}
+</style>
