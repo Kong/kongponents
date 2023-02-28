fae9635f2d6c3f8e874ee722066d1e38543bd76d
Yi Yang <Leopoldthecoder@users.noreply.github.com>
2023-02-28 21:56:10 +0800

    fix(kmenu): divider style (#1161)

--- src/components/KMenu/KMenuDivider.vue
+++ src/components/KMenu/KMenuDivider.vue
@@ -18,6 +18,12 @@ export default defineComponent({
 @import '@/styles/functions';
 
 .k-menu-item-divider {
-  padding: 0 19px 0 19px;
+  padding: 0 19px;
+
+  hr {
+    border: none;
+    border-top: 1px solid var(--grey-300);
+    margin: 16px 0;
+  }
 }
 </style>
