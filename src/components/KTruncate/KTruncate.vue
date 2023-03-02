dd32e8503e36562b3de6fb5a1474b3ee6ca51659
Maksym Portianoi <portimaksym@gmail.com>
2023-03-02 09:53:03 -0500

    fix(ktruncate): a11y and interaction improvements [KHCP-6105] (#1166)
    * fix(ktruncate): a11y and interaction improvements [KHCP-6105]
    
    * docs(truncate): small tweak [KHCP-6105]
    
    * fix(ktruncate): add padding to wrapper element [KHCP-6105]

--- src/components/KTruncate/KTruncate.vue
+++ src/components/KTruncate/KTruncate.vue
@@ -5,6 +5,30 @@
     :class="[expanded ? 'expanded' : '', `k-truncate-${isTextContent ? 'text' : 'content'}`]"
     :style="widthStyle"
   >
+    <!-- Order switched for ease when using keyboard navigation -->
+    <div
+      v-if="!isTextContent && showToggle"
+      class="d-flex h-100 align-items-end"
+    >
+      <div
+        v-if="!expanded"
+        data-testid="expand-trigger-wrapper"
+      >
+        <slot
+          :expand="handleToggleClick"
+          name="expand-trigger"
+          :truncated-count="truncatedCount"
+        >
+          <KButton
+            appearance="btn-link"
+            class="expand-trigger"
+            @click="handleToggleClick"
+          >
+            {{ truncatedCount }}
+          </KButton>
+        </slot>
+      </div>
+    </div>
     <div
       ref="kTruncateContainer"
       class="k-truncate-container"
@@ -30,29 +54,6 @@
               size="10"
               title="Show less"
             />
-          </KButton>
-        </slot>
-      </div>
-    </div>
-    <div
-      v-if="!isTextContent && showToggle"
-      class="d-flex h-100 align-items-end"
-    >
-      <div
-        v-if="!expanded"
-        data-testid="expand-trigger-wrapper"
-      >
-        <slot
-          :expand="handleToggleClick"
-          name="expand-trigger"
-          :truncated-count="truncatedCount"
-        >
-          <KButton
-            appearance="btn-link"
-            class="expand-trigger"
-            @click="handleToggleClick"
-          >
-            {{ truncatedCount }}
           </KButton>
         </slot>
       </div>
@@ -154,7 +155,7 @@ const setWrapperHeight = async (): Promise<void> => {
       // find height of tallest child
       tallestChildHeight = children[i].offsetHeight > tallestChildHeight ? children[i].offsetHeight : tallestChildHeight
     }
-    const targetWrapperHeight = (props.rows === 1 ? 0 : (props.rows - 1) * 10) + (tallestChildHeight * props.rows)
+    const targetWrapperHeight = (props.rows === 1 ? 0 : (props.rows - 1) * 10) + (tallestChildHeight * props.rows) + 6 // account for padding
     wrapperHeight.value = kTruncateContainer.value.offsetHeight > targetWrapperHeight ? `${targetWrapperHeight}px` : 'auto'
 
     await nextTick()
@@ -198,6 +199,15 @@ const countExcessElements = (): void => {
        */
       if (children[i].offsetTop > kTruncateWrapper.value.offsetHeight) {
         truncatedCount.value += 1
+        if (children[i].getAttribute('tabindex')) {
+          // set tabindex to -1 in truncated children
+          children[i].tabIndex = -1
+        }
+      } else {
+        if (children[i].getAttribute('tabindex')) {
+          // reset tabindex
+          children[i].tabIndex = 0
+        }
       }
     }
   }
@@ -236,6 +246,7 @@ onBeforeUnmount(() => {
   align-items: flex-start;
   display: flex;
   overflow: hidden;
+  padding: 3px;
 
   .expand-trigger,
   .collapse-trigger {
@@ -246,6 +257,8 @@ onBeforeUnmount(() => {
   }
 
   &.k-truncate-content {
+    display: flex;
+    flex-direction: row-reverse;
     height: v-bind('wrapperHeight');
 
     &.expanded {
@@ -271,6 +284,11 @@ onBeforeUnmount(() => {
 
       &:focus, &:active {
         box-shadow: none;
+      }
+
+      &:focus-within {
+        background-color: var(--KTruncateCollapseHover, var(--blue-200, color(blue-200)));
+        outline: -webkit-focus-ring-color auto 1px;
       }
     }
   }
