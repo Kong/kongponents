889f63b74b8138cf6c63c1755577ab1b18d3b288
Maksym Portianoi <portimaksym@gmail.com>
2023-03-28 14:33:47 -0400

    Revert "fix(kselect): add white-space: nowrap; to item label [KHCP-6433] (#1240)" (#1245)
    This reverts commit ef81956fc8c232645b50f95add0aa91b697e1ec5.

--- src/components/KSelect/KSelectItem.vue
+++ src/components/KSelect/KSelectItem.vue
@@ -107,10 +107,7 @@ export default defineComponent({
       font-size: 14px;
       font-weight: 500;
       line-height: 20px;
-      overflow: hidden;
       padding: 8px;
-      text-overflow: ellipsis;
-      white-space: nowrap;
       width: auto;
 
       :deep(.select-item-label) {
@@ -118,9 +115,6 @@ export default defineComponent({
         font-size: 14px;
         font-weight: 600;
         margin-bottom: 4px;
-        overflow: hidden;
-        text-overflow: ellipsis;
-        white-space: nowrap;
       }
 
       :deep(.select-item-desc) {
