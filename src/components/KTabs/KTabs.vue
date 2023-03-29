700c586ecc406da563da86b141ce76e85432b018
Philipp Rudloff <philipp.rudloff@konghq.com>
2023-03-29 15:37:00 +0200

    fix(ktabs): using non-semantic anchor for tab items (#1250)
    * chore(ktabs): expose types
    
    Moves types defined in `KTabs.vue` into a type definition file and exposes those types.
    
    Signed-off-by: Philipp Rudloff <philipp.rudloff@konghq.com>
    
    * fix(ktabs): using non-semantic anchor for tab items
    
    Replaces `a.tab-link` with `span.tab-link` to allow using `router-link`s for tab slots (nesting `a` is forbidden).
    
    Signed-off-by: Philipp Rudloff <philipp.rudloff@konghq.com>
    
    ---------
    
    Signed-off-by: Philipp Rudloff <philipp.rudloff@konghq.com>

--- src/components/KTabs/KTabs.vue
+++ src/components/KTabs/KTabs.vue
@@ -18,9 +18,9 @@
         @keydown.enter.prevent="handleTabChange(tab.hash)"
         @keydown.space.prevent="handleTabChange(tab.hash)"
       >
-        <a class="tab-link">
+        <div class="tab-link">
           <slot :name="`${tab.hash.replace('#','')}-anchor`">{{ tab.title }}</slot>
-        </a>
+        </div>
       </li>
     </ul>
 
@@ -44,10 +44,7 @@
 <script lang="ts">
 import { defineComponent, ref, watch, PropType } from 'vue'
 
-export interface Tab {
-  hash: string
-  title: string
-}
+import type { Tab } from '@/types'
 
 export default defineComponent({
   name: 'KTabs',
@@ -134,7 +131,6 @@ export default defineComponent({
 
     .tab-link {
       color: var(--KTabsColor, var(--black-45, color(black-45)));
-      display: block;
       &:hover {
         border: none;
         text-decoration: none;
