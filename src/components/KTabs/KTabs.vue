4e3d21129f69aef8eda07d381166b87c763a6557
Philipp Rudloff <philipp.rudloff@konghq.com>
2023-03-29 12:07:03 +0200

    chore(ktabs): expose types
    Moves types defined in `KTabs.vue` into a type definition file and exposes those types.
    
    Signed-off-by: Philipp Rudloff <philipp.rudloff@konghq.com>

--- src/components/KTabs/KTabs.vue
+++ src/components/KTabs/KTabs.vue
@@ -44,10 +44,7 @@
 <script lang="ts">
 import { defineComponent, ref, watch, PropType } from 'vue'
 
-export interface Tab {
-  hash: string
-  title: string
-}
+import type { Tab } from '@/types/tabs'
 
 export default defineComponent({
   name: 'KTabs',
---
+++ src/types/tabs.ts
@@ -0,0 +1,4 @@
+export interface Tab {
+  hash: string
+  title: string
+}
