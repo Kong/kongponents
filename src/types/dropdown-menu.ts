ac895ccd61c138ed06f29396739850318044819c
Daria <daria.yeromina@konghq.com>
2023-04-13 15:36:25 -0400

    feat(KDropdown): refactor dropdown menu item

--- src/types/dropdown-menu.ts
+++ src/types/dropdown-menu.ts
@@ -14,3 +14,19 @@ export const AppearanceArray = ['menu', 'selectionMenu'] as const
 export type Appearance = AnyElementOf<typeof AppearanceArray>
 
 export type DropdownItemType = 'link' | 'button' | 'default'
+
+export type DropdownItemRenderedType = 'link' | 'router-link' | 'button' | 'default'
+
+export interface IDropdownItemRenderedComponent {
+  tag: string;
+  onClick: ((event: Event) => void) | undefined;
+  attrs: {
+    class: string;
+    isRounded?: boolean;
+    disabled?: boolean;
+    to?: string | object | undefined;
+    href?: string;
+  }
+}
+
+export type DropdownItemRenderedRecord = Record<DropdownItemRenderedType, IDropdownItemRenderedComponent>
