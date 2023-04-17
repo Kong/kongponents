726ddfc269abbdde73d3558d780aa9df8f5e4b14
Adam DeHaven <adam.dehaven@konghq.com>
2023-04-17 09:36:46 -0400

    test(component): enable fail fast

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,10 +1,19 @@
 import { defineConfig } from 'cypress'
+import failFast from 'cypress-fail-fast/plugin'
 
 export default defineConfig({
+  env: {
+    FAIL_FAST_ENABLED: true,
+  },
   component: {
     devServer: {
       framework: 'vue',
       bundler: 'vite',
+    },
+    setupNodeEvents(on: Cypress.PluginEvents, config: Cypress.PluginConfigOptions) {
+      failFast(on, config)
+
+      return config
     },
     viewportHeight: 768,
     viewportWidth: 1366,
