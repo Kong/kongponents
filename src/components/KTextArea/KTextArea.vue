7589cd0ccb2170207c483161c3c0c0131a762876
Daria <daria.yeromina@konghq.com>
2023-05-05 15:08:19 -0400

    feat(KTextArea): switch to setup syntax [KHCP-6521]

--- src/components/KTextArea/KTextArea.vue
+++ src/components/KTextArea/KTextArea.vue
@@ -86,145 +86,141 @@
   </div>
 </template>
 
-<script lang="ts">
-import { defineComponent, ref, computed, watch } from 'vue'
+<script lang="ts" setup>
+import { ref, computed, watch, useAttrs } from 'vue'
 import { v1 as uuidv1 } from 'uuid'
 import useUtilities from '@/composables/useUtilities'
 import KLabel from '@/components/KLabel/KLabel.vue'
 
-const CHARACTER_LIMIT = 2048
-
-export default defineComponent({
-  name: 'KTextArea',
-  components: { KLabel },
-  inheritAttrs: false,
-  props: {
-    modelValue: {
-      type: String,
-      default: '',
-    },
-    label: {
-      type: String,
-      default: '',
-    },
-    overlayLabel: {
-      type: Boolean,
-      default: false,
-    },
-    labelAttributes: {
-      type: Object,
-      default: () => ({}),
-    },
-    characterLimit: {
-      type: Number,
-      default: CHARACTER_LIMIT,
-      // Ensure the characterLimit is greater than zero
-      validator: (limit: number):boolean => limit > 0,
-    },
-    disableCharacterLimit: {
-      type: Boolean,
-      default: false,
-    },
-    rows: {
-      type: Number,
-      default: 5,
-    },
-    cols: {
-      type: Number,
-      default: 52,
-    },
-    hasError: {
-      type: Boolean,
-      default: false,
-    },
-    /**
+const props = defineProps({
+  modelValue: {
+    type: String,
+    default: '',
+  },
+  label: {
+    type: String,
+    default: '',
+  },
+  overlayLabel: {
+    type: Boolean,
+    default: false,
+  },
+  labelAttributes: {
+    type: Object,
+    default: () => ({}),
+  },
+  characterLimit: {
+    type: Number,
+    default: 2048,
+    // Ensure the characterLimit is greater than zero
+    validator: (limit: number):boolean => limit > 0,
+  },
+  disableCharacterLimit: {
+    type: Boolean,
+    default: false,
+  },
+  rows: {
+    type: Number,
+    default: 5,
+  },
+  cols: {
+    type: Number,
+    default: 52,
+  },
+  hasError: {
+    type: Boolean,
+    default: false,
+  },
+  /**
      * Test mode - for testing only, strips out generated ids
      */
-    testMode: {
-      type: Boolean,
-      default: false,
-    },
-    isResizable: {
-      type: Boolean,
-      default: false,
-    },
+  testMode: {
+    type: Boolean,
+    default: false,
   },
-  emits: ['input', 'update:modelValue', 'char-limit-exceeded'],
-  setup(props, { attrs, emit }) {
-    const { stripRequiredLabel } = useUtilities()
-
-    const isRequired = computed((): boolean => attrs?.required !== undefined && String(attrs?.required) !== 'false')
-    const currValue = ref('') // We need this so that we don't lose the updated value on hover/blur event with label
-    const isFocused = ref(false)
-    const isHovered = ref(false)
-    const strippedLabel = computed((): string => stripRequiredLabel(props.label, isRequired.value))
-    // we need this so we can create a watcher for programmatic changes to the modelValue
-    const value = computed({
-      get(): string | number {
-        return props.modelValue
-      },
-      set(newValue: string | number): void {
-        inputHandler({ target: { value: newValue } })
-      },
-    })
-
-    const textAreaId = computed((): string => (attrs.id ? String(attrs.id) : props.testMode ? 'test-textArea-id-1234' : uuidv1()))
-
-    const modifiedAttrs = computed(() => {
-      const $attrs = { ...attrs }
-
-      // delete classes because we bind them to the parent
-      delete $attrs.class
-
-      return $attrs
-    })
-
-    const charLimitExceeded = computed((): boolean => !props.disableCharacterLimit && currValue.value.length > props.characterLimit)
-
-    const inputHandler = (e: any) => {
-      // avoid pass by ref
-      const val = JSON.parse(JSON.stringify(e?.target?.value))
-      // this 'input' and 'update:modelValue' events must be emitted for v-model binding to work properly
-      emit('input', val)
-      emit('update:modelValue', val)
-      currValue.value = val
-    }
-
-    watch(charLimitExceeded, (newVal, oldVal) => {
-      if (newVal !== oldVal) {
-        emit('char-limit-exceeded', {
-          value: currValue.value,
-          length: currValue.value.length,
-          characterLimit: props.characterLimit,
-          limitExceeded: newVal,
-        })
-      }
-    })
-
-    watch(value, (newVal, oldVal) => {
-      if (newVal !== oldVal) {
-        inputHandler({ target: { value: newVal } })
-      }
-    })
-
-    const getValue = (): string => {
-      return currValue.value ? currValue.value : props.modelValue
-    }
-
-    return {
-      isRequired,
-      currValue,
-      isFocused,
-      isHovered,
-      textAreaId,
-      strippedLabel,
-      modifiedAttrs,
-      charLimitExceeded,
-      inputHandler,
-      getValue,
-    }
+  isResizable: {
+    type: Boolean,
+    default: false,
   },
 })
+const emit = defineEmits<{
+  (e: 'input', val: string): void
+  (e: 'update:modelValue', val: string): void
+  (e: 'char-limit-exceeded', data: {
+    value: string,
+    length: number,
+    characterLimit: number,
+    limitExceeded: boolean,
+  }): void
+}>()
+
+const attrs = useAttrs()
+
+const { stripRequiredLabel } = useUtilities()
+
+const isRequired = computed((): boolean => attrs?.required !== undefined && String(attrs?.required) !== 'false')
+const currValue = ref('') // We need this so that we don't lose the updated value on hover/blur event with label
+const isFocused = ref(false)
+const isHovered = ref(false)
+const strippedLabel = computed((): string => stripRequiredLabel(props.label, isRequired.value))
+// we need this so we can create a watcher for programmatic changes to the modelValue
+const value = computed({
+  get(): string | number {
+    return props.modelValue
+  },
+  set(newValue: string | number): void {
+    inputHandler({ target: { value: newValue } })
+  },
+})
+
+const textAreaId = computed((): string => (attrs.id ? String(attrs.id) : props.testMode ? 'test-textArea-id-1234' : uuidv1()))
+
+const modifiedAttrs = computed(() => {
+  const $attrs = { ...attrs }
+
+  // delete classes because we bind them to the parent
+  delete $attrs.class
+
+  return $attrs
+})
+
+const charLimitExceeded = computed((): boolean => !props.disableCharacterLimit && currValue.value.length > props.characterLimit)
+
+const inputHandler = (e: any) => {
+  // avoid pass by ref
+  const val = JSON.parse(JSON.stringify(e?.target?.value))
+  // this 'input' and 'update:modelValue' events must be emitted for v-model binding to work properly
+  emit('input', val)
+  emit('update:modelValue', val)
+  currValue.value = val
+}
+
+watch(charLimitExceeded, (newVal, oldVal) => {
+  if (newVal !== oldVal) {
+    emit('char-limit-exceeded', {
+      value: currValue.value,
+      length: currValue.value.length,
+      characterLimit: props.characterLimit,
+      limitExceeded: newVal,
+    })
+  }
+})
+
+watch(value, (newVal, oldVal) => {
+  if (newVal !== oldVal) {
+    inputHandler({ target: { value: newVal } })
+  }
+})
+
+const getValue = (): string => {
+  return currValue.value ? currValue.value : props.modelValue
+}
+</script>
+
+<script lang="ts">
+export default {
+  inheritAttrs: false,
+}
 </script>
 
 <style lang="scss" scoped>
