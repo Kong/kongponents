0af1e573979e87e2f400cb66066b4223deb6a4b2
DariaYeremina <daria.yeromina@konghq.com>
2023-05-26 16:55:39 -0400

    feat(KIcon): switch to setup syntax [KHCP-6503] (#1415)

--- src/components/KIcon/KIcon.vue
+++ src/components/KIcon/KIcon.vue
@@ -25,233 +25,224 @@
   </span>
 </template>
 
-<script lang="ts">
-import { defineComponent, ref, computed, onMounted, nextTick, watch } from 'vue'
+<script lang="ts" setup>
+import { ref, computed, onMounted, nextTick, watch, useAttrs, useSlots } from 'vue'
 import * as allIcons from './icons'
 
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-ignore
 const icons: Array<string, any[]> = allIcons
-const iconNames = Object.keys(icons)
 
 const DEFAULTS = {
   size: '24',
   viewBox: '0 0 24 24',
 }
 
-export default defineComponent({
-  name: 'KIcon',
-  inheritAttrs: false,
-  props: {
-    /**
+const props = defineProps({
+  /**
      * Checks for valid icon name
      */
-    icon: {
-      type: String,
-      validator: (value: string): boolean => {
-        return iconNames.includes(value)
-      },
-      required: true,
+  icon: {
+    type: String,
+    validator: (value: string): boolean => {
+      return Object.keys(allIcons).includes(value)
     },
-    /**
+    required: true,
+  },
+  /**
      * Optional - Overrides default height and width with equal value
      */
-    size: {
-      type: String,
-      default: '',
-    },
-    /**
+  size: {
+    type: String,
+    default: '',
+  },
+  /**
      * Optional - Sets Fill color
      */
-    color: {
-      type: String,
-      default: null,
-    },
-    secondaryColor: {
-      type: String,
-      default: null,
-    },
-    /**
+  color: {
+    type: String,
+    default: null,
+  },
+  secondaryColor: {
+    type: String,
+    default: null,
+  },
+  /**
      * Optional - Defines viewbox dimensions
      */
-    viewBox: {
-      type: String,
-      default: '',
-    },
-    /**
+  viewBox: {
+    type: String,
+    default: '',
+  },
+  /**
      * Optional - Replaces default title attribute
      */
-    title: {
-      type: String,
-      default: '',
-    },
-    /**
+  title: {
+    type: String,
+    default: '',
+  },
+  /**
      * Optional - Prevents title from being shown on hover. Used by KTooltip
      */
-    hideTitle: {
-      type: Boolean,
-      default: false,
-    },
-    /**
+  hideTitle: {
+    type: Boolean,
+    default: false,
+  },
+  /**
      * If testMode enabled use the icon name for the title so we can test
      */
-    testMode: {
-      type: Boolean,
-      default: false,
-    },
+  testMode: {
+    type: Boolean,
+    default: false,
   },
-  setup(props, { attrs, slots }) {
-    const svgWrapper = ref<HTMLElement>()
-    const svg = ref<any>()
-    const svgWithSlotIsHidden = ref(true)
+})
 
-    const titleText = computed((): string => {
-      if (props.title) { // use title prop if they provided
-        return props.title
-      }
+const attrs = useAttrs()
+const slots = useSlots()
 
-      if (props.testMode) {
-        return props.icon
-      }
+const svgWrapper = ref<HTMLElement>()
+const svg = ref<any>()
+const svgWithSlotIsHidden = ref(true)
 
-      const titleElems: any = svg.value && svg.value.getElementsByTagName('title')
-      if (titleElems && titleElems.length) { // use title in SVG if it exists
-        return titleElems[0].innerHTML
-      }
+const titleText = computed((): string => {
+  if (props.title) { // use title prop if they provided
+    return props.title
+  }
 
-      const icnName = props.icon.split(/(?=[A-Z])/).join(' ') // split on capital letters in icon name and add space
+  if (props.testMode) {
+    return props.icon
+  }
 
-      return convertToTitleCase(icnName)
-    })
+  const titleElems: any = svg.value && svg.value.getElementsByTagName('title')
+  if (titleElems && titleElems.length) { // use title in SVG if it exists
+    return titleElems[0].innerHTML
+  }
 
-    const width = computed((): string | null => svg.value ? svg.value.getAttribute('width') : null)
+  const icnName = props.icon.split(/(?=[A-Z])/).join(' ') // split on capital letters in icon name and add space
 
-    const height = computed((): string | null => svg.value ? svg.value.getAttribute('height') : null)
+  return convertToTitleCase(icnName)
+})
 
-    const setSize = computed((): string => svg.value ? (props.size || (svg.value && svg.value.getAttribute('width')) || DEFAULTS.size) : DEFAULTS.size)
+const width = computed((): string | null => svg.value ? svg.value.getAttribute('width') : null)
 
-    const setViewbox = computed((): string => svg.value ? (props.viewBox || (svg.value && svg.value.getAttribute('viewBox')) || DEFAULTS.viewBox) : DEFAULTS.viewBox)
+const height = computed((): string | null => svg.value ? svg.value.getAttribute('height') : null)
 
-    const convertToTitleCase = (str: string): string => {
-      return str.split('-').map(i => i.charAt(0).toUpperCase() + i.substring(1)).join(' ')
+const setSize = computed((): string => svg.value ? (props.size || (svg.value && svg.value.getAttribute('width')) || DEFAULTS.size) : DEFAULTS.size)
+
+const setViewbox = computed((): string => svg.value ? (props.viewBox || (svg.value && svg.value.getAttribute('viewBox')) || DEFAULTS.viewBox) : DEFAULTS.viewBox)
+
+const convertToTitleCase = (str: string): string => {
+  return str.split('-').map(i => i.charAt(0).toUpperCase() + i.substring(1)).join(' ')
+}
+
+const addSlotContent = (): void => {
+  // Get slot content
+  const slotContent = svgWrapper?.value?.querySelector('.slot-content')?.innerHTML
+
+  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+  // @ts-ignore
+  svgWrapper.value?.removeChild(svgWrapper?.value?.querySelector('.slot-content'))
+
+  // Move slot content inside svg
+  svg.value.innerHTML += slotContent
+
+  // Done rendering slot, so set hidden to false
+  svgWithSlotIsHidden.value = false
+}
+
+const setSvgTitle = (): void => {
+  let svgTitle = svg.value.querySelector('title')
+  // Hide title
+  if (svgTitle && props.hideTitle) {
+    svg.value.removeChild(svgTitle)
+  } else if (!props.hideTitle) {
+    if (!svgTitle) {
+      // No title element exists, create one
+      svgTitle = document.createElement('title')
+      // Set title
+      const svgTitleText = document.createTextNode(titleText.value)
+
+      svgTitle.appendChild(svgTitleText)
+      svg.value.append(svgTitle)
+    } else {
+      svgTitle.textContent = titleText.value
+    }
+  }
+}
+
+const recursivelyCustomizeIconColors = (el: any): void => {
+  if (!el) {
+    return
+  }
+
+  const fillValue = el.hasAttribute('fill')
+  const strokeValue = el.getAttribute('stroke') && el.getAttribute('stroke') !== 'none' ? el.getAttribute('stroke') : null
+  const hasPreservedColor = el.attributes.id && el.attributes.id.value === 'preserveColor'
+  const isSecondary = el.attributes.type && el.attributes.type.value === 'secondary'
+
+  if (!hasPreservedColor && fillValue && isSecondary && props.secondaryColor) {
+    el.setAttribute('fill', props.secondaryColor)
+  } else if (!hasPreservedColor && strokeValue && isSecondary && props.secondaryColor) {
+    el.setAttribute('stroke', props.secondaryColor)
+  } else if (!hasPreservedColor && !isSecondary && fillValue && !strokeValue && props.color) {
+    el.setAttribute('fill', props.color)
+  } else if (strokeValue && props.color) {
+    el.setAttribute('stroke', props.color)
+  }
+
+  for (const child of el.children) {
+    recursivelyCustomizeIconColors(child)
+  }
+}
+
+// Re-render the svg if the icon prop changes
+watch(() => [props.icon, props.size, props.color, props.secondaryColor, props.viewBox, props.hideTitle], async () => {
+  await nextTick()
+
+  renderIcon()
+}, { deep: true })
+
+const renderIcon = (): void => {
+  svg.value = null
+
+  // Set svg
+  svg.value = svgWrapper.value ? svgWrapper.value.querySelector('svg:not(.slot-content)') : null
+
+  if (svg.value) {
+    // Check for slot content
+    if (slots.svgElements) {
+      addSlotContent()
     }
 
-    const addSlotContent = (): void => {
-      // Get slot content
-      const slotContent = svgWrapper?.value?.querySelector('.slot-content')?.innerHTML
-
-      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-      // @ts-ignore
-      svgWrapper.value?.removeChild(svgWrapper?.value?.querySelector('.slot-content'))
-
-      // Move slot content inside svg
-      svg.value.innerHTML += slotContent
-
-      // Done rendering slot, so set hidden to false
-      svgWithSlotIsHidden.value = false
-    }
-
-    const setSvgTitle = (): void => {
-      let svgTitle = svg.value.querySelector('title')
-      // Hide title
-      if (svgTitle && props.hideTitle) {
-        svg.value.removeChild(svgTitle)
-      } else if (!props.hideTitle) {
-        if (!svgTitle) {
-          // No title element exists, create one
-          svgTitle = document.createElement('title')
-          // Set title
-          const svgTitleText = document.createTextNode(titleText.value)
-
-          svgTitle.appendChild(svgTitleText)
-          svg.value.append(svgTitle)
-        } else {
-          svgTitle.textContent = titleText.value
-        }
-      }
-    }
-
-    const recursivelyCustomizeIconColors = (el: any): void => {
-      if (!el) {
-        return
-      }
-
-      const fillValue = el.hasAttribute('fill')
-      const strokeValue = el.getAttribute('stroke') && el.getAttribute('stroke') !== 'none' ? el.getAttribute('stroke') : null
-      const hasPreservedColor = el.attributes.id && el.attributes.id.value === 'preserveColor'
-      const isSecondary = el.attributes.type && el.attributes.type.value === 'secondary'
-
-      if (!hasPreservedColor && fillValue && isSecondary && props.secondaryColor) {
-        el.setAttribute('fill', props.secondaryColor)
-      } else if (!hasPreservedColor && strokeValue && isSecondary && props.secondaryColor) {
-        el.setAttribute('stroke', props.secondaryColor)
-      } else if (!hasPreservedColor && !isSecondary && fillValue && !strokeValue && props.color) {
-        el.setAttribute('fill', props.color)
-      } else if (strokeValue && props.color) {
-        el.setAttribute('stroke', props.color)
-      }
-
-      for (const child of el.children) {
-        recursivelyCustomizeIconColors(child)
-      }
-    }
-
-    // Re-render the svg if the icon prop changes
-    watch(() => [props.icon, props.size, props.color, props.secondaryColor, props.viewBox, props.hideTitle], async () => {
-      await nextTick()
-
-      renderIcon()
-    }, { deep: true })
-
-    const renderIcon = (): void => {
-      svg.value = null
-
-      // Set svg
-      svg.value = svgWrapper.value ? svgWrapper.value.querySelector('svg:not(.slot-content)') : null
-
-      if (svg.value) {
-        // Check for slot content
-        if (slots.svgElements) {
-          addSlotContent()
-        }
-
-        // Bind attributes
-        for (const [attributeName, attributeValue] of Object.entries(attrs)) {
-          if (!['class', 'id', 'style'].includes(attributeName)) {
-            // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-            // @ts-ignore
-            svg.value.setAttribute(attributeName, attributeValue)
-          }
-        }
-
-        // Add role
-        svg.value.setAttribute('role', 'img')
-
-        // Set size
-        svg.value.setAttribute('width', setSize.value || width.value)
-        svg.value.setAttribute('height', setSize.value || height.value)
-        svg.value.setAttribute('viewBox', setViewbox.value)
-
-        // Set title
-        setSvgTitle()
-
-        // Customize icon colors
-        recursivelyCustomizeIconColors(svg.value)
+    // Bind attributes
+    for (const [attributeName, attributeValue] of Object.entries(attrs)) {
+      if (!['class', 'id', 'style'].includes(attributeName)) {
+        // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+        // @ts-ignore
+        svg.value.setAttribute(attributeName, attributeValue)
       }
     }
 
-    onMounted(async () => {
-      await nextTick()
+    // Add role
+    svg.value.setAttribute('role', 'img')
 
-      renderIcon()
-    })
+    // Set size
+    svg.value.setAttribute('width', setSize.value || width.value)
+    svg.value.setAttribute('height', setSize.value || height.value)
+    svg.value.setAttribute('viewBox', setViewbox.value)
 
-    return {
-      icons,
-      svgWrapper,
-      svgWithSlotIsHidden,
-    }
-  },
+    // Set title
+    setSvgTitle()
+
+    // Customize icon colors
+    recursivelyCustomizeIconColors(svg.value)
+  }
+}
+
+onMounted(async () => {
+  await nextTick()
+
+  renderIcon()
 })
 </script>
 
