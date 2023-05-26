689cbb942f11512bef6ed495b19df949d3b7f731
Daria <daria.yeromina@konghq.com>
2023-05-26 16:39:32 -0400

    feat(KPagination): remove commas [KHCP-6510]

--- src/types/pagination.ts
+++ src/types/pagination.ts
@@ -2,13 +2,13 @@ export type PaginationType = 'default' | 'offset'
 
 export interface PageChangedData {
   page: number
-  pageCount: number,
-  firstItem: number,
-  lastItem: number,
-  visibleItems: any[],
+  pageCount: number
+  firstItem: number
+  lastItem: number
+  visibleItems: any[]
 }
 
 export interface PageSizeChangedData {
-  pageSize: number,
+  pageSize: number
   pageCount: number
 }
