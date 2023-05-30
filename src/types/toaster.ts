320ea8e1bc6342b5681bbe7596377dad0559209b
DariaYeremina <daria.yeromina@konghq.com>
2023-05-30 09:51:39 -0400

    feat(KToaster): switch to setup syntax [KHCP-6522] (#1418)
    * feat(KToaster): switch to setup syntax [KHCP-6522]
    
    * feat(KToaster): fix type error [KHCP-6522]

---
+++ src/types/toaster.ts
@@ -0,0 +1,12 @@
+import { AlertAppearance, AlertAppearanceRecord } from '@/types/alert'
+import { appearances } from '@/components/KAlert/KAlert.vue'
+
+export interface Toast {
+  key?: any // unique identifier of toaster
+  appearance?: AlertAppearance
+  message: string // Text to display in toaster
+  timer?: any
+  timeoutMilliseconds?: number
+}
+
+export const toasterAppearances: AlertAppearanceRecord = appearances
