d853fa891f7b30ee0dacc9057177ccb8f46388b0
Daria <daria.yeromina@konghq.com>
2023-06-12 11:21:04 -0400

    feat(KInlineEdit): switch to setup syntax [KHCP-6504]

---
+++ src/types/inline-edit.ts
@@ -0,0 +1,9 @@
+// Styles we want copied from the element
+export enum Styles {
+  fontSize = 'font-size',
+  fontWeight = 'font-weight',
+  fontFamily = 'font-family',
+  color = 'color',
+  margin = 'margin',
+  padding = 'padding',
+}
