ea47855f7441fb224849df9dcd61c56b37ea0037
DariaYeremina <daria.yeromina@konghq.com>
2023-07-07 10:38:17 -0400

    fix(KTruncate, KMultiselect): add fix for ResizeObserver [KHCP-7734] (#1508)
    * fix(KTruncate, KMultiselect): add fix for ResizeObserver [KHCP-7734]
    
    * fix(KTruncate, KMultiselect): add comments [KHCP-7734]

--- src/components/KTruncate/KTruncate.vue
+++ src/components/KTruncate/KTruncate.vue
@@ -227,7 +227,17 @@ const widthStyle = computed((): Record<string, string> => {
 })
 
 onMounted(() => {
-  resizeObserver.value = new ResizeObserver(setWrapperHeight).observe(kTruncateContainer.value as HTMLDivElement)
+  resizeObserver.value = new ResizeObserver(entries => {
+    // Wrapper 'window.requestAnimationFrame' is needed for disabling "ResizeObserver loop limit exceeded" error in DD
+    window.requestAnimationFrame(() => {
+      if (!Array.isArray(entries) || !entries.length) {
+        return
+      }
+      // Actual code
+      setWrapperHeight()
+    })
+  })
+  resizeObserver.value.observe(kTruncateContainer.value as HTMLDivElement)
   updateToggleVisibility()
 })
 
