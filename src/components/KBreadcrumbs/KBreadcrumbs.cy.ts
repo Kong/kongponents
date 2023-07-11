43c9f4f462c0e2eca68cca3be2e0294970289e98
kai.arrowood <kai.arrowood@konghq.com>
2023-07-11 13:58:31 -0400

    test(kbreadcrumb): add test

--- src/components/KBreadcrumbs/KBreadcrumbs.cy.ts
+++ src/components/KBreadcrumbs/KBreadcrumbs.cy.ts
@@ -20,6 +20,29 @@ describe('KBreadcrumbs', () => {
     cy.get('.k-breadcrumbs').find('li .k-breadcrumb-icon').its('length').should('eq', 1)
   })
 
+  it('renders custom divider when using slot', () => {
+    const customDivider = 'custom_divider'
+
+    mount(KBreadcrumbs, {
+      props: {
+        items: [
+          {
+            key: 'docs',
+            to: 'https://docs.konghq.com',
+            title: 'Go to Kong Docs',
+            icon: 'kong',
+          },
+        ],
+      },
+      slots: {
+        divider: customDivider,
+      },
+    })
+
+    cy.get('.k-breadcrumbs').find('li').its('length').should('eq', 1)
+    cy.get('.k-breadcrumbs .k-breadcrumb-divider').should('contain.text', customDivider)
+  })
+
   it('renders breadcrumb links without needing a router', () => {
     mount(KBreadcrumbs, {
       props: {
