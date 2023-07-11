d12b5b778f57c942d3b9e2cbdebf8c4247660bbf
portikM <maksym.portianoi@konghq.com>
2023-07-11 09:42:16 -0400

    fix(kbadge): types [KHCP-7694]

--- src/types/badge.ts
+++ src/types/badge.ts
@@ -3,3 +3,18 @@ export type BadgeAppearanceRecord = Record<BadgeAppearance, BadgeAppearance>
 
 export type BadgeShape = 'rounded' | 'rectangular'
 export type BadgeShapeRecord = Record<BadgeShape, BadgeShape>
+
+export const appearances: BadgeAppearanceRecord = {
+  default: 'default',
+  success: 'success',
+  danger: 'danger',
+  info: 'info',
+  warning: 'warning',
+  custom: 'custom',
+  neutral: 'neutral',
+}
+
+export const shapes: BadgeShapeRecord = {
+  rounded: 'rounded',
+  rectangular: 'rectangular',
+}
