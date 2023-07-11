86b70236db4b733a5b46a34a7a0cc15f20e6ba96
Maksym Portianoi <portimaksym@gmail.com>
2023-07-11 15:50:08 -0400

    feat(kbadge): introduce kui tokens [KHCP-7694] (#1511)
    * feat(kbadge): introduce kui tokens [KHCP-7694]
    
    * fix(kbadge): break down utility classes [KHCP-7694]
    
    * docs(badge): break down utility classes [KHCP-7694]
    
    * fix(kbadge): move const export to types [KHCP-7694]
    
    * fix(kbadge): remove comments [KHCP-7694]
    
    * fix(kbadge): types [KHCP-7694]
    
    * fix(kbadge): address PR feedback [KHCP-7694]
    
    * fix(kbadge): address PR feedback [KHCP-7694]
    
    Co-authored-by: Adam DeHaven <2229946+adamdehaven@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Adam DeHaven <2229946+adamdehaven@users.noreply.github.com>

--- src/types/badge.ts
+++ src/types/badge.ts
@@ -3,3 +3,18 @@ export type BadgeAppearanceRecord = Record<BadgeAppearance, BadgeAppearance>
 
 export type BadgeShape = 'rounded' | 'rectangular'
 export type BadgeShapeRecord = Record<BadgeShape, BadgeShape>
+
+export const BadgeAppearances: BadgeAppearanceRecord = {
+  default: 'default',
+  success: 'success',
+  danger: 'danger',
+  info: 'info',
+  warning: 'warning',
+  custom: 'custom',
+  neutral: 'neutral',
+} as const
+
+export const BadgeShapes: BadgeShapeRecord = {
+  rounded: 'rounded',
+  rectangular: 'rectangular',
+} as const
