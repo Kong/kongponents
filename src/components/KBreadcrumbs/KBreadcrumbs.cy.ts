8d8ae253fc6aee26048a27e65b9a7137ef228a77
kai.arrowood <kai.arrowood@konghq.com>
2023-07-14 12:30:15 -0400

    test(kbreadcrumbs): add tests

--- src/components/KBreadcrumbs/KBreadcrumbs.cy.ts
+++ src/components/KBreadcrumbs/KBreadcrumbs.cy.ts
@@ -20,6 +20,47 @@ describe('KBreadcrumbs', () => {
     cy.get('.k-breadcrumbs').find('li .k-breadcrumb-icon').its('length').should('eq', 1)
   })
 
+  it('renders with emphasis', () => {
+    mount(KBreadcrumbs, {
+      props: {
+        items: [
+          {
+            key: 'docs',
+            to: 'https://docs.konghq.com',
+            title: 'Go to Kong Docs',
+            icon: 'kong',
+          },
+        ],
+        emphasis: true,
+      },
+    })
+
+    cy.get('.k-breadcrumbs').find('li').its('length').should('eq', 1)
+    cy.get('.k-breadcrumbs').find('.k-breadcrumb-text.emphasis').its('length').should('eq', 1)
+  })
+
+  it('correctly renders an non-link breadcrumbs', () => {
+    mount(KBreadcrumbs, {
+      props: {
+        items: [
+          {
+            key: 'docs',
+            title: 'Go to Kong Docs',
+            icon: 'kong',
+          },
+          {
+            key: 'specific-doc',
+            title: 'My Doc',
+          },
+        ],
+      },
+    })
+
+    cy.get('.k-breadcrumbs').find('li').its('length').should('eq', 2)
+    cy.get('.k-breadcrumbs').find('.k-breadcrumb-text.non-link').its('length').should('eq', 2)
+    cy.get('.k-breadcrumbs').find('.k-breadcrumb-divider').its('length').should('eq', 1)
+  })
+
   it('renders custom divider when using slot', () => {
     const customDivider = 'custom_divider'
 
