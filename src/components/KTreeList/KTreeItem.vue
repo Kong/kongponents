47382f7681652bf267f3fdb4f60dfc4f48b969bf
kai.arrowood <kai.arrowood@konghq.com>
2023-07-25 16:04:43 -0400

    feat(ktreelist): tokenization [khcp-7731]

--- src/components/KTreeList/KTreeItem.vue
+++ src/components/KTreeList/KTreeItem.vue
@@ -12,14 +12,14 @@
   >
     <div
       v-if="hasIcon"
-      class="k-tree-item-icon mr-2"
+      class="k-tree-item-icon"
       data-testid="k-tree-item-icon"
     >
       <slot name="item-icon">
         <KIcon
           :icon="itemIcon"
           :secondary-color="iconSecondaryColor"
-          size="20"
+          :size="KUI_ICON_SIZE_40"
         />
       </slot>
     </div>
@@ -36,8 +36,9 @@
 
 <script lang="ts">
 import { computed, PropType, useSlots } from 'vue'
-import KIcon from '@/components/KIcon/KIcon.vue'
 import { TreeListItem } from '@/types'
+import { KUI_ICON_SIZE_40 } from '@kong/design-tokens'
+import KIcon from '@/components/KIcon/KIcon.vue'
 
 export const itemsHaveRequiredProps = (items: TreeListItem[]): boolean => {
   return items.every(i => i.name !== undefined && i.id !== undefined && (!i.children?.length || itemsHaveRequiredProps(i.children)))
@@ -68,7 +69,9 @@ const itemIcon = computed((): string => props.item.icon ? props.item.icon : 'doc
 
 const iconSecondaryColor = (): string | undefined => {
   if (itemIcon.value === 'documentList') {
-    return props.item.selected ? 'var(--KTreeListItemSelectedBorder, var(--teal-200))' : 'var(--KTreeListItemUnselectedBorder, var(--grey-200))'
+    return props.item.selected
+      ? 'var(--KTreeListItemSelectedBorder, $tmp-color-teal-200)'
+      : 'var(--KTreeListItemUnselectedBorder, var(--kui-color-background-neutral-weaker, $kui-color-background-neutral-weaker))'
   }
 
   return undefined
@@ -80,30 +83,31 @@ const handleClick = () => {
 </script>
 
 <style lang="scss" scoped>
-@import '@/styles/variables';
 @import '@/styles/functions';
+@import '@/styles/tmp-variables';
 
 .k-tree-item {
   align-items: center;
-  background-color: var(--KTreeListItemUnselectedBackground, var(--white));
-  border: 1px solid var(--KTreeListItemUnselectedBorder, var(--grey-200));
-  border-radius: 8px;
-  color: var(--KTreeListItemText, var(--black-500));
+  background-color: var(--KTreeListItemUnselectedBackground, var(--kui-color-background, $kui-color-background));
+  border: $kui-border-width-10 solid var(--KTreeListItemUnselectedBorder, var(--kui-color-background-neutral-weaker, $kui-color-background-neutral-weaker));
+  border-radius: $kui-border-radius-40;
+  color: var(--KTreeListItemText, var(--kui-color-text-neutral-strongest, $kui-color-text-neutral-strongest));
   display: flex;
-  padding: 4px;
+  padding: $kui-space-20;
   text-decoration: none;
 
   .k-tree-item-icon {
     line-height: 1;
+    margin-right: $kui-space-40 !important;
   }
 
   &.selected {
-    background-color: var(--KTreeListItemSelectedBackground, var(--teal-100));
-    border-color: var(--KTreeListItemSelectedBorder, var(--teal-200));
+    background-color: var(--KTreeListItemSelectedBackground, $tmp-color-teal-100);
+    border-color: var(--KTreeListItemSelectedBorder, $tmp-color-teal-200);
   }
 
   &:hover {
-    color: var(--KTreeListItemText, var(--black-500));
+    color: var(--KTreeListItemText, var(--kui-color-text-neutral-strongest, $kui-color-text-neutral-strongest));
     cursor: grab;
   }
 
