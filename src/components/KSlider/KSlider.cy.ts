4d5fb545c7a3e7f14f7c9f30783ca3469841324b
Jilson Thomas <jilson@live.in>
2023-07-28 14:35:35 -0400

    chore(*): remove old component

--- src/components/KSlider/KSlider.cy.ts
+++
@@ -1,8 +0,0 @@
-import { mount } from 'cypress/vue'
-import KSlider from '@/components/KSlider/KSlider.vue'
-
-describe('KSlider', () => {
-  it('TODO: This is just an example test', () => {
-    mount(KSlider)
-  })
-})
