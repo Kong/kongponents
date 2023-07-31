433c242bdc0e99e99356b52d98d74778696741e0
Adam DeHaven <adam.dehaven@konghq.com>
2023-07-31 17:39:19 -0400

    ci(eslint): add deprecated class rules

--- .stylelintrc.cjs
+++ .stylelintrc.cjs
@@ -14,8 +14,16 @@ module.exports = {
         'unit-disallowed-list': [
           ['rem', 'em'],
         ],
+        // Only allow @kong/design-tokens or Kongponents theming CSS custom properties
+        // TODO: Remove `K` pattern here when theming properties are removed in the breaking release
+        'custom-property-pattern': [
+          "^(kui-|K).+$",
+          {
+            message: "Expected custom property \"%s\" to be sourced from @kong/design-tokens with prefix '--kui-' or have prefix '--K' as a local theming variable",
+          }
+        ],
+        'custom-property-no-missing-var-function': true,
         // Disable the following rules
-        'custom-property-no-missing-var-function': null,
         'no-descending-specificity': null,
         'no-extra-semicolons': null,
       }
