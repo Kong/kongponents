df01f804a58b5b67cce25872f1a895bbd8de3b71
Adam DeHaven <2229946+adamdehaven@users.noreply.github.com>
2023-09-07 16:30:02 -0400

    chore: versioning prep (#1681)

--- commitlint.config.cjs
+++ commitlint.config.cjs
@@ -4,5 +4,6 @@ module.exports = {
     'header-max-length': [2, 'always', 100],
     'body-max-line-length': [1, 'always', 150],
   },
+  ignores: [(message) => /^chore\(release\): .+$/m.test(message)],
   helpUrl: 'https://kongponents.konghq.com/contributing/getting-started.html',
 }
