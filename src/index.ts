9deb254a87a972e800c4614cc79d437ec88a7154
Adam DeHaven <adam.dehaven@konghq.com>
2023-09-28 16:45:10 -0400

    revert: back out comment

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,6 @@ import type { App } from 'vue'
 import * as components from './components' // Import all components
 import './styles/styles.scss' // Import all styles
 
-// COMMENT TEST
-
 // Export install function
 export default {
   install: (app: App): void => {
