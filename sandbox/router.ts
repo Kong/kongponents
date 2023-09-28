8e3f72e34d7b3d0938a2f73ea86c6fad6f970360
Adam DeHaven <adam.dehaven@konghq.com>
2023-09-28 11:15:25 -0400

    chore: use router hash mode

--- sandbox/router.ts
+++ sandbox/router.ts
@@ -1,7 +1,7 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 
 export default createRouter({
-  history: createWebHistory('/kongponents'),
+  history: createWebHashHistory(),
   routes: [
     {
       path: '/',
