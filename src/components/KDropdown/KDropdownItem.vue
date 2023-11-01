237b0dde84d35cb2f4ead7753440d72b4d87f1d9
portikM <maksym.portianoi@konghq.com>
2023-11-01 13:45:41 -0400

    fix(kdropdownitem): fix item width

--- src/components/KDropdown/KDropdownItem.vue
+++ src/components/KDropdown/KDropdownItem.vue
@@ -185,6 +185,7 @@ const availableComponents = computed((): DropdownItemRenderedRecord => ({
 /* Component styles */
 
 .k-dropdown-item, :deep(.k-dropdown-item) {
+  display: flex;
   list-style: none;
 
   &.has-divider {
@@ -226,6 +227,7 @@ const availableComponents = computed((): DropdownItemRenderedRecord => ({
     color: var(--kui-color-text-neutral, $kui-color-text);
     cursor: pointer;
     display: flex;
+    flex: 1;
     font-family: var(--kui-font-family-text, $kui-font-family-text);
     font-size: var(--kui-font-size-30, $kui-font-size-30);
     font-weight: var(--kui-font-weight-medium, $kui-font-weight-medium);
