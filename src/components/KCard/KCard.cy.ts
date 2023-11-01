5de65ea70e7b0d23c9a9d5309d246cde7a2e9393
portikM <maksym.portianoi@konghq.com>
2023-11-01 10:50:03 -0400

    test(kcard): fix component tests [KHCP-8971]

--- src/components/KCard/KCard.cy.ts
+++ src/components/KCard/KCard.cy.ts
@@ -11,43 +11,37 @@ describe('KCard', () => {
     cy.get('.k-card').find('card-footer').should('not.exist')
   })
 
-  it('renders title and content slots when passed', () => {
+  it('renders title prop when passed', () => {
     const titleProp = 'Title prop'
-    const contentProp = 'Content prop'
 
     mount(KCard, {
       props: {
         title: titleProp,
-        content: contentProp,
       },
     })
 
     cy.get('.k-card').find('.card-title').should('contain', titleProp)
-    cy.get('.k-card').find('.card-content').should('contain', contentProp)
   })
 
   it('renders slots when passed', () => {
     const titleProp = 'Test title'
-    const contentProp = 'Test content'
     const titleText = 'I am the title'
-    const contentText = 'I am the content'
 
     mount(KCard, {
       props: {
         title: titleProp,
-        content: contentProp,
       },
       slots: {
         title: `<span data-testid="card-title">${titleText}</span>`,
         actions: '<span data-testid="card-actions">Card actions</span>',
-        default: `<span data-testid="card-content">${contentText}</span>`,
+        default: '<span data-testid="card-content">Card content</span>',
         footer: '<span data-testid="card-footer">Card footer</span>',
       },
     })
 
     cy.get('.k-card').find('.card-title').get('[data-testid="card-title"]').should('contain', titleText)
-    cy.get('.k-card').get('[data-testid="card-actions"]').should('be.visible')
-    cy.get('.k-card').find('.card-content').get('[data-testid="card-content"]').should('contain', contentText)
-    cy.get('.k-card').get('[data-testid="card-footer"]').should('be.visible')
+    cy.get('.k-card').find('.card-actions').get('[data-testid="card-actions"]').should('be.visible')
+    cy.get('.k-card').find('.card-content').get('[data-testid="card-content"]').should('be.visible')
+    cy.get('.k-card').find('.card-footer').get('[data-testid="card-footer"]').should('be.visible')
   })
 })
