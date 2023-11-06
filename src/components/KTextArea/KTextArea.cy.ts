d11dec04d539e971ab11d73a8c8fbdf9d1425b87
portikM <maksym.portianoi@konghq.com>
2023-11-06 16:52:13 -0500

    test(ktextarea): update component tests

--- src/components/KTextArea/KTextArea.cy.ts
+++ src/components/KTextArea/KTextArea.cy.ts
@@ -1,33 +1,22 @@
 import { mount } from 'cypress/vue'
 import KTextArea from '@/components/KTextArea/KTextArea.vue'
 
-/**
- * ALL TESTS MUST USE testMode: true
- * We generate unique IDs for reference by aria properties. Debug mode strips these out
- * allowing for successful snapshot verification.
- * props: {
- *   testMode: true
- * }
- */
-
 describe('KTextArea', () => {
   it('renders text when value is passed', () => {
     const value = 'Howdy!'
     mount(KTextArea, {
       props: {
-        testMode: true,
-        modelValue: value, // v-model
+        modelValue: value,
       },
     })
 
     cy.get('textarea').should('have.value', value)
   })
 
-  it('renders label when value is passed', () => {
+  it('renders `label` when value is passed', () => {
     const labelText = 'A Label!'
     mount(KTextArea, {
       props: {
-        testMode: true,
         label: labelText,
       },
     })
@@ -35,14 +24,13 @@ describe('KTextArea', () => {
     cy.get('.k-label').should('contain.text', labelText)
   })
 
-  it('renders label with labelAttributes applied', () => {
+  it('renders label with `labelAttributes` applied', () => {
     const labelText = 'A Label'
     mount(KTextArea, {
       props: {
-        testMode: true,
         label: labelText,
         labelAttributes: {
-          info: 'some help text',
+          info: 'some info text',
         },
       },
     })
@@ -51,54 +39,34 @@ describe('KTextArea', () => {
     cy.get('.k-label .tooltip-trigger-icon').should('be.visible')
   })
 
-  it('renders overlayed label when value is passed', () => {
-    const labelText = 'A Label'
+  it('handles `required` attribute correctly', () => {
     mount(KTextArea, {
       props: {
-        testMode: true,
-        label: labelText,
-        overlayLabel: true,
-      },
-    })
-
-    cy.get('.text-on-input label').should('contain.text', labelText)
-  })
-
-  it('renders an asterisk when `overlayLabel` is true and `required` attr is set', () => {
-    const label = 'A label'
-    mount(KTextArea, {
-      props: {
-        testMode: true,
-        label,
-        overlayLabel: true,
-      },
-      attrs: {
+        label: 'A label',
         required: true,
       },
     })
 
-    cy.get('.text-on-input  .is-required').should('exist')
+    cy.get('.k-label').should('have.class', 'required')
   })
 
-  it('renders textarea when rows and cols are passed in', () => {
+  it('renders textarea when `rows` prop is passed in', () => {
     mount(KTextArea, {
       props: {
-        testMode: true,
         rows: 2,
-        cols: 15,
       },
     })
 
-    cy.get('textarea').should('be.visible')
+    cy.get('textarea').should('be.visible').should('have.attr', 'rows', '2')
   })
 
   it('reacts to text changes', () => {
     const value1 = 'hey'
     const value2 = 'hey, dude'
+
     mount(KTextArea, {
       props: {
-        testMode: true,
-        modelValue: value1, // v-model
+        modelValue: value1,
       },
     })
 
@@ -112,50 +80,27 @@ describe('KTextArea', () => {
     cy.get('textarea').should('have.value', value2)
   })
 
-  it('can configure character limit', () => {
-    const charLimit = 500
+  it('shows character count when `characterLimit` prop is set and exceeded', () => {
+    const textCharCount = 28
+    const charLimit = 5
+
     mount(KTextArea, {
       props: {
-        testMode: true,
         characterLimit: charLimit,
       },
     })
 
-    cy.get('.char-limit').should('contain.text', charLimit)
+    cy.get('textarea').type(`This input has ${textCharCount} characters`)
+    cy.get('.k-textarea.input-error .help-text').should('contain.text', `${textCharCount} / ${charLimit}`)
   })
 
-  it('should have style when value exceeds the character limit', () => {
-    const charLimit = 20
+  it('should handle `resizable` prop correctly', () => {
     mount(KTextArea, {
       props: {
-        testMode: true,
-        characterLimit: charLimit,
+        resizable: true,
       },
     })
 
-    cy.get('textarea').type('a'.repeat(charLimit + 1))
-    cy.get('textarea').should('have.value', 'a'.repeat(charLimit + 1))
-    cy.get('div.over-char-limit').should('be.visible')
-  })
-
-  it('should allow disabling character limit', () => {
-    mount(KTextArea, {
-      props: {
-        testMode: true,
-        disableCharacterLimit: true,
-      },
-    })
-
-    cy.get('.char-limit').should('not.exist')
-  })
-
-  it('should have `is-resizable` class when is-resizable prop is enabled', () => {
-    mount(KTextArea, {
-      props: {
-        isResizable: true,
-      },
-    })
-
-    cy.get('textarea').should('have.class', 'is-resizable')
+    cy.get('textarea').should('have.class', 'resizable')
   })
 })
