4dbb3cb8e22f523ba711fc4729a6fc98ee4b8550
portikM <maksym.portianoi@konghq.com>
2023-12-07 12:00:03 -0500

    fix(kmethodbadge): remove component [KHCP-8989]

--- src/types/method-badge.ts
+++
@@ -1,7 +0,0 @@
-export type Method = 'get' | 'post' | 'put' | 'patch' | 'delete' | 'head' | 'options' | 'trace' | 'connect' | 'custom'
-export interface MethodBadgeColors {
-  color: string
-  backgroundColor: string
-}
-
-export const MethodsArray = ['get', 'post', 'put', 'patch', 'delete', 'head', 'options', 'trace', 'connect', 'custom'] as const
