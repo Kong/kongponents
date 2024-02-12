7318b0924ef73a6fb303ae1f61029461fa115649
portikM <maksym.portianoi@konghq.com>
2024-02-12 16:03:35 -0500

    chore(sandbox): set up component sandbox [KHCP-9000]

---
+++ sandbox/pages/SandboxSlideout.vue
@@ -0,0 +1,118 @@
+<template>
+  <SandboxLayout
+    :links="inject('app-links', [])"
+    title="KSlideout"
+  >
+    <div class="kslideout-sandbox">
+      <SandboxSectionComponent>
+        <KExternalLink href="https://www.figma.com/file/Yze0SWXl5nKjR0rFdilljK/Components?type=design&node-id=2020%3A9543&mode=dev&t=USFhEkebi5cmYxaK-1">
+          Figma
+        </KExternalLink>
+      </SandboxSectionComponent>
+
+      <!-- Props -->
+      <SandboxTitleComponent
+        is-subtitle
+        title="Props"
+      />
+      <SandboxSectionComponent title="visible">
+        <KComponent
+          v-slot="{ data }"
+          :data="{ slideoutVisible: false }"
+        >
+          <KButton
+            @click="data.slideoutVisible = true"
+          >
+            KSlideout
+          </KButton>
+
+          <KSlideout
+            :visible="data.slideoutVisible"
+            @close="data.slideoutVisible = false"
+          />
+        </KComponent>
+      </SandboxSectionComponent>
+      <SandboxSectionComponent title="title">
+        <KComponent
+          v-slot="{ data }"
+          :data="{ slideoutVisible: false }"
+        >
+          <KButton
+            @click="data.slideoutVisible = true"
+          >
+            KSlideout
+          </KButton>
+
+          <KSlideout
+            title="Slideout Title"
+            :visible="data.slideoutVisible"
+            @close="data.slideoutVisible = false"
+          />
+        </KComponent>
+      </SandboxSectionComponent>
+      <SandboxSectionComponent title="offsetTop">
+        <KComponent
+          v-slot="{ data }"
+          :data="{ slideoutVisible: false }"
+        >
+          <KButton
+            @click="data.slideoutVisible = true"
+          >
+            KSlideout
+          </KButton>
+
+          <KSlideout
+            :offset-top="100"
+            :visible="data.slideoutVisible"
+            @close="data.slideoutVisible = false"
+          />
+        </KComponent>
+      </SandboxSectionComponent>
+      <SandboxSectionComponent title="hasOverlay">
+        <KComponent
+          v-slot="{ data }"
+          :data="{ slideoutVisible: false }"
+        >
+          <KButton
+            @click="data.slideoutVisible = true"
+          >
+            KSlideout
+          </KButton>
+
+          <KSlideout
+            :has-overlay="false"
+            :visible="data.slideoutVisible"
+            @close="data.slideoutVisible = false"
+          />
+        </KComponent>
+      </SandboxSectionComponent>
+      <SandboxSectionComponent title="closeOnBlur">
+        <!-- TODO -->
+      </SandboxSectionComponent>
+      <SandboxSectionComponent title="maxWidth">
+        <KComponent
+          v-slot="{ data }"
+          :data="{ slideoutVisible: false }"
+        >
+          <KButton
+            @click="data.slideoutVisible = true"
+          >
+            KSlideout
+          </KButton>
+
+          <KSlideout
+            max-width="90%"
+            :visible="data.slideoutVisible"
+            @close="data.slideoutVisible = false"
+          />
+        </KComponent>
+      </SandboxSectionComponent>
+    </div>
+  </SandboxLayout>
+</template>
+
+<script setup lang="ts">
+import SandboxTitleComponent from '../components/SandboxTitleComponent.vue'
+import SandboxSectionComponent from '../components/SandboxSectionComponent.vue'
+import { inject } from 'vue'
+</script>
