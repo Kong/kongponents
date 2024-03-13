ab0c70e77fcbc9746f3d12c07c08280e590ade3c
portikM <maksym.portianoi@konghq.com>
2024-03-13 11:02:14 -0400

    chore(sandbox): setup component sandbox [KHCP-9008]

---
+++ sandbox/pages/SandboxTreeList.vue
@@ -0,0 +1,39 @@
+<template>
+  <SandboxLayout
+    :links="inject('app-links', [])"
+    title="KTreeList"
+  >
+    <div class="ktreelist-sandbox">
+      <SandboxSectionComponent>
+        <KExternalLink href="https://www.figma.com/file/Yze0SWXl5nKjR0rFdilljK/Components?type=design&node-id=2020%3A12779&mode=dev&t=sqzAn4PCrvEoYpdz-1">
+          Figma
+        </KExternalLink>
+      </SandboxSectionComponent>
+
+      <!-- Props -->
+      <SandboxTitleComponent
+        is-subtitle
+        title="Props"
+      />
+      <SandboxSectionComponent title="item" />
+      <SandboxSectionComponent title="draggable" />
+      <SandboxSectionComponent title="maxDepth" />
+      <SandboxSectionComponent title="width" />
+      <SandboxSectionComponent title="hideIcon" />
+
+      <!-- Slots -->
+      <SandboxTitleComponent
+        is-subtitle
+        title="Slots"
+      />
+      <SandboxSectionComponent title="item-icon" />
+      <SandboxSectionComponent title="item-label" />
+    </div>
+  </SandboxLayout>
+</template>
+
+<script setup lang="ts">
+import { inject } from 'vue'
+import SandboxTitleComponent from '../components/SandboxTitleComponent.vue'
+import SandboxSectionComponent from '../components/SandboxSectionComponent.vue'
+</script>
