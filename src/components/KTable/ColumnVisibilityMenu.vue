448db604a3435f45850c22e6e577f6174d5ae00f
kai.arrowood <kai.arrowood@konghq.com>
2024-04-12 09:14:08 -0400

    feat(ktable): make visibility menu scrollable

--- src/components/KTable/ColumnVisibilityMenu.vue
+++ src/components/KTable/ColumnVisibilityMenu.vue
@@ -2,6 +2,7 @@
   <div class="table-column-visibility-menu">
     <KDropdown
       data-testid="table-column-visibility-menu"
+      :kpop-attributes="{ placement: 'left' }"
       @toggle-dropdown="handleDropdownToggle"
     >
       <KTooltip text="Show/Hide Columns">
@@ -18,29 +19,34 @@
       </KTooltip>
 
       <template #items>
-        <KDropdownItem
-          v-for="col in columns"
-          :key="col.key"
-          class="column-visibility-menu-item"
-          :data-testid="`column-visibility-menu-item-${col.key}`"
-          @click.stop="() => {
-            visibilityMap[col.key] = !visibilityMap[col.key]
-            isDirty = true
-          }"
+        <div
+          ref="menuItemsRef"
+          class="menu-items-wrapper"
         >
-          <!-- KLabel must be separate to maintain click handling on the label within the dropdown item -->
-          <KCheckbox
-            v-model="visibilityMap[col.key]"
-            :aria-labelledby="`${tableId}-${col.key}-visibility-checkbox-label`"
-            :data-testid="`column-visibility-checkbox-${col.key}`"
-          />
-          <KLabel
-            :id="`${tableId}-${col.key}-visibility-checkbox-label`"
-            class="visibility-checkbox-label"
+          <KDropdownItem
+            v-for="col in columns"
+            :key="col.key"
+            class="column-visibility-menu-item"
+            :data-testid="`column-visibility-menu-item-${col.key}`"
+            @click.stop="() => {
+              visibilityMap[col.key] = !visibilityMap[col.key]
+              isDirty = true
+            }"
           >
-            {{ col.label }}
-          </KLabel>
-        </KDropdownItem>
+            <!-- KLabel must be separate to maintain click handling on the label within the dropdown item -->
+            <KCheckbox
+              v-model="visibilityMap[col.key]"
+              :aria-labelledby="`${tableId}-${col.key}-visibility-checkbox-label`"
+              :data-testid="`column-visibility-checkbox-${col.key}`"
+            />
+            <KLabel
+              :id="`${tableId}-${col.key}-visibility-checkbox-label`"
+              class="visibility-checkbox-label"
+            >
+              {{ col.label }}
+            </KLabel>
+          </KDropdownItem>
+        </div>
         <div class="apply-button-wrapper">
           <KButton
             appearance="tertiary"
@@ -57,7 +63,7 @@
 </template>
 
 <script setup lang="ts">
-import { ref, watch, onBeforeMount, type PropType } from 'vue'
+import { ref, watch, onBeforeMount, onMounted, type PropType } from 'vue'
 import type { TableHeader } from '@/types'
 import { TableColumnsIcon } from '@kong/icons'
 import KButton from '@/components/KButton/KButton.vue'
@@ -86,6 +92,7 @@ const props = defineProps({
 
 const visibilityMap = ref<Record<string, boolean>>({})
 const isDirty = ref(false)
+const menuItemsRef = ref<HTMLDivElement>()
 
 const initVisibilityMap = (): void => {
   visibilityMap.value = props.columns.reduce((acc, col: TableHeader) => {
@@ -109,8 +116,37 @@ const handleDropdownToggle = (isOpen: boolean): void => {
   }
 }
 
+/**
+ * Fade out the bottom of the menu items when scrollable
+ */
+const setOverflowClass = (el: HTMLDivElement) => {
+  const isScrollable = el.scrollHeight > el.clientHeight
+
+  // GUARD: If element is not scrollable, remove all classes
+  if (!isScrollable) {
+    el.classList.remove('is-bottom-overflowing')
+    return
+  }
+
+  // Otherwise, the element is overflowing!
+  // One pixel is added to the height to account for non-integer heights.
+  const isScrolledToBottom = el.scrollHeight < el.clientHeight + el.scrollTop + 1
+  el.classList.toggle('is-bottom-overflowing', !isScrolledToBottom)
+}
+
 watch(() => props.visibilityPreferences, () => {
   initVisibilityMap()
+})
+
+onMounted(() => {
+  if (menuItemsRef.value) {
+    menuItemsRef.value.addEventListener('scroll', (e) => {
+      const el = e.currentTarget as HTMLDivElement
+
+      setOverflowClass(el)
+    })
+    setOverflowClass(menuItemsRef.value)
+  }
 })
 
 onBeforeMount(() => {
@@ -122,6 +158,21 @@ onBeforeMount(() => {
 <style lang="scss" scoped>
 .table-column-visibility-menu {
   margin-left: var(--kui-space-auto, $kui-space-auto);
+
+  .menu-items-wrapper {
+    $bottom-mask-size: 0px;
+    -webkit-mask-image: linear-gradient(to bottom, black calc(100% - $bottom-mask-size), transparent 100%);
+    mask-image: linear-gradient(to bottom, black calc(100% - $bottom-mask-size), transparent 100%);
+    max-height: 400px;
+    overflow-y: auto;
+
+    &.is-bottom-overflowing {
+      $bottom-mask-size: 48px;
+
+      -webkit-mask-image: linear-gradient(to bottom, black calc(100% - $bottom-mask-size), transparent 100%);
+      mask-image: linear-gradient(to bottom, black calc(100% - $bottom-mask-size), transparent 100%);
+    }
+  }
 
   .apply-button-wrapper {
     display: flex;
