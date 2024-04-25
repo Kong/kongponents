53647a822dd41c7823845d0bebb7fcf4ae472dc5
Adam DeHaven <2229946+adamdehaven@users.noreply.github.com>
2024-04-25 16:42:41 -0400

    fix/commitlint (#2163)
    * fix: commitlint casing
    
    * fix: commitlint rules
    
    * refactor: config file

--- commitlint.config.ts
+++ commitlint.config.ts
@@ -1,8 +1,10 @@
-module.exports = {
+export default {
   extends: ['@commitlint/config-conventional'],
   rules: {
     'header-max-length': [2, 'always', 100],
     'body-max-line-length': [1, 'always', 150],
+    'type-case': [2, 'always', 'lower-case'],
+    'scope-case': [2, 'always', 'lower-case'],
   },
   ignores: [(message) => /^chore\(release\): .+$/m.test(message)],
   helpUrl: 'https://kongponents.konghq.com/contributing/getting-started.html',
