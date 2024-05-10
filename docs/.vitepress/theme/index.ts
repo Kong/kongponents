8aca344a57b1da8c037f2b547899da3f497ca0cb
portikM <maksym.portianoi@konghq.com>
2024-05-10 11:50:44 -0400

    feat(kicon): remove component [KHCP-8983]

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -2,7 +2,6 @@ import type { EnhanceAppContext } from 'vitepress'
 import DefaultTheme from 'vitepress/theme'
 import Kongponents from '../../../src'
 // Import component-specific files
-import * as icons from '../../../src/components/KIcon/icons' // KIcon icons
 import ToastManager from '../../../src/components/KToaster/ToastManager'
 import RouterLink from '../components/RouterLink.vue'
 
