8cf484abc00cb9fb5c885cdf22f9e5262dca69c4
portikM <maksym.portianoi@konghq.com>
2024-05-15 16:34:20 -0400

    chore(sandbox): simulate race condition

--- sandbox/pages/SandboxTablePreferences.vue
+++ sandbox/pages/SandboxTablePreferences.vue
@@ -13,15 +13,23 @@
 </template>
 
 <script setup lang="ts">
-import { inject, ref } from 'vue'
+import { inject, nextTick, onMounted, ref } from 'vue'
 import type { TableHeader, TablePreferences } from '@/types'
 import { useTablePreferences } from '@kong-ui-public/core'
 
 const { setTablePreferences, getTablePreferences } = useTablePreferences()
 
 const preferencesKey = 'kongponents-sandbox-ktable-table-preferences'
+const defaultPreferences: TablePreferences = {
+  pageSize: 30,
+  sortColumnKey: undefined,
+  sortColumnOrder: undefined,
+  columnWidths: {},
+  columnVisibility: {},
+}
 
-const tablePreferences = ref<TablePreferences>(getTablePreferences(preferencesKey))
+// setup with default preferences and update on mounted to simulate race condition
+const tablePreferences = ref<TablePreferences>(defaultPreferences)
 
 const onTablePreferencesUpdate = (newTablePreferences: TablePreferences): void => {
   console.log('Table preferences updated:', newTablePreferences)
@@ -35,16 +43,79 @@ const headers: TableHeader[] = [
   { key: 'actions', label: 'Row actions', hideLabel: true },
 ]
 
-const fetcher = async (): Promise<any> => {
-  // Fake delay
-  await new Promise((resolve) => setTimeout(resolve, 2000))
-
-  const response = await fetch('https://jsonplaceholder.typicode.com/users')
-  const responseData = await response.json()
+const fetcher = (): any => {
+  const responseData = [
+    {
+      id: 1,
+      name: 'Leanne Graham',
+      username: 'Bret',
+      email: 'Sincere@april.biz',
+    },
+    {
+      id: 2,
+      name: 'Ervin Howell',
+      username: 'Antonette',
+      email: 'Shanna@melissa.tv',
+    },
+    {
+      id: 3,
+      name: 'Clementine Bauch',
+      username: 'Samantha',
+      email: 'Nathan@yesenia.net',
+    },
+    {
+      id: 4,
+      name: 'Patricia Lebsack',
+      username: 'Karianne',
+      email: 'Julianne.OConner@kory.org',
+    },
+    {
+      id: 5,
+      name: 'Chelsey Dietrich',
+      username: 'Kamren',
+      email: 'Lucio_Hettinger@annie.ca',
+    },
+    {
+      id: 6,
+      name: 'Mrs. Dennis Schulist',
+      username: 'Leopoldo_Corkery',
+      email: 'Karley_Dach@jasper.info',
+    },
+    {
+      id: 7,
+      name: 'Kurtis Weissnat',
+      username: 'Elwyn.Skiles',
+      email: 'Telly.Hoeger@billy.biz',
+    },
+    {
+      id: 8,
+      name: 'Nicholas Runolfsdottir V',
+      username: 'Maxime_Nienow',
+      email: 'Sherwood@rosamond.me',
+    },
+    {
+      id: 9,
+      name: 'Glenna Reichert',
+      username: 'Delphine',
+      email: 'Chaim_McDermott@dana.io',
+    },
+    {
+      id: 10,
+      name: 'Clementina DuBuque',
+      username: 'Moriah.Stanton',
+      email: 'Rey.Padberg@karina.biz',
+    },
+  ]
 
   return {
     data: responseData,
     total: responseData.length,
   }
 }
+
+onMounted(async () => {
+  // update table preferences with value from local storage
+  await nextTick()
+  tablePreferences.value = getTablePreferences(preferencesKey)
+})
 </script>
