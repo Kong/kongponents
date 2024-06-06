cf8e91241336d22f386ab55dc4ddb7cd597989b6
Adam DeHaven <adam.dehaven@konghq.com>
2024-06-05 20:43:51 -0400

    feat(eslint): eslint v9 and shared flat config [KHCP-11627]

---
+++ eslint.config.mjs
@@ -0,0 +1,22 @@
+import eslintKongUiConfig from '@kong/eslint-config-kong-ui'
+import eslintKongUiConfigCypress from '@kong/eslint-config-kong-ui/cypress'
+
+export default [
+  ...eslintKongUiConfig,
+  // Only apply the shared Cypress config to files that match the given pattern
+  ...eslintKongUiConfigCypress.map(config => ({
+    ...config,
+    files: [
+      '**/*.cy.ts',
+      '**/cypress/**',
+    ],
+  })),
+  {
+    ignores: [
+      'docs/.vitepress/.temp/',
+      'docs/.vitepress/.cache/',
+      'docs/.vitepress/dist/',
+      'cli/template',
+    ],
+  },
+]
