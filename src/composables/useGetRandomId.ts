572dd0386d8c317ec3328240a9b7e253acad2bfd
portikM <maksym.portianoi@konghq.com>
2024-06-10 15:33:29 -0400

    fix: remove console [KHCP-11956]

--- src/composables/useGetRandomId.ts
+++ src/composables/useGetRandomId.ts
@@ -4,7 +4,6 @@ import { v4 as uuidv4 } from 'uuid'
 
 export default function useGetRandomId() {
   const kongponentsId: () => string = inject<() => string>(kongponentsRandomIdFunction, uuidv4)
-  console.log(kongponentsId, typeof kongponentsId)
 
   return { kongponentsId }
 }
