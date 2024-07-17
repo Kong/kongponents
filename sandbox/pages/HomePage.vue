78bc2a3f3c85c2c3a9dded64bff6b01cace0686b
portikM <maksym.portianoi@konghq.com>
2024-07-17 13:42:09 -0400

    chore: resize observer infinite loop [KHCP-12603]

--- sandbox/pages/HomePage.vue
+++ sandbox/pages/HomePage.vue
@@ -11,10 +11,33 @@
       <p>
         By using the sandbox, you can verify that components maintain their visual consistency, regardless of the host app in which they are used.
       </p>
+      <div ref="resizeObserverContainer">
+        Resize Observer
+      </div>
     </div>
   </SandboxLayout>
 </template>
 
 <script setup lang="ts">
-import { inject } from 'vue'
+import { inject, onMounted, onUnmounted, ref } from 'vue'
+import { ResizeObserverHelper } from '@/utilities/resizeObserverHelper'
+
+const resizeObserverContainer = ref<HTMLElement | null>(null)
+const resizeObserver = ref<ResizeObserverHelper>()
+
+const onResizeCallback = () => {
+  const elWidth = resizeObserverContainer.value?.offsetWidth
+  if (resizeObserverContainer.value) {
+    resizeObserverContainer.value.style.width = `${elWidth! + 10}px`
+  }
+}
+
+onMounted(() => {
+  resizeObserver.value = ResizeObserverHelper.create(onResizeCallback)
+  resizeObserver.value.observe(resizeObserverContainer.value as HTMLDivElement)
+})
+
+onUnmounted(() => {
+  resizeObserver.value?.unobserve(resizeObserverContainer.value as HTMLDivElement)
+})
 </script>
