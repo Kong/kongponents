b2c4999c517573643c2febd230510845f1a715fc
Maksym Portianoi <maksym.portianoi@konghq.com>
2024-08-28 14:14:23 -0400

    fix(ktreelist): add group prop [KHCP-13028] (#2361)
    * fix(ktreelist): add group prop [KHCP-13028]
    
    * test(ktreelist): add group prop tests [KHCP-13028]
    
    * docs(tree-list): update component docs [KHCP-13028]
    
    * fix: address pr feedback

--- sandbox/pages/SandboxTreeList.vue
+++ sandbox/pages/SandboxTreeList.vue
@@ -12,28 +12,46 @@
       <SandboxSectionComponent title="item">
         <KTreeList :items="items1" />
       </SandboxSectionComponent>
+      <SandboxSectionComponent title="group">
+        <KComponent
+          v-slot="{ data }"
+          :data="{ grouping: true }"
+        >
+          <KInputSwitch
+            v-model="data.grouping"
+            :label="data.grouping ? 'Different groups' : 'Same group'"
+          />
+          <div class="groups-example">
+            <KTreeList :items="items2" />
+            <KTreeList
+              :group="data.grouping ? 'i-stand-alone' : undefined"
+              :items="items3"
+            />
+          </div>
+        </KComponent>
+      </SandboxSectionComponent>
       <SandboxSectionComponent title="disableDrag">
         <KTreeList
           disable-drag
-          :items="items2"
+          :items="items4"
         />
       </SandboxSectionComponent>
       <SandboxSectionComponent title="maxDepth">
         <KTreeList
-          :items="items3"
+          :items="items5"
           :max-depth="4"
         />
       </SandboxSectionComponent>
       <SandboxSectionComponent title="width">
         <KTreeList
-          :items="items4"
+          :items="items6"
           width="300"
         />
       </SandboxSectionComponent>
       <SandboxSectionComponent title="hideIcons">
         <KTreeList
           hide-icons
-          :items="items5"
+          :items="items7"
         />
       </SandboxSectionComponent>
 
@@ -43,7 +61,7 @@
         title="Slots"
       />
       <SandboxSectionComponent title="item-icon">
-        <KTreeList :items="items6">
+        <KTreeList :items="items8">
           <template #item-icon="{ item }">
             <InboxIcon
               v-if="item.id.includes('folder')"
@@ -53,7 +71,7 @@
         </KTreeList>
       </SandboxSectionComponent>
       <SandboxSectionComponent title="item-label">
-        <KTreeList :items="items7">
+        <KTreeList :items="items9">
           <template #item-label="{ item }">
             <span v-if="item.id.includes('folder')">
               <strong>{{ item.name }}</strong>
@@ -132,4 +150,18 @@ const items4 = ref<TreeListItem[]>(JSON.parse(JSON.stringify(defaultItems)))
 const items5 = ref<TreeListItem[]>(JSON.parse(JSON.stringify(defaultItems)))
 const items6 = ref<TreeListItem[]>(JSON.parse(JSON.stringify(defaultItems)))
 const items7 = ref<TreeListItem[]>(JSON.parse(JSON.stringify(defaultItems)))
+const items8 = ref<TreeListItem[]>(JSON.parse(JSON.stringify(defaultItems)))
+const items9 = ref<TreeListItem[]>(JSON.parse(JSON.stringify(defaultItems)))
 </script>
+
+<style lang="scss" scoped>
+.groups-example {
+  display: flex;
+  flex-direction: column;
+  gap: $kui-space-40;
+
+  @media (min-width: $kui-breakpoint-mobile) {
+    flex-direction: row;
+  }
+}
+</style>
