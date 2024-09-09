cd8ceb518e219e16795a169c353aeb0d46807436
Maksym Portianoi <maksym.portianoi@konghq.com>
2024-09-09 10:19:42 -0400

    feat(ktableview): add bulk actions feature [KHCP-13106] (#2365)
    * chore(sandbox): minor fixes [KHCP-13106]
    
    * feat(ktableview): add bulk actions column feature [KHCP-13106]
    
    * fix(ktableview): add bulk actions dropdown [KHCP-13106]
    
    * test(ktableview): add bulk actions tests [KHCP-13106]
    
    * fix: address pr feedback
    
    * fix(ktableview): sticky bulk actions column [KHCP-13106]
    
    * fix(ktableview): add bulk actions dropdown component [KHCP-13106]
    
    * fix(ktableview): allow disable row bulk actions [KHCP-13106]
    
    * test(ktableview): bulk actions disabled [KHCP-13106]
    
    * fix(ktableview): minor fixes [KHCP-13106]
    
    * fix(ktableview): add row bulk action prop [KHCP-13106]
    
    * docs(ktableview): update component docs [KHCP-13106]
    
    * fix: minor fix
    
    * fix: minor fix
    
    * fix: minor fix
    
    * fix: address pr feedback
    
    * fix(ktableview): apply pr feedback
    
    Co-authored-by: Adam DeHaven <2229946+adamdehaven@users.noreply.github.com>
    
    * fix: address pr feedback
    
    * fix: address pr feedback
    
    * fix: address pr feedback
    
    * fix: minor fix
    
    * test(ktableview): fix component tests [KHCP-13106]
    
    * fix: address pr feedback
    
    * fix: address pr feedback
    
    * fix: apply pr feedback
    
    Co-authored-by: Adam DeHaven <2229946+adamdehaven@users.noreply.github.com>
    
    * test(ktableview): fix test [KHCP-13106]
    
    * fix: address pr feedback
    
    * fix: address pr feedback
    
    * fix: minor fix
    
    * fix: address pr feedback
    
    * fix: minor fix
    
    ---------
    
    Co-authored-by: Adam DeHaven <2229946+adamdehaven@users.noreply.github.com>

---
+++ src/components/KTableView/BulkActionsDropdown.vue
@@ -0,0 +1,76 @@
+<template>
+  <KDropdown
+    class="bulk-actions-dropdown"
+    data-testid="bulk-actions-dropdown"
+    :disabled="disabled"
+    :kpop-attributes="{ placement: 'bottom-end' }"
+  >
+    <KTooltip
+      placement="bottom-end"
+      text="Selected Row Actions"
+    >
+      <KButton
+        appearance="secondary"
+        :aria-label="buttonLabel"
+        class="bulk-actions-dropdown-trigger"
+        data-testid="bulk-actions-dropdown-trigger"
+        :disabled="disabled"
+        icon
+        size="large"
+      >
+        <MoreIcon
+          class="more-icon"
+          decorative
+        />
+        <Transition name="kongponents-fade-transition">
+          <span
+            v-if="count"
+            class="bulk-actions-dropdown-trigger-count"
+          >
+            {{ count }}
+          </span>
+        </Transition>
+      </KButton>
+    </KTooltip>
+
+    <template #items>
+      <slot name="items" />
+    </template>
+  </KDropdown>
+</template>
+
+<script setup lang="ts">
+import KTooltip from '@/components/KTooltip/KTooltip.vue'
+import KDropdown from '@/components/KDropdown/KDropdown.vue'
+import KButton from '@/components/KButton/KButton.vue'
+import { MoreIcon } from '@kong/icons'
+
+defineProps({
+  disabled: {
+    type: Boolean,
+    default: false,
+  },
+  count: {
+    type: String,
+    default: '',
+  },
+  buttonLabel: {
+    type: String,
+    default: '',
+  },
+})
+</script>
+
+<style lang="scss" scoped>
+.bulk-actions-dropdown {
+  .bulk-actions-dropdown-trigger {
+    gap: var(--kui-space-0, $kui-space-0);
+
+    .bulk-actions-dropdown-trigger-count {
+      font-size: var(--kui-font-size-30, $kui-font-size-30);
+      line-height: var(--kui-line-height-30, $kui-line-height-30);
+      margin-right: var(--kui-space-20, $kui-space-20);
+    }
+  }
+}
+</style>
