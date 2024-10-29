2e127613eee04e3dfc7d15e97cdb11450402a9eb
portikM <maksym.portianoi@konghq.com>
2024-10-29 12:18:11 -0400

    fix(ktableview, ktabledata): row-attrs prop type

--- src/types/table.ts
+++ src/types/table.ts
@@ -137,7 +137,7 @@ interface TablePropsShared {
   /**
    * A function that conditionally specifies row attributes on each row
    */
-  rowAttrs?: (row: Record<string, any>) => Record<string, string>
+  rowAttrs?: (row: Record<string, any>) => Record<string, any>
   /**
    * A function that conditionally turns a row into a link
    */
