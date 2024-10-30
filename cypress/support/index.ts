a4476e42faa1c53f12577e106084d36c27280a20
Tian Tan <tian.tan@konghq.com>
2024-10-30 13:33:41 +0800

    test(kbutton): add mountWithProdRouter for kbutton test [KM-644]

--- cypress/support/index.ts
+++ cypress/support/index.ts
@@ -1,9 +1,22 @@
 import { mount } from 'cypress/vue'
+//  vue-router only throws uncaught errors in production mode
+import { createMemoryHistory, createRouter } from 'vue-router/dist/vue-router.prod.cjs'
+import type { RouteRecordRaw } from 'vue-router'
 import type { App, ComputedOptions } from 'vue'
 import Chainable = Cypress.Chainable
 import 'cypress-fail-fast'
 // Import Kongponent styles
 import '@/styles/styles.scss'
+
+const testRoutes: RouteRecordRaw[] = [
+  {
+    path: '/',
+    name: 'home',
+    alias: '/:pathMatch(.*)*',
+    meta: { title: 'Kongponents Sandbox' },
+    component: () => import('../../sandbox/pages/HomePage.vue'),
+  },
+]
 
 Cypress.Commands.add('getTestId', (dataTestId: string): Chainable => {
   return cy.get(`[data-testid="${dataTestId}"]`)
@@ -23,6 +36,29 @@ Cypress.Commands.add('mount', (component: ComputedOptions, options = {}): Chaina
       if (options.router) {
         app.use(options.router)
       }
+    },
+  })
+
+  return mount(component, options)
+})
+
+Cypress.Commands.add('mountWithProdRouter', (component: ComputedOptions, options = {}) => {
+  // Setup options object
+  options.global = options.global || {}
+  options.global.plugins = options.global.plugins || []
+
+  // create router if one is not provided
+  if (!options.router) {
+    options.router = createRouter({
+      routes: testRoutes,
+      history: createMemoryHistory(),
+    })
+  }
+
+  // Add router plugin
+  options.global.plugins.push({
+    install(app) {
+      app.use(options.router)
     },
   })
 
