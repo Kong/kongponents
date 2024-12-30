974a707a1e126ff430f0a5a0ce51164c730ade76
GU Yiling <justice360@gmail.com>
2024-12-30 21:54:07 +0800

    fix(kcodeblock): fix maxHeight and getSizeFromString util (#2554)

--- src/composables/useUtilities.cy.ts
+++ src/composables/useUtilities.cy.ts
@@ -183,46 +183,60 @@ describe('Client-side sorting (deprecated in favor of server-side sorting)', () 
 })
 
 describe('getSizeFromString(): ', () => {
-  it('handles numbers', () => {
+  it('handles integers', () => {
     const sizeStr = '500'
     const result = getSizeFromString(sizeStr)
 
     expect(result).equal(`${sizeStr}px`)
   })
 
+  it('handles floats', () => {
+    const sizeStr = '500.500'
+    const result = getSizeFromString(sizeStr)
+
+    expect(result).equal('500.5px')
+  })
+
   it('handles auto', () => {
     const sizeStr = 'auto'
     const result = getSizeFromString(sizeStr)
 
-    expect(result).equal(`${sizeStr}`)
+    expect(result).equal(sizeStr)
   })
 
   it('handles percentages', () => {
     const sizeStr = '500%'
     const result = getSizeFromString(sizeStr)
 
-    expect(result).equal(`${sizeStr}`)
+    expect(result).equal(sizeStr)
   })
 
   it('handles vh', () => {
     const sizeStr = '500vh'
     const result = getSizeFromString(sizeStr)
 
-    expect(result).equal(`${sizeStr}`)
+    expect(result).equal(sizeStr)
   })
 
   it('handles vw', () => {
     const sizeStr = '500vw'
     const result = getSizeFromString(sizeStr)
 
-    expect(result).equal(`${sizeStr}`)
+    expect(result).equal(sizeStr)
   })
 
   it('handles px', () => {
     const sizeStr = '500px'
     const result = getSizeFromString(sizeStr)
 
-    expect(result).equal(`${sizeStr}`)
+    expect(result).equal(sizeStr)
+  })
+
+  it('handles invalid values', () => {
+    const sizeStr = 'foo'
+    const result = getSizeFromString(sizeStr)
+
+    expect(result).equal(sizeStr)
   })
 })
 
