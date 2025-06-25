4a2a140f3e084e58503242009453750bd440749d
Justineo <justice360@gmail.com>
2025-06-25 16:31:26 +0800

    fix(ktoggle): ts improvements for ktoggle

---
+++ src/components/KToggle/KToggle.vue
@@ -0,0 +1,36 @@
+<template>
+  <slot v-if="$slots.default" :is-toggled="isToggled" :toggle="toggle" />
+</template>
+
+<script setup lang="ts">
+import type { ToggleEmits, ToggleProps, ToggleSlots } from '@/types';
+import { ref } from 'vue'
+
+const { toggled } = defineProps<ToggleProps>()
+
+const emit = defineEmits<ToggleEmits>()
+
+const isToggled = ref(toggled)
+
+const toggle = (): void => {
+  isToggled.value = !isToggled.value
+  emit('toggled', isToggled.value)
+}
+
+const slots = defineSlots<ToggleSlots>()
+
+if (!slots.default) {
+  console.error(`KToggle expects to have slot content.
+
+Example usage:
+
+<KToggle>
+  <template #default="{ isToggled, toggle }">
+    <button @click="toggle">
+      {{ isToggled ? 'hello' : 'goodbye' }}
+    </button>
+  </template>
+</KToggle>
+`)
+}
+</script>
---
+++ src/types/toggle.ts
@@ -0,0 +1,21 @@
+export interface ToggleProps {
+  /**
+   * Whether the toggle is toggled on or off by default.
+   * @default false
+   */
+  toggled?: boolean
+}
+
+export interface ToggleEmits {
+  /**
+   * Emitted when the toggle is changed.
+   */
+  toggled: [value: boolean]
+}
+
+export interface ToggleSlots {
+  /**
+   * Content to toggle.
+   */
+  default?(props: { isToggled: boolean, toggle: () => void }): any
+}
