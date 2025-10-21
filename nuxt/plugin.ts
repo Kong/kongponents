7c58ba9270a30fdc5a7c47f119e6feb9d04eabd9
Arash Ari Sheyda <ari.sheyda@konghq.com>
2025-10-21 13:34:09 -0600

    fix: update

--- nuxt/plugin.ts
+++ nuxt/plugin.ts
@@ -1,4 +1,7 @@
 // @ts-ignore - file exists
+import { defineNuxtPlugin } from '#app'
+
+// @ts-ignore - file exists
 import '@kong/kongponents/dist/style.css'
 // In some NodeJS environments, the `crypto` module is not available by default, so import it and make it available on the server
 import crypto from 'node:crypto'
