0aff611e44e04020e4e9ccf65d15dc6ea5189cb3
portikM <maksym.portianoi@konghq.com>
2025-11-11 12:07:48 -0500

    test(ktoaster): tweak test [KHCP-18535]

--- src/components/KToaster/ToastManager.spec.ts
+++ src/components/KToaster/ToastManager.spec.ts
@@ -1,18 +1,20 @@
 import { describe, it, expect } from 'vitest'
 import ToastManager from './ToastManager'
 
+const toastersContainerId = 'kongponents-toaster-container'
+
 describe('ToastManager', () => {
-  it('should initialize without errors', () => {
+  it('should create a toasters container when initialized', () => {
 
     const toastManager = new ToastManager()
-    // No assertions needed; if no error is thrown, the test passes
+    const containers = document.querySelectorAll(`#${toastersContainerId}`)
+    expect(containers.length).toBe(1)
 
     // Clean up
     toastManager.destroy()
   })
 
   it('should not create a duplicate toasters container if one already exists', () => {
-    const toastersContainerId = 'kongponents-toaster-container'
     const existingContainer = document.createElement('div')
     existingContainer.id = toastersContainerId
     document.body.appendChild(existingContainer)
@@ -28,7 +30,6 @@ describe('ToastManager', () => {
   it('should create a toasters container if one does not exist when open is called', () => {
     const toastManager = new ToastManager()
 
-    const toastersContainerId = 'kongponents-toaster-container'
     const existingContainer = document.querySelectorAll(`#${toastersContainerId}`)
     expect(existingContainer.length).toBe(1)
     document.querySelector(`#${toastersContainerId}`)?.remove()
