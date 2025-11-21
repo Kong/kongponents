2da5c0ae31b3f472fc3e65958fe721bb69ebf79d
ValeryG <vvalggor@gmail.com>
2025-11-21 13:19:00 -0700

    Revert "fix: remove harde runner (#2997)"
    This reverts commit 5a5232a64c9792b542c103589acdb3eee3aa59a2.

--- src/index.ts
+++ src/index.ts
@@ -17,7 +17,7 @@ export default {
 // Export a named object exporting all available components. This is utilized to loop through all available components in this package.
 export { components }
 
-// Export all other named exports
+// All other named exports
 export * from './components'
 export * from './global-components'
 export * from './types'
