17b94c3bad18906f7cbbfb3d02ca5d7a176ddf0e
Yue JIN <yue.jin@konghq.com>
2025-12-01 17:56:54 +0800

    fix: add comments

--- sandbox/utils/shiki.ts
+++ sandbox/utils/shiki.ts
@@ -4,7 +4,7 @@ import {
 } from 'shiki/core'
 import { createJavaScriptRegexEngine } from 'shiki/engine-javascript.mjs'
 
-
+// Create a bundled highlighter with lazy-loaded languages and themes
 const createHighlighter = createdBundledHighlighter({
   langs: {
     json: () => import('shiki/langs/json.mjs'),
@@ -16,6 +16,7 @@ const createHighlighter = createdBundledHighlighter({
   engine: () => createJavaScriptRegexEngine(),
 })
 
+// Export singleton shorthand for converting code to HTML
 const { codeToHtml } = createSingletonShorthands(createHighlighter)
 
 export { codeToHtml }
