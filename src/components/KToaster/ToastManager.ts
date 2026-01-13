c962f88ab33f4f01e3a61d339b53dbd4c12df4a7
John Cowen <john.cowen@konghq.com>
2026-01-13 09:55:00 +0000

    fix(ktoaster): singleton guard and noop SSR manager

--- src/components/KToaster/ToastManager.ts
+++ src/components/KToaster/ToastManager.ts
@@ -1,10 +1,19 @@
 import { createVNode, render, ref } from 'vue'
 import type { Ref, VNode } from 'vue'
-import type { Toast, ToasterAppearance, ToasterOptions } from '@/types'
+import type { Toast, ToasterOptions } from '@/types'
 import { ToasterAppearances } from '@/types'
 import KToaster from '@/components/KToaster/KToaster.vue'
 import { getUniqueStringId } from '@/utilities'
 import SharedPool from './SharedPool'
+
+interface IToastManager {
+  toasts: Ref<Toast[]>
+  setTimer(key: string, timeout: number): number
+  open(args: Record<string, any> | string): void
+  close(key: string): void
+  closeAll(): void
+  destroy(): void
+}
 
 const toasterContainerId = 'kongponents-toaster-container'
 
@@ -13,19 +22,26 @@ const toasterDefaults = {
   appearance: ToasterAppearances.info,
 }
 
-class InternalToastManager {
+class SSRToastManager implements IToastManager {
+  public toasts = ref<Toast[]>([])
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  setTimer(...args: Parameters<IToastManager['setTimer']>) {
+    return 0
+  }
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  public open(...args: Parameters<IToastManager['open']>) {}
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  close(...args: Parameters<IToastManager['close']>) {}
+  closeAll() {}
+  public destroy() {}
+
+}
+class DOMToastManager implements IToastManager {
   private toastersContainer: HTMLElement | null = null
   private toaster: VNode | null = null
-  public toasts: Ref<Toast[]> = ref<Toast[]>([])
+  public toasts = ref<Toast[]>([])
 
   constructor() {
-    // For SSR, prevents failing on the build)
-    if (typeof document === 'undefined') {
-      console.warn('ToastManager should only be initialized in the browser environment. Docs: https://kongponents.konghq.com/components/toaster.html')
-
-      return
-    }
-
     this.toastersContainer = document.createElement('div')
     this.toastersContainer.id = toasterContainerId
     document.body.appendChild(this.toastersContainer)
@@ -40,18 +56,19 @@ class InternalToastManager {
     }
   }
 
-  setTimer(key: string, timeout: number): number {
+  setTimer(key: string, timeout: number) {
     return window?.setTimeout(() => this.close(key), timeout) || 0
   }
 
-  public open(args: Record<string, any> | string): void {
+  public open(args: Record<string, any> | string) {
     // eslint-disable-next-line @typescript-eslint/ban-ts-comment
     // @ts-ignore
     const { key, timeoutMilliseconds, appearance, message, title } = args
 
-    const toastKey: string = key ? String(key) : getUniqueStringId()
-    const toastAppearance: ToasterAppearance = (appearance && Object.keys(ToasterAppearances).indexOf(appearance) !== -1) ? appearance : toasterDefaults.appearance
-    const timer: number = this.setTimer(key, timeoutMilliseconds || toasterDefaults.timeoutMilliseconds)
+    const toastKey = key ? String(key) : getUniqueStringId()
+    const toastAppearance = ((appearance && Object.keys(ToasterAppearances).indexOf(appearance) !== -1) ?
+      appearance : toasterDefaults.appearance) as (keyof typeof ToasterAppearances)
+    const timer = this.setTimer(toastKey, timeoutMilliseconds || toasterDefaults.timeoutMilliseconds)
     const toasterMessage = typeof args === 'string' ? args : message
 
     // Add toaster to state
@@ -65,13 +82,13 @@ class InternalToastManager {
     })
   }
 
-  close(key: string): void {
-    const i: number = this.toasts.value?.findIndex(n => key === n.key)
+  close(key: string) {
+    const i = this.toasts.value?.findIndex(n => key === n.key)
     clearTimeout(this.toasts.value[i]?.timer)
     this.toasts.value.splice(i, 1)
   }
 
-  closeAll(): void {
+  closeAll() {
     this.toasts.value.forEach(toast => clearTimeout(toast?.timer))
     this.toasts.value = []
   }
@@ -83,10 +100,17 @@ class InternalToastManager {
     }
   }
 }
-const pool = new SharedPool<string, InternalToastManager>((state, id, item) => {
+
+const pool = new SharedPool<string, IToastManager>((state, id, item) => {
   switch (state) {
     case 'creating':
-      return new InternalToastManager()
+      // For SSR, prevents failing on the build)
+      if (typeof document === 'undefined') {
+        console.warn('ToastManager should only be initialized in the browser environment, all methods are noops. Docs: https://kongponents.konghq.com/components/toaster.html')
+        return new SSRToastManager()
+      }
+
+      return new DOMToastManager()
     case 'acquiring':
       return item
     case 'releasing':
@@ -109,29 +133,34 @@ export default class ToastManager {
   constructor(options?: ToasterOptions)
 
   // internal usage
-  constructor(options: ToasterOptions | undefined, manager: InternalToastManager)
+  constructor(options: ToasterOptions | undefined, manager: IToastManager)
   constructor(
     options?: ToasterOptions,
-    protected manager: InternalToastManager = pool.acquire(toasterContainerId, this.sym),
+    protected manager: IToastManager = pool.acquire(toasterContainerId, this.sym),
   ) {}
 
-  setTimer(...args: Parameters<InternalToastManager['setTimer']>): number {
+  get toasts() {
+    return this.manager.toasts
+  }
+
+  setTimer(...args: Parameters<IToastManager['setTimer']>) {
     return this.manager.setTimer(...args)
   }
 
-  open(...args: Parameters<InternalToastManager['open']>): void {
+  open(...args: Parameters<IToastManager['open']>) {
     return this.manager.open(...args)
   }
 
-  close(...args: Parameters<InternalToastManager['close']>): void {
+  close(...args: Parameters<IToastManager['close']>) {
     return this.manager.close(...args)
   }
 
-  closeAll(...args: Parameters<InternalToastManager['closeAll']>): void {
+  closeAll(...args: Parameters<IToastManager['closeAll']>) {
     return this.manager.closeAll(...args)
   }
 
-  destroy() {
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  destroy(...args: Parameters<IToastManager['destroy']>) {
     return pool.release(toasterContainerId, this.sym)
   }
 }
