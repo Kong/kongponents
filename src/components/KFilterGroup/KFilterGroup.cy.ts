87f1dde25da574124e8811464e97ce2241312b9f
Jonathan Walters <jonathan.walters@konghq.com>
2026-02-06 10:43:39 -0500

    feat: basic component initialization

---
+++ src/components/KFilterGroup/KFilterGroup.cy.ts
@@ -0,0 +1,8 @@
+import { mount } from 'cypress/vue'
+import KFilterGroup from '@/components/KFilterGroup/KFilterGroup.vue'
+
+describe('KFilterGroup', () => {
+  it('TODO: This is just an example test', () => {
+    mount(KFilterGroup)
+  })
+})
---
+++ src/components/KFilterGroup/KFilterGroup.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="k-filter-group">
+    <!-- TODO: The <div> tag here is just a placeholder for your component content. -->
+  </div>
+</template>
+
+<script setup lang="ts">
+// TODO: Add component script logic
+</script>
+
+<style lang="scss" scoped>
+.k-filter-group {
+  // TODO: Top-level wrapper class for component styles
+}
+</style>
