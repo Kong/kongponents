6c4141fb68aea256d981203252b3a1f775c4b45d
portikM <maksym.portianoi@konghq.com>
2026-02-17 12:47:07 -0500

    fix(ktoaster): deprecate zindex prop, set z-index on toaster container

--- sandbox/pages/SandboxToaster.vue
+++ sandbox/pages/SandboxToaster.vue
@@ -4,6 +4,8 @@
     title="KToaster"
   >
     <div class="ktoaster-sandbox">
+      <div class="toast-overlay" />
+
       <!-- Props -->
       <SandboxTitleComponent
         is-subtitle
@@ -152,7 +154,7 @@ const openToaster = (argument: string) => {
         title: 'Timeout',
         message: 'This is toaster with timeout (10s)',
         appearance: 'info',
-        timeoutMilliseconds: 10000,
+        timeoutMilliseconds: 1000000,
       }
       break
   }
@@ -168,10 +170,23 @@ const multipleToastersHandler = () => {
 
 <style lang="scss" scoped>
 .ktoaster-sandbox {
+  height: 100%;
+  position: relative;
+
   .horizontal-container {
     display: flex;
     flex-wrap: wrap;
     gap: $kui-space-50;
+  }
+
+  .toast-overlay {
+    background-color: red;
+    bottom: 16px;
+    height: 100px;
+    position: absolute;
+    right: 16px;
+    width: 200px;
+    z-index: 2;
   }
 }
 </style>
