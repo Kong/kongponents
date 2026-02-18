c9b4895510023f818e4121b4700654cd79228dda
Jonathan Walters <Chalks@users.noreply.github.com>
2026-02-18 17:40:14 -0500

    feat: kfiltergroup base functionality [khcp-19485, khcp-19482] (#3092)
    * feat: basic component initialization
    
    * feat: child component InteractivePill and types
    
    * feat: child component FilterSelector
    
    * feat: child component FilterPill
    
    * feat: added KFilterGroup basic functionality
    
    * feat: basic sandbox page
    
    * fix: address PR comments
    
    * fix: var all the tokens
    
    * test: adjust tests after changes
    
    * fix: no faking design tokens
    
    * feat: use buttons for clickable things
    
    * fix: required layout for tooltip to work
    
    * fix: address feedback

---
+++ docs/components/filter-group.md
@@ -0,0 +1,69 @@
+# Filter Group
+
+**KFilterGroup** should be described here in the first paragraph.
+
+<KFilterGroup />
+
+```html
+<template>
+  <KFilterGroup />
+</template>
+
+<script setup lang="ts">
+  // Example code goes here
+</script>
+```
+
+## Props
+
+### examplePropName
+
+Description of `examplePropName`
+
+Actual component using examplePropName
+
+<KFilterGroup :examplePropName="true" />
+
+```html
+<KFilterGroup examplePropName="variation1" />
+<KFilterGroup examplePropName="variation2" />
+<KFilterGroup examplePropName="variation3" />
+```
+
+## Slots
+
+- `default` - default slot description
+- `slotName` - slot description
+
+```html
+<KFilterGroup>
+  here is some slot content
+</KFilterGroup>
+```
+
+## Events
+
+- `@change` - Emitted when...
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  setup (props) {
+    // ... code goes here
+  }
+})
+</script>
+
+<style lang="scss" scoped>
+.KFilterGroup-wrapper {
+  --KFilterGroup-wrapperBorderColor: lime;
+}
+</style>
+```
+
+<style lang="scss" scoped>
+.KFilterGroup-wrapper {
+  --KFilterGroup-wrapperBorderColor: lime;
+}
+</style>
---
+++ sandbox/pages/SandboxFilterGroup.vue
@@ -0,0 +1,57 @@
+<template>
+  <SandboxLayout
+    :links="inject('app-links', [])"
+    title="KFilterGroup"
+  >
+    <SandboxTitleComponent
+      is-subtitle
+      title="WIP, content is not complete"
+    />
+
+    <KFilterGroup
+      v-model="selection"
+      :filters="filters"
+    />
+  </SandboxLayout>
+</template>
+
+<script setup lang="ts">
+import { inject, ref } from 'vue'
+import SandboxTitleComponent from '../components/SandboxTitleComponent.vue'
+import type { FilterGroupSelection } from '@/types'
+
+const filters = ref({
+  status: {
+    label: 'Status',
+  },
+  controlPlane: {
+    label: 'Control plane',
+    pinned: true,
+  },
+  bar: {
+    label: 'Bar',
+    pinned: true,
+  },
+  foo: {
+    label: 'Foo',
+  },
+  loremipsum: {
+    label: 'Lorem ipsum',
+  },
+  longlabel: {
+    label: 'Really long label that will cause issues everywhere maybe.',
+  },
+})
+
+const selection = ref<FilterGroupSelection>({ controlPlane: { operator: 'eq', value: 'ay', text: 'Ayy' } })
+</script>
+
+<style scoped lang="scss">
+.interactive-pill-controls {
+  align-items: center;
+  display: flex;
+  flex-wrap: wrap;
+  gap: 20px;
+  padding-bottom: $kui-space-80;
+}
+</style>
