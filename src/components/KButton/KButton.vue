d13dfa8ff38731ec97ae1c544f1a488b9ed01794
Adam DeHaven <adam.dehaven@konghq.com>
2026-05-13 12:50:45 -0400

    fix: button font tokens

--- src/components/KButton/KButton.vue
+++ src/components/KButton/KButton.vue
@@ -149,8 +149,8 @@ export default {
     border-radius: var(--kui-button-border-radius, var(--kui-border-radius-30, $kui-border-radius-30));
     cursor: pointer;
     display: inline-flex;
-    font-family: var(--kui-font-family-text, $kui-font-family-text);
-    font-weight: var(--kui-font-weight-semibold, $kui-font-weight-semibold);
+    font-family: var(--kui-button-font-family, var(--kui-font-family-text, $kui-font-family-text));
+    font-weight: var(--kui-button-font-weight, var(--kui-font-weight-semibold, $kui-font-weight-semibold));
     justify-content: center;
     // Remove tap color highlight on mobile Safari
     -webkit-tap-highlight-color: rgba(255, 255, 255, 0);
