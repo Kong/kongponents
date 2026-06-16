52cd45463d38555c204bc2b7b9f5609512ba4f29
Adam DeHaven <adam.dehaven@konghq.com>
2026-06-15 21:58:56 -0400

    fix(deps): new stylelint plugin

--- .stylelintrc.mjs
+++ .stylelintrc.mjs
@@ -6,7 +6,7 @@ export default {
   ],
   plugins: [
     'stylelint-order',
-    '@kong/design-tokens/stylelint-plugin',
+    '@kong/stylelint-plugin-design-tokens',
     '@stylistic/stylelint-plugin',
   ],
   rules: {
