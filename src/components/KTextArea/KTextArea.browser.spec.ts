90e33055070ea9f4fd11f29069cd82b0de84b3c6
arora-r <arrohitarora@gmail.com>
2026-08-27 14:08:46 -0400

    fix: ignore type check here

--- src/components/KTextArea/KTextArea.browser.spec.ts
+++ src/components/KTextArea/KTextArea.browser.spec.ts
@@ -179,6 +179,7 @@ describe('KTextArea', () => {
 
     await render(KTextArea, {
       props: {
+        // @ts-expect-error - to allow passing an invalid characterLimit
         characterLimit: true,
         modelValue: string,
       },
